f40ca90f509772e581e65816a9efd203239e8a48
Protogenoi <michael@networkingbits.com>
2016-12-12 16:15:16 +0000

    Updated MySQLI connection datatables/getleadaudits.php

--- datatables/getleadaudits.php
+++ datatables/getleadaudits.php
@@ -1,9 +1,9 @@
 <?php
 
-include('../includes/PDOcon.php');
+include('../includes/ADL_MYSQL_CON.php');
 
     $sql = 'SELECT id, date_submitted, lead_gen_name, total, score, auditor, grade, edited, date_edited from lead_gen_audit ';
-    $result = mysqli_query($connection, $sql) or die("Error in Selecting " . mysqli_error($connection));
+    $result = mysqli_query($conn, $sql) or die("Error in Selecting " . mysqli_error($conn));
 
 
     $rows = array();

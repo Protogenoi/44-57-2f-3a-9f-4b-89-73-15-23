e6d7de9f466f7215dcee2066102b5b503a64f806
Protogenoi <michael@networkingbits.com>
2016-12-13 16:26:41 +0000

    Fixed error caused by restricted page

--- Life/Templates/TrustLetter.php
+++ Life/Templates/TrustLetter.php
@@ -1,9 +1,4 @@
 <?php
-include($_SERVER['DOCUMENT_ROOT']."/classes/access_user/access_user_class.php"); 
-$page_protect = new Access_user;
-$page_protect->access_page($_SERVER['PHP_SELF'], "", 2);
-$hello_name = ($page_protect->user_full_name != "") ? $page_protect->user_full_name : $page_protect->user;
-
 require('../../fpdf17/fpdf.php');
 include('../../includes/ADL_PDO_CON.php');
 

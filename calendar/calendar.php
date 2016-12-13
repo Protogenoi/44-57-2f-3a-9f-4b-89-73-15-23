1bde832e754d75e2768262e42f18672a78eac8e0
Protogenoi <michael@networkingbits.com>
2016-12-13 15:45:12 +0000

    calendar/ Folder Updated code for MySQL sessions

--- calendar/calendar.php
+++ calendar/calendar.php
@@ -6,9 +6,9 @@ include('../includes/adlfunctions.php');
 
 include($_SERVER['DOCUMENT_ROOT']."/classes/access_user/access_user_class.php"); 
 
-$test_access_level = new Access_user;
-$test_access_level->access_page($_SERVER['PHP_SELF'], "", 2); 
-$hello_name = ($test_access_level->user_full_name != "") ? $test_access_level->user_full_name : $test_access_level->user;
+$page_protect = new Access_user;
+$page_protect->access_page($_SERVER['PHP_SELF'], "", 2); 
+$hello_name = ($page_protect->user_full_name != "") ? $page_protect->user_full_name : $page_protect->user;
 
 include('../includes/Access_Levels.php');
 
--- calendar/events.php
+++ calendar/events.php
@@ -3,9 +3,9 @@ include($_SERVER['DOCUMENT_ROOT']."/classes/access_user/access_user_class.php");
 
 include ($_SERVER['DOCUMENT_ROOT']."/includes/ADL_PDO_CON.php");
 
-$test_access_level = new Access_user;
-$test_access_level->access_page($_SERVER['PHP_SELF'], "", 2); 
-$hello_name = ($test_access_level->user_full_name != "") ? $test_access_level->user_full_name : $test_access_level->user;
+$page_protect = new Access_user;
+$page_protect->access_page($_SERVER['PHP_SELF'], "", 2); 
+$hello_name = ($page_protect->user_full_name != "") ? $page_protect->user_full_name : $page_protect->user;
 // List of events
  $json = array();
 

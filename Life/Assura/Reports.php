b1f3c7bd1d2ea3ff50b2c9fe2b36dc5415d513c6
Protogenoi <michael@networkingbits.com>
2016-12-13 15:01:32 +0000

    Updated code for MySQL sessions

--- Life/Assura/Reports.php
+++ Life/Assura/Reports.php
@@ -1,8 +1,8 @@
 <?php 
 include($_SERVER['DOCUMENT_ROOT']."/classes/access_user/access_user_class.php"); 
-$test_access_level = new Access_user;
-$test_access_level->access_page($_SERVER['PHP_SELF'], "", 7);
-$hello_name = ($test_access_level->user_full_name != "") ? $test_access_level->user_full_name : $test_access_level->user;
+$page_protect = new Access_user;
+$page_protect->access_page($_SERVER['PHP_SELF'], "", 7);
+$hello_name = ($page_protect->user_full_name != "") ? $page_protect->user_full_name : $page_protect->user;
 
 include('../../includes/adlfunctions.php'); 
 

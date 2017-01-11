baa65f82c4ce0e42a1f7e0276dfd169a71913ef7
Protogenoi <michael@networkingbits.com>
2017-01-11 14:49:41 +0000

    Updated results for dispo

--- dialer/AllScore.php
+++ dialer/AllScore.php
@@ -162,6 +162,148 @@ elseif ($result['campaign_id'] =='10' && $result['lead_id']>'1') {$result['statu
 } 
 echo "</table>";
 
+$query = $TRB_DB_PDO->prepare("select 
+vicidial_live_agents.uniqueid
+, vicidial_live_agents.status
+, vicidial_live_agents.pause_code
+, vicidial_auto_calls.phone_number
+, vicidial_users.full_name
+, TIMEDIFF(current_TIMESTAMP, vicidial_live_agents.last_state_change) as Time
+, count(IF(vicidial_agent_log.status='QML',1,NULL)) AS QML
+, count(IF(vicidial_agent_log.status='QQQ',1,NULL)) AS QQQ
+, count(IF(vicidial_agent_log.status='QCBK',1,NULL)) AS QCBK
+, count(IF(vicidial_agent_log.status='DIDNO',1,NULL)) AS DIDNO
+, count(IF(vicidial_agent_log.status='QNQ',1,NULL)) AS QNQ
+, count(IF(vicidial_agent_log.status='QUN',1,NULL)) AS QUN
+, count(IF(vicidial_agent_log.status='QDE',1,NULL)) AS QDE
+, count(IF(vicidial_agent_log.status='NoCard',1,NULL)) AS  NoCard
+, count(IF(vicidial_agent_log.status NOT IN ('QDE','QUN','NoCard','QNQ','DIDNO','QCBK','QQQ','QML'),1,NULL)) AS  OTHER
+, vicidial_live_inbound_agents.calls_today AS Leads 
+, (vicidial_live_inbound_agents.calls_today/count(IF(vicidial_agent_log.status='SALE',1,NULL))) As Total
+from vicidial_agent_log
+JOIN vicidial_users ON vicidial_users.user = vicidial_agent_log.user
+JOIN vicidial_live_inbound_agents on vicidial_users.full_name = vicidial_live_inbound_agents.group_id
+LEFT JOIN vicidial_live_agents ON vicidial_agent_log.user = vicidial_live_agents.user
+LEFT JOIN vicidial_list on vicidial_live_agents.lead_id =vicidial_list.lead_id
+LEFT JOIN vicidial_auto_calls on vicidial_live_agents.lead_id = vicidial_auto_calls.lead_id
+LEFT JOIN vicidial_lists on vicidial_list.list_id = vicidial_lists.list_id
+WHERE vicidial_agent_log.event_time >= CURDATE()
+AND vicidial_agent_log.campaign_id ='15'
+GROUP by vicidial_agent_log.user ORDER BY vicidial_live_agents.status ASC, last_state_change LIMIT 10");
+
+echo "<table id='main2' cellspacing='0' cellpadding='10'>
+<th class='status_PAUSED'>Closer</th>
+<th class='status_PAUSED'>Quote Mortgage Lead</th>
+<th class='status_PAUSED'>Quoted</th>
+<th class='status_PAUSED'>Quote Callback</th>
+<th class='status_PAUSED'>Didn't Beat</th>
+<th class='status_PAUSED'>No Quote</th>
+<th class='status_PAUSED'>Underwritten</th>
+<th class='status_PAUSED'>Decline</th>
+<th class='status_PAUSED'>No Card</th>
+<th class='status_PAUSED'>Other</th>";
+    
+$query->execute();
+if ($query->rowCount()>0) {
+while ($result=$query->fetch(PDO::FETCH_ASSOC)){
+
+    $QML = $result['QML'];
+    $QQQ = $result['QQQ'];
+    $QCBK = $result['QCBK'];
+    $DIDNO = $result['DIDNO'];
+    $QNQ = $result['QNQ'];
+    $QUN = $result['QUN'];
+    $QDE = $result['QDE'];
+    $NoCard = $result['NoCard'];
+    $OTHER = $result['OTHER'];
+
+$Sales = $result['Sales'];
+$Leads = $result['Leads'];
+$Total = $result['Total'];
+$CLOSER_NAME=$result['full_name'];
+
+
+$FormattedConversionrate = number_format($Total,1);
+
+if($Sales=='0') {
+    
+}
+
+switch ($CLOSER_NAME) {
+    case("Matt"):
+        $LEADS = $Leads - 0;
+        $SALES = $Sales + 0;
+        break;
+    case("Richard"):
+        $LEADS = $Leads - 0;
+        $SALES = $Sales + 0;        
+        break;
+    case("Kyle"):
+        $LEADS = $Leads - 0;
+        $SALES = $Sales + 0;
+        break;
+    case("Sarah"):
+        $LEADS = $Leads - 0;
+        $SALES = $Sales + 0;
+        break; 
+    case("Gavin"):
+        $LEADS = $Leads - 0;
+        $SALES = $Sales + 0;    
+        break; 
+    case("James"):
+        $LEADS = $Leads - 0;
+        $SALES = $Sales + 0;    
+        break; 
+    case("Ricky"):
+        $LEADS = $Leads - 0;
+        $SALES = $Sales + 0;       
+        break; 
+    default:
+        $LEADS = $Leads;
+        $SALES = $Sales;
+        break;
+    
+}
+   
+$Conversionrate = $LEADS /$SALES;
+$Formattedrate = number_format($Conversionrate,1);
+
+ switch( $result['status'] )
+    {
+      case("READY"):
+         $class2 = 'status_READY12';
+          break;
+        case("INCALL"):
+          $class2 = 'status_INCALL12';
+	if ($result['uniqueid']=='0') {$result['status'] = 'MANUAL'; $class2 = 'status_MANUAL2';}
+	if ($result['phone_number']<='0') {$result['status'] = 'DEAD'; $class2 = 'status_DEAD2';}
+elseif ($result['campaign_id'] =='10' && $result['lead_id']>'1') {$result['status'] = 'TRANSFER'; $class2 = 'status_piltrans';}
+           break;
+       case("PAUSED"):
+            $class2 = 'status_PAUSED12';
+          break;
+       case("QUEUE"):
+            $class2 = 'status_QUEUE2';
+          break;
+        default:
+            $class2 = 'status_READY12';
+            break;
+ }
+	echo '<tr><td class='.$class2.'><strong style="font-size: 65px;">'.$CLOSER_NAME.' </strong></td>';
+        echo '<td class='.$class2.'><strong style="font-size: 65px;">'.$QML.'</strong></td>';
+        echo '<td class='.$class2.'><strong style="font-size: 65px;">'.$QQQ.'</strong></td>';
+        echo '<td class='.$class2.'><strong style="font-size: 65px;">'.$QCBK.'</strong></td>';
+        echo '<td class='.$class2.'><strong style="font-size: 65px;">'.$DIDNO.'</strong></td>';
+        echo '<td class='.$class2.'><strong style="font-size: 65px;">'.$QNQ.'</strong></td><';
+        echo '<td class='.$class2.'><strong style="font-size: 65px;">'.$QUN.'</strong></td>';
+        echo '<td class='.$class2.'><strong style="font-size: 65px;">'.$QDE.'</strong></td>';
+        echo '<td class='.$class2.'><strong style="font-size: 65px;">'.$NoCard.'</strong></td>';
+        echo '<td class='.$class2.'><strong style="font-size: 65px;">'.$OTHER.'</strong></td></tr>';
+
+}
+} 
+echo "</table>";
+ 
 ?>
         </div>
     </div>

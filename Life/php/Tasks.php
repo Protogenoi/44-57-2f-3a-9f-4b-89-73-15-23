5009c5ea9b9248d4cf540951cd43b3140b9d957b
Protogenoi <michael@networkingbits.com>
2017-02-07 18:18:21 +0000

    Updated access level required

--- Life/php/Tasks.php
+++ Life/php/Tasks.php
@@ -1,7 +1,7 @@
 <?php
 include($_SERVER['DOCUMENT_ROOT']."/classes/access_user/access_user_class.php"); 
 $page_protect = new Access_user;
-$page_protect->access_page($_SERVER['PHP_SELF'], "", 7); 
+$page_protect->access_page($_SERVER['PHP_SELF'], "", 6); 
 $hello_name = ($page_protect->user_full_name != "") ? $page_protect->user_full_name : $page_protect->user;
         
         ini_set('display_errors', 1);

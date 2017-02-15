f9b5311ff43d71b9a3def11c94776348370bd763
Protogenoi <michael@networkingbits.com>
2017-02-15 17:43:00 +0000

    Altered MySQL for business register to display comm date and payment amounts

--- export/Export.php
+++ export/Export.php
@@ -57,22 +57,20 @@ if(isset($query)) {
         }
         
         if($query=='LIFE') {
-            $output = "Application_Number,Policy_Number,Sale_Date,Title,Forename,Surname,Tel,Alt_Tel,DOB,EMail,Address_Line_1,Address_Line_2,Town,Postcode,Premium,Type,Commission,Paid_to_HWIFS,Net_Paid,Closer,Status,Insurer,Owner,Company,Date_Added\n";
-            $query = $pdo->prepare("SELECT client_policy.application_number, client_policy.policy_number, client_policy.sale_date, '' AS empty_col , client_policy.client_name, '' AS empty_col , client_details.phone_number,  client_details.alt_number, CONCAT(client_details.dob,' - ',client_details.dob2) AS CDOB, client_details.email, client_details.address1, client_details.address2, CONCAT(client_details.address3, ' ', client_details.town) AS TOWN, client_details.post_code, client_policy.premium, client_policy.type, client_policy.commission,'' AS empty_col ,'' AS empty_col2 , CONCAT(client_policy.lead, '/', client_policy.closer) AS AGENTS, client_policy.policystatus, client_policy.insurer, client_policy.submitted_by, client_details.company, client_details.submitted_date FROM  client_policy LEFT JOIN client_details ON client_policy.client_id=client_details.client_id WHERE DATE(client_policy.sale_date) between :datefrom and :dateto OR (DATE(client_policy.submitted_date) between :datefrom2 and :dateto2) AND client_details.company ='The Review Bureau'");
+            $output = "Application_Number,Policy_Number, Sale_Date,Title,Forename,Surname,Tel,Alt_Tel,DOB,EMail,Address_Line_1,Address_Line_2,Town,Postcode,Premium,Type,Commission,Paid_to_HWIFS,Net_Paid,Closer,Status,Insurer,Owner,Company,Date_Added\n";
+            $query = $pdo->prepare("SELECT financial_statistics_history.payment_amount, client_policy.application_number, client_policy.policy_number, CONCAT(DATE(client_policy.sale_date),' | ', DATE(financial_statistics_history.insert_date)) AS sale_date, '' AS empty_col , client_policy.client_name, '' AS empty_col , client_details.phone_number,  client_details.alt_number, CONCAT(client_details.dob,' - ',client_details.dob2) AS CDOB, client_details.email, client_details.address1, client_details.address2, CONCAT(client_details.address3, ' ', client_details.town) AS TOWN, client_details.post_code, client_policy.premium, client_policy.type, client_policy.commission,'' AS empty_col ,'' AS empty_col2 , CONCAT(client_policy.lead, '/', client_policy.closer) AS AGENTS, client_policy.policystatus, client_policy.insurer, client_policy.submitted_by, client_details.company, client_details.submitted_date FROM  client_policy LEFT JOIN client_details ON client_policy.client_id=client_details.client_id LEFT JOIN financial_statistics_history ON financial_statistics_history.policy=client_policy.policy_number  WHERE DATE(client_policy.sale_date) between :datefrom and :dateto OR (DATE(client_policy.submitted_date) between :datefrom2 and :dateto2)");
             $query->bindParam(':datefrom', $datefrom, PDO::PARAM_STR);
             $query->bindParam(':dateto', $dateto, PDO::PARAM_STR);
             $query->bindParam(':datefrom2', $datefrom, PDO::PARAM_STR);
             $query->bindParam(':dateto2', $dateto, PDO::PARAM_STR);
             $query->execute();
-            
             $list = $query->fetchAll();
             foreach ($list as $rs) {
-                $output .= $rs['application_number'].",".$rs['policy_number'].",".$rs['sale_date'].",".$rs['empty_col'].",".$rs['client_name'].",".$rs['empty_col'].",".$rs['phone_number'].",".$rs['alt_number'].",".$rs['CDOB'].",".$rs['email'].",".$rs['address1'].",".$rs['address2'].",".$rs['TOWN'].",".$rs['post_code'].",".$rs['premium'].",".$rs['type'].",".$rs['commission'].",".$rs['empty_col'].",".$rs['empty_col2'].",".$rs['AGENTS'].",".$rs['policystatus'].",".$rs['insurer'].",".$rs['submitted_by'].",".$rs['company'].",".$rs['submitted_date']."\n";
+                $output .= $rs['application_number'].",".$rs['policy_number'].",".$rs['sale_date'].",".$rs['payment_amount'].",".$rs['client_name'].",".$rs['empty_col'].",".$rs['phone_number'].",".$rs['alt_number'].",".$rs['CDOB'].",".$rs['email'].",".$rs['address1'].",".$rs['address2'].",".$rs['TOWN'].",".$rs['post_code'].",".$rs['premium'].",".$rs['type'].",".$rs['commission'].",".$rs['empty_col'].",".$rs['empty_col2'].",".$rs['AGENTS'].",".$rs['policystatus'].",".$rs['insurer'].",".$rs['submitted_by'].",".$rs['company'].",".$rs['submitted_date']."\n";
                 
             }
             echo $output;
             exit;
-            
             }
             
             if($query=='VITALITYLIFE') {

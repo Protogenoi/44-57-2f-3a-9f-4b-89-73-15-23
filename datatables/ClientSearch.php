8a92c3570cf3167ec16511a4c003f1b9e8b04d51
Protogenoi <michael@networkingbits.com>
2017-03-21 12:30:32 +0000

    Changed to DESC instead of ASC ordering

--- datatables/ClientSearch.php
+++ datatables/ClientSearch.php
@@ -11,7 +11,7 @@ if(isset($ClientSearch)) {
         
     include('../includes/ADL_PDO_CON.php');
 
-        $query = $pdo->prepare("SELECT company, phone_number, submitted_date, client_id, CONCAT(title, ' ', first_name, ' ', last_name) AS Name, CONCAT(title2, ' ', first_name2, ' ', last_name2) AS Name2, post_code FROM client_details ORDER BY client_id ASC");
+        $query = $pdo->prepare("SELECT company, phone_number, submitted_date, client_id, CONCAT(title, ' ', first_name, ' ', last_name) AS Name, CONCAT(title2, ' ', first_name2, ' ', last_name2) AS Name2, post_code FROM client_details ORDER BY client_id DESC");
 $query->execute()or die(print_r($query->errorInfo(), true));
 json_encode($results['aaData']=$query->fetchAll(PDO::FETCH_ASSOC));
 

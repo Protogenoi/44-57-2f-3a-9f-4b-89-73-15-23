01874c93fe8c110c23aca9671e74ed6b75148825
Protogenoi <michael@networkingbits.com>
2017-03-24 14:29:52 +0000

    Removed old stylesheet

--- Life/ViewClient.php
+++ Life/ViewClient.php
@@ -103,7 +103,6 @@ if (isset($Single_Client['callauditid'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="/styles/layoutcrm.css" type="text/css" />
-    <link rel="stylesheet" href="/styles/Timeline.css" type="text/css" />
     <link rel="stylesheet" href="/bootstrap-3.3.5-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="/bootstrap-3.3.5-dist/css/bootstrap-theme.min.css">
     <link rel="stylesheet" href="/font-awesome/css/font-awesome.min.css">

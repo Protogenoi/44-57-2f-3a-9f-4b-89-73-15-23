77d73b6a3154af078063cd4f87fa5b5254176c09
Michael <michael@adl-crm.uk>
2017-04-05 16:20:26 +0100

    Updated access levels for Nick

--- includes/navbar.php
+++ includes/navbar.php
@@ -30,10 +30,10 @@ $companydetailsq = $cnquery->fetch(PDO::FETCH_ASSOC);
 $companynamere = $companydetailsq['company_name'];
 
 if ($companynamere == 'The Review Bureau') {
-    $Level_10_Access = array("Michael", "Matt", "leighton");
-    $Level_8_Access = array("Michael", "Matt", "leighton", "Abbiek", "carys", "Tina", "Heidy", "Nicola", "Mike");
-    $Level_3_Access = array("Michael", "Matt", "leighton", "Abbiek", "carys", "Jakob", "Nicola", "Tina", 'Heidy', 'Amy', "Mike", "Keith", "Renee", "Victoria", "Christian", "Audits", "Tiaba");
-    $Level_1_Access = array("Michael", "Matt", "leighton", "Abbiek", "carys", "Jakob", "Nicola", "Tina", 'Heidy', 'Amy', "Mike", "Keith", "Renee", "Victoria", "Christian", "Audits", "Tiaba");
+    $Level_10_Access = array("Michael", "Matt", "leighton","Nick");
+    $Level_8_Access = array("Michael", "Matt", "leighton", "Nick", "Abbiek", "carys", "Tina", "Heidy", "Nicola", "Mike");
+    $Level_3_Access = array("Michael", "Matt", "leighton", "Nick", "Abbiek", "carys", "Jakob", "Nicola", "Tina", 'Heidy', 'Amy', "Mike", "Keith", "Renee", "Victoria", "Christian", "Audits", "Tiaba");
+    $Level_1_Access = array("Michael", "Matt", "leighton", "Nick", "Abbiek", "carys", "Jakob", "Nicola", "Tina", 'Heidy', 'Amy', "Mike", "Keith", "Renee", "Victoria", "Christian", "Audits", "Tiaba");
     $SECRET = array("Michael", "Abbiek", "carys", "Jakob", "Nicola", "Tina", 'Amy', "Victoria", "Christian");
     $Task_Access = array("Michael", "Abbiek", "Victoria", "Keith");
 }

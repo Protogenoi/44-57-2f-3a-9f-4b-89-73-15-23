92ca51f338728a51191bf47c86e168d603be1d38
Michael <michael@adl-crm.uk>
2017-04-06 10:59:06 +0100

    Added level 9 access

--- includes/navbar.php
+++ includes/navbar.php
@@ -31,6 +31,7 @@ $companynamere = $companydetailsq['company_name'];
 
 if ($companynamere == 'The Review Bureau') {
     $Level_10_Access = array("Michael", "Matt", "leighton","Nick");
+    $Level_9_Access = array("Michael", "Matt", "leighton","Nick","carys");
     $Level_8_Access = array("Michael", "Matt", "leighton", "Nick", "Abbiek", "carys", "Tina", "Heidy", "Nicola", "Mike");
     $Level_3_Access = array("Michael", "Matt", "leighton", "Nick", "Abbiek", "carys", "Jakob", "Nicola", "Tina", 'Heidy', 'Amy', "Mike", "Keith", "Renee", "Victoria", "Christian", "Audits", "Tiaba");
     $Level_1_Access = array("Michael", "Matt", "leighton", "Nick", "Abbiek", "carys", "Jakob", "Nicola", "Tina", 'Heidy', 'Amy', "Mike", "Keith", "Renee", "Victoria", "Christian", "Audits", "Tiaba");
@@ -40,6 +41,7 @@ if ($companynamere == 'The Review Bureau') {
 
 if ($companynamere == 'ADL_CUS') {
     $Level_10_Access = array("Michael", "Dean", "Helen", "Andrew", "David");
+    $Level_9_Access = array("Michael", "Dean", "Helen", "Andrew", "David");
     $Level_8_Access = array("Michael", "Dean", "Helen", "Andrew", "David");
     $Level_3_Access = array("Michael", "Dean", "Helen", "Andrew", "David");
     $Level_1_Access = array("Michael", "Dean", "Helen", "Andrew", "David");

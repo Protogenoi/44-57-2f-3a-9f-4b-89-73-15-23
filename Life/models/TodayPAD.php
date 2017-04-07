1192cf6cd845a182a6a644e2969d5706deaefff8
Michael <michael@adl-crm.uk>
2017-04-07 11:19:41 +0100

    Updates

--- Life/models/TodayPAD.php
+++ Life/models/TodayPAD.php
@@ -8,9 +8,23 @@ class TodayPadModal {
         $this->pdo = $pdo;
     }
 
-    public function getTodayPad($search) {
+    public function getTodayPad() {
 
-        $stmt = $this->pdo->prepare("SELECT pad_statistics_id, pad_statistics_lead, pad_statistics_closer, pad_statistics_notes, pad_statistics_status, pad_statistics_col FROM pad_statistics ORDER BY pad_statistics_update_date DESC");
+        $stmt = $this->pdo->prepare("SELECT 
+    pad_statistics_group,
+    pad_statistics_id,
+    pad_statistics_lead,
+    pad_statistics_closer,
+    pad_statistics_notes,
+    pad_statistics_status,
+    pad_statistics_col,
+    DATE(pad_statistics_update_date) AS pad_statistics_update_date,
+    pad_statistics_group
+FROM
+    pad_statistics
+WHERE
+    pad_statistics_added_date >= CURDATE()
+ORDER BY pad_statistics_added_date DESC");
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }

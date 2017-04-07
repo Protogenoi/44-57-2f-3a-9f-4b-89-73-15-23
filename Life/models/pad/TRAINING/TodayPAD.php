7c8d600a7b2e71aa7d478075cc715033d47735c7
Michael <michael@adl-crm.uk>
2017-04-07 17:56:39 +0100

    Updates to code.

---
+++ Life/models/pad/TRAINING/TodayPAD.php
@@ -0,0 +1,35 @@
+<?php
+
+class TRAININGTodayPadModal {
+
+    protected $pdo;
+
+    public function __construct(PDO $pdo) {
+        $this->pdo = $pdo;
+    }
+
+    public function TRAININGgetTodayPad($TEAM) {
+
+        $stmt = $this->pdo->prepare("SELECT 
+    pad_statistics_group,
+    pad_statistics_id,
+    pad_statistics_lead,
+    pad_statistics_closer,
+    pad_statistics_notes,
+    pad_statistics_status,
+    pad_statistics_col,
+    DATE(pad_statistics_update_date) AS pad_statistics_update_date,
+    pad_statistics_group
+FROM
+    pad_statistics
+WHERE
+    pad_statistics_added_date >= CURDATE() AND pad_statistics_group=:team
+ORDER BY pad_statistics_added_date DESC");
+        $stmt->bindParam(':team', $TEAM, PDO::PARAM_STR);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+}
+
+?>

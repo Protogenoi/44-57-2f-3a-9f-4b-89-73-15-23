e057ce6f94931b408b9921308fef38e67823b058
Michael <michael@adl-crm.uk>
2017-04-10 16:11:03 +0100

    Added datefrom to MySQL query conditions

--- Life/models/pad/ADMIN/TodayPAD.php
+++ Life/models/pad/ADMIN/TodayPAD.php
@@ -1,16 +1,52 @@
 <?php
 
-class ADMINTodayPadModal {
+if (isset($datafrom)) {
 
-    protected $pdo;
+    class ADMINTodayPadModal {
 
-    public function __construct(PDO $pdo) {
-        $this->pdo = $pdo;
+        protected $pdo;
+
+        public function __construct(PDO $pdo) {
+            $this->pdo = $pdo;
+        }
+
+        public function ADMINgetTodayPad($datefrom) {
+
+            $stmt = $this->pdo->prepare("SELECT 
+    pad_statistics_group,
+    pad_statistics_id,
+    pad_statistics_lead,
+    pad_statistics_closer,
+    pad_statistics_notes,
+    pad_statistics_status,
+    pad_statistics_col,
+    DATE(pad_statistics_update_date) AS pad_statistics_update_date,
+    pad_statistics_group
+FROM
+    pad_statistics
+WHERE
+    DATE(pad_statistics_added_date)=:datefrom AND pad_statistics_group='Admin'
+ORDER BY pad_statistics_added_date DESC");
+            $stmt->bindParam(':datefrom', $datefrom, PDO::PARAM_STR);
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+
     }
 
-    public function ADMINgetTodayPad() {
+} else {
 
-        $stmt = $this->pdo->prepare("SELECT 
+    class ADMINTodayPadModal {
+
+        protected $pdo;
+
+        public function __construct(PDO $pdo) {
+            $this->pdo = $pdo;
+        }
+
+        public function ADMINgetTodayPad() {
+
+            $stmt = $this->pdo->prepare("SELECT 
     pad_statistics_group,
     pad_statistics_id,
     pad_statistics_lead,
@@ -25,10 +61,11 @@ FROM
 WHERE
     pad_statistics_added_date >= CURDATE() AND pad_statistics_group='Admin'
 ORDER BY pad_statistics_added_date DESC");
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+
     }
 
 }
-
 ?>

aa83c62386505f4b7bfe8d27e1899851d6132641
Michael <michael@adl-crm.uk>
2017-04-10 16:11:09 +0100

    Added datefrom to MySQL query conditions

--- Life/models/pad/TeamPAD.php
+++ Life/models/pad/TeamPAD.php
@@ -1,16 +1,46 @@
 <?php
 
-class TeamPadModal {
+if (isset($datafrom)) {
 
-    protected $pdo;
+    class TeamPadModal {
 
-    public function __construct(PDO $pdo) {
-        $this->pdo = $pdo;
+        protected $pdo;
+
+        public function __construct(PDO $pdo) {
+            $this->pdo = $pdo;
+        }
+
+        public function getTeamPad($datefrom, $TEAM) {
+
+            $stmt = $this->pdo->prepare("SELECT 
+    SUM(pad_statistics_col) AS COMM,
+    AVG(pad_statistics_col) AS AVG,
+    pad_statistics_group
+FROM
+    pad_statistics
+WHERE
+    pad_statistics_added_date=:datefrom AND pad_statistics_group=:team GROUP BY pad_statistics_group");
+            $stmt->bindParam(':team', $TEAM, PDO::PARAM_STR);
+            $stmt->bindParam(':datefrom', $datefrom, PDO::PARAM_STR);
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+
     }
 
-    public function getTeamPad($TEAM) {
+} else {
 
-        $stmt = $this->pdo->prepare("SELECT 
+    class TeamPadModal {
+
+        protected $pdo;
+
+        public function __construct(PDO $pdo) {
+            $this->pdo = $pdo;
+        }
+
+        public function getTeamPad($TEAM) {
+
+            $stmt = $this->pdo->prepare("SELECT 
     SUM(pad_statistics_col) AS COMM,
     AVG(pad_statistics_col) AS AVG,
     pad_statistics_group
@@ -18,11 +48,12 @@ FROM
     pad_statistics
 WHERE
     pad_statistics_added_date >= CURDATE() AND pad_statistics_group=:team GROUP BY pad_statistics_group");
-        $stmt->bindParam(':team', $TEAM, PDO::PARAM_STR);
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $stmt->bindParam(':team', $TEAM, PDO::PARAM_STR);
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+
     }
 
 }
-
 ?>

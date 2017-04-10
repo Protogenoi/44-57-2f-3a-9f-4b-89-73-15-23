6dfbb2001792d6357dc4ee367fafbdac5c986fd1
Michael <michael@adl-crm.uk>
2017-04-10 13:32:07 +0100

    Correct code for Training POD

--- Life/views/pad/TRAINING/Team-PAD.php
+++ Life/views/pad/TRAINING/Team-PAD.php
@@ -1,20 +1,20 @@
 <?php foreach ($TRAININGTeamPadList as $TRAININGTeam_Pad): ?>
 
     <?php
-    $POD_COMM6 = number_format($TRAININGTeam_Pad['COMM'], 2);
-    $POD_AVG6 = number_format($TRAININGTeam_Pad['AVG'], 2);
-    $POD_TEAM6 = $TRAININGTeam_Pad['pad_statistics_group'];
+    $TRAINING_COMM = number_format($TRAININGTeam_Pad['COMM'], 2);
+    $TRAINING_AVG = number_format($TRAININGTeam_Pad['AVG'], 2);
+    $TRAINING_TEAM = $TRAININGTeam_Pad['pad_statistics_group'];
     ?>
     <tr>
-        <td><?php echo $POD_TEAM6; ?></td>
-                <td><input size="8" disabled class="form-control" type="currency" name="POD_AVG" value="<?php
-                   if (isset($POD_AVG6)) {
-                       echo "£$POD_AVG6";
+        <td><?php echo $TRAINING_TEAM; ?></td>
+                <td><input size="8" disabled class="form-control" type="currency" name="TRAINING_AVG" value="<?php
+                   if (isset($TRAINING_AVG)) {
+                       echo "£$TRAINING_AVG";
                    }
                    ?>"></td>
-        <td><input size="8" disabled class="form-control" type="currency" name="POD_COMM" value="<?php
-                   if (isset($POD_COMM6)) {
-                       echo "£$POD_COMM6";
+        <td><input size="8" disabled class="form-control" type="currency" name="TRAINING_COMM" value="<?php
+                   if (isset($TRAINING_COMM)) {
+                       echo "£$TRAINING_COMM";
                    }
                    ?>"></td></tr>
 

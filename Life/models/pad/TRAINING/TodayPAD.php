aef70603be145004a77a919f68238c6c58db138a
Michael <michael@adl-crm.uk>
2017-04-10 13:19:36 +0100

    Correct code for Training POD

--- Life/models/pad/TRAINING/TodayPAD.php
+++ Life/models/pad/TRAINING/TodayPAD.php
@@ -8,7 +8,7 @@ class TRAININGTodayPadModal {
         $this->pdo = $pdo;
     }
 
-    public function TRAININGgetTodayPad($TEAM) {
+    public function TRAININGgetTodayPad() {
 
         $stmt = $this->pdo->prepare("SELECT 
     pad_statistics_group,
@@ -23,9 +23,8 @@ class TRAININGTodayPadModal {
 FROM
     pad_statistics
 WHERE
-    pad_statistics_added_date >= CURDATE() AND pad_statistics_group=:team
+    pad_statistics_added_date >= CURDATE() AND pad_statistics_group='POD 6'
 ORDER BY pad_statistics_added_date DESC");
-        $stmt->bindParam(':team', $TEAM, PDO::PARAM_STR);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }

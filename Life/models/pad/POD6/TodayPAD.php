b4b66fbda633804acd2ee6e1e317a864e8724005
Michael <michael@adl-crm.uk>
2017-04-10 17:20:39 +0100

    Update for MySQL date from query

--- Life/models/pad/POD6/TodayPAD.php
+++ Life/models/pad/POD6/TodayPAD.php
@@ -1,5 +1,41 @@
 <?php
 
+if (isset($datafrom)) {  
+    
+    class POD6TodayPadModal {
+
+    protected $pdo;
+
+    public function __construct(PDO $pdo) {
+        $this->pdo = $pdo;
+    }
+
+    public function POD6getTodayPad($datefrom) {
+
+        $stmt = $this->pdo->prepare("SELECT 
+    pad_statistics_group,
+    pad_statistics_id,
+    pad_statistics_lead,
+    pad_statistics_closer,
+    pad_statistics_notes,
+    pad_statistics_status,
+    pad_statistics_col,
+    DATE(pad_statistics_update_date) AS pad_statistics_update_date,
+    pad_statistics_group
+FROM
+    pad_statistics
+WHERE
+     DATE(pad_statistics_update_date)=:datefrom AND pad_statistics_group='POD 6'
+ORDER BY pad_statistics_added_date DESC");
+         $stmt->bindParam(':datefrom', $datefrom, PDO::PARAM_STR);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+}
+    
+} else {
+
 class POD6TodayPadModal {
 
     protected $pdo;
@@ -31,4 +67,6 @@ ORDER BY pad_statistics_added_date DESC");
 
 }
 
+
+}
 ?>

9ea101619081c52047f3cbcdacad74a502edec68
Michael <michael@adl-crm.uk>
2017-04-10 17:00:05 +0100

    Update for MySQL date from query

--- Life/models/pad/CLOSER/TodayPAD.php
+++ Life/models/pad/CLOSER/TodayPAD.php
@@ -1,5 +1,42 @@
 <?php
 
+if (isset($datafrom)) {
+    
+    class CLOSERTodayPadModal {
+
+    protected $pdo;
+
+    public function __construct(PDO $pdo) {
+        $this->pdo = $pdo;
+    }
+
+    public function CLOSERgetTodayPad($datefrom) {
+
+        $stmt = $this->pdo->prepare("SELECT 
+    pad_statistics_group,
+    pad_statistics_id,
+    pad_statistics_lead,
+    pad_statistics_closer,
+    pad_statistics_notes,
+    pad_statistics_status,
+    pad_statistics_col,
+    DATE(pad_statistics_update_date) AS pad_statistics_update_date,
+    pad_statistics_group
+FROM
+    pad_statistics
+WHERE
+    pad_statistics_added_date >= CURDATE() AND pad_statistics_group='Closers'
+ORDER BY pad_statistics_added_date DESC");
+         $stmt->bindParam(':datefrom', $datefrom, PDO::PARAM_STR);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+}
+    
+} else {
+    
+
 class CLOSERTodayPadModal {
 
     protected $pdo;
@@ -31,4 +68,5 @@ ORDER BY pad_statistics_added_date DESC");
 
 }
 
+}
 ?>

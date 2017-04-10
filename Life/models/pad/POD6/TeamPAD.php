55474359f6a712d6e1f5a83603c717ee5d1ab597
Michael <michael@adl-crm.uk>
2017-04-10 17:20:34 +0100

    Update for MySQL date from query

--- Life/models/pad/POD6/TeamPAD.php
+++ Life/models/pad/POD6/TeamPAD.php
@@ -1,5 +1,34 @@
 <?php
 
+if (isset($datafrom)) {  
+    
+    class POD6TeamPadModal {
+
+    protected $pdo;
+
+    public function __construct(PDO $pdo) {
+        $this->pdo = $pdo;
+    }
+
+    public function POD6getTeamPad($datefrom) {
+
+        $stmt = $this->pdo->prepare("SELECT 
+    SUM(pad_statistics_col) AS COMM,
+    AVG(pad_statistics_col) AS AVG,
+    pad_statistics_group
+FROM
+    pad_statistics
+WHERE
+    DATE(pad_statistics_update_date)=:datefrom AND pad_statistics_group='POD 6' GROUP BY pad_statistics_group");
+        $stmt->bindParam(':datefrom', $datefrom, PDO::PARAM_STR);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+}
+    
+} else {
+
 class POD6TeamPadModal {
 
     protected $pdo;
@@ -24,4 +53,5 @@ WHERE
 
 }
 
+}
 ?>

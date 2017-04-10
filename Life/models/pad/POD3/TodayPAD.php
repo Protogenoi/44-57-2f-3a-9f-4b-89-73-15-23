afb224357a8d516c73f62b26395512d958afd169
Michael <michael@adl-crm.uk>
2017-04-10 12:43:05 +0100

    Correct code for POD 3

--- Life/models/pad/POD3/TodayPAD.php
+++ Life/models/pad/POD3/TodayPAD.php
@@ -8,7 +8,7 @@ class POD3TodayPadModal {
         $this->pdo = $pdo;
     }
 
-    public function POD3getTodayPad($TEAM) {
+    public function POD3getTodayPad() {
 
         $stmt = $this->pdo->prepare("SELECT 
     pad_statistics_group,
@@ -23,9 +23,8 @@ class POD3TodayPadModal {
 FROM
     pad_statistics
 WHERE
-    pad_statistics_added_date >= CURDATE() AND pad_statistics_group=:team
+    pad_statistics_added_date >= CURDATE() AND pad_statistics_group='POD 3'
 ORDER BY pad_statistics_added_date DESC");
-        $stmt->bindParam(':team', $TEAM, PDO::PARAM_STR);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }

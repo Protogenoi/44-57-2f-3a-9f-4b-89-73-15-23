ee2ce24364a30de36667db08619228aba7058c3e
Michael <michael@adl-crm.uk>
2017-04-10 14:01:09 +0100

    Correct code for Admin

--- Life/models/pad/ADMIN/TeamPAD.php
+++ Life/models/pad/ADMIN/TeamPAD.php
@@ -8,7 +8,7 @@ class ADMINTeamPadModal {
         $this->pdo = $pdo;
     }
 
-    public function ADMINgetTeamPad($TEAM) {
+    public function ADMINgetTeamPad() {
 
         $stmt = $this->pdo->prepare("SELECT 
     SUM(pad_statistics_col) AS COMM,
@@ -17,8 +17,7 @@ class ADMINTeamPadModal {
 FROM
     pad_statistics
 WHERE
-    pad_statistics_added_date >= CURDATE() AND pad_statistics_group=:team GROUP BY pad_statistics_group");
-        $stmt->bindParam(':team', $TEAM, PDO::PARAM_STR);
+    pad_statistics_added_date >= CURDATE() AND pad_statistics_group='Admin' GROUP BY pad_statistics_group");
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }

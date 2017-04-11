9dfae3f2cbd30b8db7b3714751b72e81d320df9f
Michael <michael@adl-crm.uk>
2017-04-11 14:59:14 +0100

    Added code to check for un-delivered messages

--- Life/SMS/Status.php
+++ Life/SMS/Status.php
@@ -41,7 +41,7 @@ if(isset($fferror)) {
                             
                         $MESSAGE="SMS has been successfully delivered to $CALLID";  
                         
-                            $INSERT = $pdo->prepare("INSERT INTO client_note set client_id=:CID, client_name='ADL Alert', sent_by='Twilio', note_type='SMS Delivered', message=:CALLERID");
+                            $INSERT = $pdo->prepare("INSERT INTO client_note set client_id=:CID, client_name='ADL Alert', sent_by='ADL', note_type='SMS Delivered', message=:CALLERID");
                             $INSERT->bindParam(':CID',$CID, PDO::PARAM_INT);
                             $INSERT->bindParam(':CALLERID',$MESSAGE, PDO::PARAM_STR);
                             $INSERT->execute();
@@ -49,6 +49,37 @@ if(isset($fferror)) {
                         }
                         
                         }
+                        
+                if($MessageStatus=='undelivered') {
+                    
+                    include('../../includes/ADL_PDO_CON.php');
+                    include('../../classes/database_class.php');
+                    
+                    $database = new Database(); 
+                    
+                    $CALLID = preg_replace('~^[0\D]++44|\D++~', '0', $TO);
+                    
+                    $database->query("SELECT client_id FROM client_details WHERE phone_number =:CALLID");
+                    $database->bind(':CALLID', $CALLID);
+                    $database->execute();
+                    $data2=$database->single();
+                    
+                    if(isset($data2['client_id'])) {
+                        $CID=$data2['client_id'];
+                        }
+                        
+                        if ($database->rowCount()>=1) {
+                            
+                        $MESSAGE="SMS has failed to be delivered to $CALLID";  
+                        
+                            $INSERT = $pdo->prepare("INSERT INTO client_note set client_id=:CID, client_name='ADL Alert', sent_by='ADL', note_type='SMS Failed', message=:CALLERID");
+                            $INSERT->bindParam(':CID',$CID, PDO::PARAM_INT);
+                            $INSERT->bindParam(':CALLERID',$MESSAGE, PDO::PARAM_STR);
+                            $INSERT->execute();
+                            
+                        }
+                        
+                        }                        
                         
                         }
                         

116a3ce2bfc0e46e0b90840efce6427290669b87
Michael <michael@adl-crm.uk>
2017-04-18 09:41:05 +0100

    Updated access arrays

--- includes/Access_Levels.php
+++ includes/Access_Levels.php
@@ -17,7 +17,7 @@ $cnquery = $pdo->prepare("select company_name from company_details limit 1");
                                 $Task_Access = array("Michael", "Abbiek", "Victoria","Keith");
                                 $SECRET = array("Michael","Abbiek", "carys","Jakob","Nicola","Tina",'Amy',"Victoria","Christian");
                                 $Agent_Access = array ("111111111");
-                                $Closer_Access = array ("James","Hayley","David","Mike","Kyle","Sarah","Richard","Mike");
+                                $Closer_Access = array ("James","Hayley","David","Mike","Kyle","Sarah","Richard","Mike","Gavin");
 
                                 
                             }

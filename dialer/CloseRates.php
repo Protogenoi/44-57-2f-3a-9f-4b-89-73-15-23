9397d5a69995065e4abd44ad7704731f2a2c3c85
Michael <michael@adl-crm.uk>
2017-04-18 15:44:52 +0100

    Updated code as trackers have gone live

--- dialer/CloseRates.php
+++ dialer/CloseRates.php
@@ -26,7 +26,7 @@
                 url: ' ',
                 type: 'POST',
                 success: function (results) {
-                    jQuery(".container").html(results);
+                    jQuery(".contain-to-grid").html(results);
                 }
             });
         }
@@ -124,137 +124,99 @@ ORDER BY Sales, Leads");
             ?>
         </table>
 
-        <?php $STAT_QRY = $pdo->prepare("SELECT 
-    closer,
-    COUNT(IF(sale = 'QML',
-        1,
-        NULL)) AS QML,
-    COUNT(IF(sale = 'QQQ',
-        1,
-        NULL)) AS QQQ,
-    COUNT(IF(sale = 'QCBK',
-        1,
-        NULL)) AS QCBK,
-    COUNT(IF(sale = 'DIDNO',
-        1,
-        NULL)) AS DIDNO,
-    COUNT(IF(sale = 'QNQ',
-        1,
-        NULL)) AS QNQ,
-    COUNT(IF(sale = 'QUN',
-        1,
-        NULL)) AS QUN,
-    COUNT(IF(sale = 'QDE'
-            OR sale = 'DEC',
-        1,
-        NULL)) AS QDE,
-    COUNT(IF(sale = 'NoCard',
-        1,
-        NULL)) AS NoCard,
-    COUNT(IF(sale = 'SALE',
-        1,
-        NULL)) AS Sales,
-    COUNT(IF(sale NOT IN ('SALE' , 'NoCard',
-            'QDE',
-            'DEC',
-            'QUN',
-            'QNQ',
-            'DIDNO',
-            'QCBK',
-            'QQQ',
-            'QML'),
-        1,
-        NULL)) AS Other
-FROM
-    closer_trackers
-WHERE
-date_added > DATE(NOW())
-GROUP BY closer
-ORDER BY closer"); ?>
 
-        <table id='main2' cellspacing='0' cellpadding='10'>
-            <th class='status_PAUSED'>Closer</th>
-            <th class='status_PAUSED'>Sales</th>
-            <th class='status_PAUSED'>Quote Mortgage Lead</th>
-            <th class='status_PAUSED'>Quoted</th>
-            <th class='status_PAUSED'>Quote Callback</th>
-            <th class='status_PAUSED'>Didn't Beat</th>
-            <th class='status_PAUSED'>No Quote</th>
-            <th class='status_PAUSED'>Underwritten</th>
-            <th class='status_PAUSED'>Decline</th>
-            <th class='status_PAUSED'>No Card</th>
-            <th class='status_PAUSED'>Other</th>
+        <?php
+        $TRACKER = $pdo->prepare("SELECT date_updated, tracker_id, agent, closer, client, current_premium, our_premium, comments, sale, date_updated FROM closer_trackers WHERE date_updated >= CURDATE() ORDER BY date_added DESC LIMIT 5");
+
+        $TRACKER->execute();
+        $i = 0;
+
+        if ($TRACKER->rowCount() > 0) {
+            ?>
+
+            <table id="tracker" class="table" id="users">
+                <thead>
+                    <tr>
+                        <th>Row</th>
+                        <th>Closer</th>
+                        <th>Agent</th>
+                        <th>Client</th>
+                        <th>Current Premium</th>
+                        <th>Our Premium</th>
+                        <th>Notes</th>
+                        <th>DISPO</th>
+                    </tr>
+                </thead> <?php
+                while ($TRACKERresult = $TRACKER->fetch(PDO::FETCH_ASSOC)) {
+
+                    $i++;
+
+                    $TRK_tracker_id = $TRACKERresult['tracker_id'];
+                    $TRK_agent = $TRACKERresult['agent'];
+                    $TRK_closer = $TRACKERresult['closer'];
+                    $TRK_client = $TRACKERresult['client'];
+
+                    $TRK_current_premium = $TRACKERresult['current_premium'];
+                    $TRK_our_premium = $TRACKERresult['our_premium'];
+                    $TRK_comments = $TRACKERresult['comments'];
+                    $TRK_sale = $TRACKERresult['sale'];
+                    
+                         switch ($TRK_sale) {
+                                                        case "QCBK":
+                                                            $TRK_sale="Quoted Callback";
+                                                            $TRK_BG="#cc99ff";
+                                                            break;
+                                                        case "SALE":
+                                                              $TRK_sale="SALE";
+                                                            $TRK_BG="#00ff00";
+                                                            break;
+                                                        case "QQQ":
+                                                               $TRK_sale="Quoted";
+                                                            $TRK_BG="#66ccff";
+                                                            break;
+                                                        case "NoCard":
+                                                               $TRK_sale="No Card Details";
+                                                            $TRK_BG="##cc0000";
+                                                            break;
+                                                        case "QUN":
+                                                               $TRK_sale="Underwritten";
+                                                            $TRK_BG="#ff0066";
+                                                            break;
+                                                        case "QNQ":
+                                                            $TRK_sale="No Quote";
+                                                            $TRK_BG="#ffcc00";
+                                                            break;
+                                                        case "DIDNO":
+                                                            $TRK_sale="Quote Not Beaten";
+                                                            $TRK_BG="#ff6600";
+                                                            break;
+                                                            case "QDE":
+                                                            $TRK_sale="Quote Mortgage Lead";
+                                                            $TRK_BG="#669900";
+                                                            break;
+                                                    
+                                                        default:
+                                                           $TRK_sale=$TRK_sale;
+                                                            $TRK_BG="#ffffff";
+                                                    }
+                    ?>
+
+                    <tr>
+                        <td bgcolor="#ffffff"> <?php echo $i; ?></td>
+                        <td bgcolor="#ffffff"> <strong style="font-size: 40px;"> <?php echo $TRK_closer; ?></strong></td>
+                        <td bgcolor="#ffffff"><strong style="font-size: 40px;"><?php echo $TRK_agent; ?></strong></td>
+                        <td bgcolor="#ffffff"><strong><?php echo $TRK_client; ?></strong></td>
+                        <td bgcolor="#ffffff"><strong><?php echo $TRK_current_premium; ?></strong></td>
+                        <td bgcolor="#ffffff"><strong><?php echo $TRK_our_premium; ?></strong></td>
+                        <td bgcolor="#ffffff"><strong style="font-size: 40px;"><?php echo $TRK_comments; ?></strong></td>
+                        <td bgcolor="<?php echo $TRK_BG; ?>"><strong><?php echo $TRK_sale; ?></strong></td>
+
+                    <?php }
+                }
+                ?>          
+        </table> 
 
 
-<?php
-$STAT_QRY->execute();
-if ($STAT_QRY->rowCount() > 0) {
-    while ($STAT_result = $STAT_QRY->fetch(PDO::FETCH_ASSOC)) {
-
-        $QML = $STAT_result['QML'];
-        $QQQ = $STAT_result['QQQ'];
-        $QCBK = $STAT_result['QCBK'];
-        $DIDNO = $STAT_result['DIDNO'];
-        $QNQ = $STAT_result['QNQ'];
-        $QUN = $STAT_result['QUN'];
-        $QDE = $STAT_result['QDE'];
-        $NoCard = $STAT_result['NoCard'];
-        $OTHER = $STAT_result['Other'];
-
-        $Sales = $STAT_result['Sales'];
-        $Leads = $STAT_result['Leads'];
-        $CLOSER_NAME = $STAT_result['closer'];
-
-        switch ($CLOSER_NAME) {
-            case("Matt"):
-                $LEADS = $Leads - 0;
-                $SALES = $Sales + 0;
-                break;
-            case("Richard"):
-                $LEADS = $Leads - 0;
-                $SALES = $Sales + 0;
-                break;
-            case("Kyle"):
-                $LEADS = $Leads - 0;
-                $SALES = $Sales + 0;
-                break;
-            case("Sarah"):
-                $LEADS = $Leads - 0;
-                $SALES = $Sales + 0;
-                break;
-            case("Gavin"):
-                $LEADS = $Leads - 0;
-                $SALES = $Sales + 0;
-                break;
-            case("James"):
-                $LEADS = $Leads - 0;
-                $SALES = $Sales + 0;
-                break;
-            case("Ricky"):
-                $LEADS = $Leads - 0;
-                $SALES = $Sales + 0;
-                break;
-            default:
-                $LEADS = $Leads;
-                $SALES = $Sales;
-                break;
-        }
-
-        echo '<tr><td class="status_READY10"><strong style="font-size: 50px;">' . $CLOSER_NAME . ' </strong></td>';
-        echo '<td class="status_PAUSED12"><strong style="font-size: 50px;">' . $SALES . '</strong></td>';
-        echo '<td class="status_PAUSED12"><strong style="font-size: 50px;">' . $QML . '</strong></td>';
-        echo '<td class="status_PAUSED12"><strong style="font-size: 50px;">' . $QQQ . '</strong></td>';
-        echo '<td class="status_PAUSED12"><strong style="font-size: 50px;">' . $QCBK . '</strong></td>';
-        echo '<td class="status_PAUSED12"><strong style="font-size: 50px;">' . $DIDNO . '</strong></td>';
-        echo '<td class="status_PAUSED12"><strong style="font-size: 50px;">' . $QNQ . '</strong></td><';
-        echo '<td class="status_PAUSED12"><strong style="font-size: 50px;">' . $QUN . '</strong></td>';
-        echo '<td class="status_PAUSED12"><strong style="font-size: 50px;">' . $QDE . '</strong></td>';
-        echo '<td class="status_PAUSED12"><strong style="font-size: 50px;">' . $NoCard . '</strong></td>';
-        echo '<td class="status_PAUSED12"><strong style="font-size: 50px;">' . $OTHER . '</strong></td>';
-    }
-}
-?>
-            <</table>
     </div>
 
 </body>

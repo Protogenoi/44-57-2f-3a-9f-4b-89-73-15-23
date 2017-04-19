40270240786adce86a1fe4612f143d3ee645a3db
Michael <michael@adl-crm.uk>
2017-04-19 17:47:52 +0100

    Updated code for POD 5

--- Life/models/pad/POD5/TeamPAD.php
+++ Life/models/pad/POD5/TeamPAD.php
@@ -1,56 +1,43 @@
 <?php
 
-if (isset($datafrom)) {  
-    
     class POD5TeamPadModal {
 
-    protected $pdo;
+        protected $pdo;
 
-    public function __construct(PDO $pdo) {
-        $this->pdo = $pdo;
-    }
+        public function __construct(PDO $pdo) {
+            $this->pdo = $pdo;
+        }
 
-    public function POD5getTeamPad($datefrom) {
-
-        $stmt = $this->pdo->prepare("SELECT 
+        public function POD5getTeamPad($datefrom) {
+if (isset($datefrom)) {
+            $stmt = $this->pdo->prepare("SELECT 
     SUM(pad_statistics_col) AS COMM,
     AVG(pad_statistics_col) AS AVG,
     pad_statistics_group
 FROM
     pad_statistics
 WHERE
-    DATE(pad_statistics_update_date)=:datefrom AND pad_statistics_group='POD 5' GROUP BY pad_statistics_group");
-        $stmt->bindParam(':datefrom', $datefrom, PDO::PARAM_STR);
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
-    
-} } else {
+   DATE(pad_statistics_added_date)=:datefrom AND pad_statistics_group='POD 5'");
+            $stmt->bindParam(':datefrom', $datefrom, PDO::PARAM_STR);
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+        
+        if (!isset($datefrom)) {
 
-class POD5TeamPadModal {
-
-    protected $pdo;
-
-    public function __construct(PDO $pdo) {
-        $this->pdo = $pdo;
-    }
-
-    public function POD5getTeamPad() {
-
-        $stmt = $this->pdo->prepare("SELECT 
+            $stmt = $this->pdo->prepare("SELECT 
     SUM(pad_statistics_col) AS COMM,
     AVG(pad_statistics_col) AS AVG,
     pad_statistics_group
 FROM
     pad_statistics
 WHERE
-    pad_statistics_added_date >= CURDATE() AND pad_statistics_group='POD 5' GROUP BY pad_statistics_group");
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    pad_statistics_added_date >= CURDATE() AND pad_statistics_group='POD 5'");
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+
+}
     }
-
-}
-
-}
-
 ?>

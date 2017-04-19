2ba3a92cabb64e80b3122a7b42b25484810f113b
Michael <michael@adl-crm.uk>
2017-04-19 13:47:01 +0100

    Fixed code for date search

--- Life/models/pad/POD1/TeamPAD.php
+++ Life/models/pad/POD1/TeamPAD.php
@@ -1,34 +1,5 @@
 <?php
 
-if (isset($datafrom)) {
-    
-    class POD1TeamPadModal {
-
-    protected $pdo;
-
-    public function __construct(PDO $pdo) {
-        $this->pdo = $pdo;
-    }
-
-    public function POD1getTeamPad($datefrom) {
-
-        $stmt = $this->pdo->prepare("SELECT 
-    SUM(pad_statistics_col) AS COMM,
-    AVG(pad_statistics_col) AS AVG,
-    pad_statistics_group
-FROM
-    pad_statistics
-WHERE
-    DATE(pad_statistics_added_date)=:datefrom AND pad_statistics_group='POD 1'");
-        $stmt->bindParam(':datefrom', $datefrom, PDO::PARAM_STR);
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
-
-}
-    
-} else {
-    
 class POD1TeamPadModal {
 
     protected $pdo;
@@ -37,9 +8,23 @@ class POD1TeamPadModal {
         $this->pdo = $pdo;
     }
 
-    public function POD1getTeamPad() {
+    public function POD1getTeamPad($datefrom) {
+        if (isset($datafrom)) {
+            $stmt = $this->pdo->prepare("SELECT 
+    SUM(pad_statistics_col) AS COMM,
+    AVG(pad_statistics_col) AS AVG,
+    pad_statistics_group
+FROM
+    pad_statistics
+WHERE
+    DATE(pad_statistics_added_date)=:datefrom AND pad_statistics_group='POD 1'");
+            $stmt->bindParam(':datefrom', $datefrom, PDO::PARAM_STR);
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+        if (!isset($datefrom)) {
 
-        $stmt = $this->pdo->prepare("SELECT 
+            $stmt = $this->pdo->prepare("SELECT 
     SUM(pad_statistics_col) AS COMM,
     AVG(pad_statistics_col) AS AVG,
     pad_statistics_group
@@ -47,10 +32,11 @@ FROM
     pad_statistics
 WHERE
     DATE(pad_statistics_added_date) >= CURDATE() AND pad_statistics_group='POD 1'");
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
     }
 
 }
-}
+
 ?>

7c2daf0a7425d922517f63282b1b9bbf0be90d56
Michael <michael@adl-crm.uk>
2017-04-19 17:32:36 +0100

    Updated code for POD 2

--- Life/views/pad/POD2/Team-PAD.php
+++ Life/views/pad/POD2/Team-PAD.php
@@ -1,22 +1,29 @@
-<?php foreach ($POD2TeamPadList as $POD2Team_Pad): ?>
+                                <table  class="table table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th>POD 2 Statistics</th>
+                                        </tr>
+                                        <tr>
+                                            <th>Team</th>
+                                            <th>TOTAL</th>
+                                        </tr>
+                                    </thead>
+<?php foreach ($TeamPadList as $Team_Pad):?>
 
     <?php
-    $POD_COMM2 = number_format($POD2Team_Pad['COMM'], 2);
-    $POD_AVG2 = number_format($POD2Team_Pad['AVG'], 2);
-    $POD_TEAM2 = $POD2Team_Pad['pad_statistics_group'];
+    $POD_COMM = number_format($Team_Pad['COMM'], 2);
+    $POD_AVG = number_format($Team_Pad['AVG'], 2);
+    $POD_TEAM = $Team_Pad['pad_statistics_group'];
     ?>
     <tr>
-        <td><?php echo $POD_TEAM2; ?></td>
-                <td><input size="8" disabled class="form-control" type="currency" name="POD_AVG" value="<?php
-                   if (isset($POD_AVG2)) {
-                       echo "£$POD_AVG2";
-                   }
-                   ?>"></td>
+        <td><?php echo $POD_TEAM; ?></td>
         <td><input size="8" disabled class="form-control" type="currency" name="POD_COMM" value="<?php
-                   if (isset($POD_COMM2)) {
-                       echo "£$POD_COMM2";
-                   }
-                   ?>"></td></tr>
+                   if (isset($POD_COMM)) {
+                   echo "£$POD_COMM (Avg £$POD_AVG)";}
+                   
+                   ?>"></td>
+    
+    </tr>
 
 <?php endforeach ?>
-        
+                                </table>

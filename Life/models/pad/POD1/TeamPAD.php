7bca8e283a34577f24bc35127228f9a68ee52fb2
Michael <michael@adl-crm.uk>
2017-04-19 15:54:54 +0100

    Fixed code for POD 1

--- Life/models/pad/POD1/TeamPAD.php
+++ Life/models/pad/POD1/TeamPAD.php
@@ -1,15 +1,15 @@
 <?php
 
-class POD1TeamPadModal {
+    class POD1TeamPadModal {
 
-    protected $pdo;
+        protected $pdo;
 
-    public function __construct(PDO $pdo) {
-        $this->pdo = $pdo;
-    }
+        public function __construct(PDO $pdo) {
+            $this->pdo = $pdo;
+        }
 
-    public function POD1getTeamPad($datefrom) {
-        if (isset($datafrom)) {
+        public function POD1getTeamPad($datefrom) {
+if (isset($datefrom)) {
             $stmt = $this->pdo->prepare("SELECT 
     SUM(pad_statistics_col) AS COMM,
     AVG(pad_statistics_col) AS AVG,
@@ -17,11 +17,12 @@ class POD1TeamPadModal {
 FROM
     pad_statistics
 WHERE
-    DATE(pad_statistics_added_date)=:datefrom AND pad_statistics_group='POD 1'");
+   DATE(pad_statistics_added_date)=:datefrom AND pad_statistics_group='POD 1'");
             $stmt->bindParam(':datefrom', $datefrom, PDO::PARAM_STR);
             $stmt->execute();
             return $stmt->fetchAll(PDO::FETCH_ASSOC);
         }
+        
         if (!isset($datefrom)) {
 
             $stmt = $this->pdo->prepare("SELECT 
@@ -31,12 +32,12 @@ WHERE
 FROM
     pad_statistics
 WHERE
-    DATE(pad_statistics_added_date) >= CURDATE() AND pad_statistics_group='POD 1'");
+    pad_statistics_added_date >= CURDATE() AND pad_statistics_group='POD 1'");
             $stmt->execute();
             return $stmt->fetchAll(PDO::FETCH_ASSOC);
         }
-    }
+
 
 }
-
+    }
 ?>

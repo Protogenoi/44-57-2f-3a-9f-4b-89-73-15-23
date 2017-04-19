2a354c532a7eba6c6566fb986646dabd2d1b7a42
Michael <michael@adl-crm.uk>
2017-04-19 17:47:57 +0100

    Updated code for POD 5

--- Life/models/pad/POD5/TodayPAD.php
+++ Life/models/pad/POD5/TodayPAD.php
@@ -1,17 +1,35 @@
 <?php
 
-if (isset($datafrom)) {  
-    
     class POD5TodayPadModal {
 
-    protected $pdo;
+        protected $pdo;
 
-    public function __construct(PDO $pdo) {
-        $this->pdo = $pdo;
-    }
+        public function __construct(PDO $pdo) {
+            $this->pdo = $pdo;
+        }
 
-    public function POD5getTodayPad($datefrom) {
+        public function POD5getTodayPad($datefrom) {
+if (isset($datefrom)) {
+            $stmt = $this->pdo->prepare("SELECT 
+    pad_statistics_group,
+    pad_statistics_id,
+    pad_statistics_lead,
+    pad_statistics_closer,
+    pad_statistics_notes,
+    pad_statistics_status,
+    pad_statistics_col,
+    DATE(pad_statistics_update_date) AS pad_statistics_update_date
+FROM
+    pad_statistics
+WHERE
+    DATE(pad_statistics_added_date)=:datefrom AND pad_statistics_group='POD 5'");
+            $stmt->bindParam(':datefrom', $datefrom, PDO::PARAM_STR);
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+if (!isset($datefrom)) {
 
+    
         $stmt = $this->pdo->prepare("SELECT 
     pad_statistics_group,
     pad_statistics_id,
@@ -20,54 +38,16 @@ if (isset($datafrom)) {
     pad_statistics_notes,
     pad_statistics_status,
     pad_statistics_col,
-    DATE(pad_statistics_update_date) AS pad_statistics_update_date,
-    pad_statistics_group
+    DATE(pad_statistics_update_date) AS pad_statistics_update_date
 FROM
     pad_statistics
 WHERE
-    DATE(pad_statistics_update_date)=:datefrom AND pad_statistics_group='POD 5'
-ORDER BY pad_statistics_added_date DESC");
-        $stmt->bindParam(':datefrom', $datefrom, PDO::PARAM_STR);
+    pad_statistics_added_date >= CURDATE() AND pad_statistics_group='POD 5'");
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
 }
-    
-} else {
-    
-
-class POD5TodayPadModal {
-
-    protected $pdo;
-
-    public function __construct(PDO $pdo) {
-        $this->pdo = $pdo;
     }
-
-    public function POD5getTodayPad() {
-
-        $stmt = $this->pdo->prepare("SELECT 
-    pad_statistics_group,
-    pad_statistics_id,
-    pad_statistics_lead,
-    pad_statistics_closer,
-    pad_statistics_notes,
-    pad_statistics_status,
-    pad_statistics_col,
-    DATE(pad_statistics_update_date) AS pad_statistics_update_date,
-    pad_statistics_group
-FROM
-    pad_statistics
-WHERE
-    pad_statistics_added_date >= CURDATE() AND pad_statistics_group='POD 5'
-ORDER BY pad_statistics_added_date DESC");
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
-
-}
-
-}
 
 ?>

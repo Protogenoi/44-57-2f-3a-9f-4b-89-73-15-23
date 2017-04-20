ae678b2e4a25658d315015164a0fa19af970883d
Michael <michael@adl-crm.uk>
2017-04-20 11:49:15 +0100

    Corrected PAD code for TRAINING group

--- Life/views/pad/TRAINING/Team-PAD.php
+++ Life/views/pad/TRAINING/Team-PAD.php
@@ -1,22 +1,29 @@
-<?php foreach ($TRAININGTeamPadList as $TRAININGTeam_Pad): ?>
+                                <table  class="table table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th>Training Statistics</th>
+                                        </tr>
+                                        <tr>
+                                            <th>Team</th>
+                                            <th>TOTAL</th>
+                                        </tr>
+                                    </thead>
+<?php foreach ($TeamPadList as $Team_Pad):?>
 
     <?php
-    $TRAINING_COMM = number_format($TRAININGTeam_Pad['COMM'], 2);
-    $TRAINING_AVG = number_format($TRAININGTeam_Pad['AVG'], 2);
-    $TRAINING_TEAM = $TRAININGTeam_Pad['pad_statistics_group'];
+    $POD_COMM = number_format($Team_Pad['COMM'], 2);
+    $POD_AVG = number_format($Team_Pad['AVG'], 2);
+    $POD_TEAM = $Team_Pad['pad_statistics_group'];
     ?>
     <tr>
-        <td><?php echo $TRAINING_TEAM; ?></td>
-                <td><input size="8" disabled class="form-control" type="currency" name="TRAINING_AVG" value="<?php
-                   if (isset($TRAINING_AVG)) {
-                       echo "£$TRAINING_AVG";
-                   }
+        <td><?php echo $POD_TEAM; ?></td>
+        <td><input size="8" disabled class="form-control" type="currency" name="POD_COMM" value="<?php
+                   if (isset($POD_COMM)) {
+                   echo "£$POD_COMM (Avg £$POD_AVG)";}
+                   
                    ?>"></td>
-        <td><input size="8" disabled class="form-control" type="currency" name="TRAINING_COMM" value="<?php
-                   if (isset($TRAINING_COMM)) {
-                       echo "£$TRAINING_COMM";
-                   }
-                   ?>"></td></tr>
+    
+    </tr>
 
 <?php endforeach ?>
-        
+                                </table>

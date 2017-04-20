c4480cb146d7ac11e6d5835cc0ef9bc2b5d90c8b
Michael <michael@adl-crm.uk>
2017-04-20 12:17:25 +0100

    Corrected code for closers

--- Life/models/pad/CLOSER/TodayPAD.php
+++ Life/models/pad/CLOSER/TodayPAD.php
@@ -1,17 +1,35 @@
 <?php
 
-if (isset($datafrom)) {
-    
     class CLOSERTodayPadModal {
 
-    protected $pdo;
+        protected $pdo;
 
-    public function __construct(PDO $pdo) {
-        $this->pdo = $pdo;
-    }
+        public function __construct(PDO $pdo) {
+            $this->pdo = $pdo;
+        }
 
-    public function CLOSERgetTodayPad($datefrom) {
+        public function CLOSERgetTodayPad($datefrom) {
+if (isset($datefrom)) {
+            $stmt = $this->pdo->prepare("SELECT 
+    pad_statistics_group,
+    pad_statistics_id,
+    pad_statistics_lead,
+    pad_statistics_closer,
+    pad_statistics_notes,
+    pad_statistics_status,
+    pad_statistics_col,
+    DATE(pad_statistics_update_date) AS pad_statistics_update_date
+FROM
+    pad_statistics
+WHERE
+    DATE(pad_statistics_added_date)=:datefrom AND pad_statistics_group='Closers'");
+            $stmt->bindParam(':datefrom', $datefrom, PDO::PARAM_STR);
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+if (!isset($datefrom)) {
 
+    
         $stmt = $this->pdo->prepare("SELECT 
     pad_statistics_group,
     pad_statistics_id,
@@ -20,53 +38,16 @@ if (isset($datafrom)) {
     pad_statistics_notes,
     pad_statistics_status,
     pad_statistics_col,
-    DATE(pad_statistics_update_date) AS pad_statistics_update_date,
-    pad_statistics_group
+    DATE(pad_statistics_update_date) AS pad_statistics_update_date
 FROM
     pad_statistics
 WHERE
-    pad_statistics_added_date >= CURDATE() AND pad_statistics_group='Closers'
-ORDER BY pad_statistics_added_date DESC");
-         $stmt->bindParam(':datefrom', $datefrom, PDO::PARAM_STR);
+    pad_statistics_added_date >= CURDATE() AND pad_statistics_group='Closers'");
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
 }
-    
-} else {
-    
-
-class CLOSERTodayPadModal {
-
-    protected $pdo;
-
-    public function __construct(PDO $pdo) {
-        $this->pdo = $pdo;
     }
 
-    public function CLOSERgetTodayPad() {
-
-        $stmt = $this->pdo->prepare("SELECT 
-    pad_statistics_group,
-    pad_statistics_id,
-    pad_statistics_lead,
-    pad_statistics_closer,
-    pad_statistics_notes,
-    pad_statistics_status,
-    pad_statistics_col,
-    DATE(pad_statistics_update_date) AS pad_statistics_update_date,
-    pad_statistics_group
-FROM
-    pad_statistics
-WHERE
-    pad_statistics_added_date >= CURDATE() AND pad_statistics_group='Closers'
-ORDER BY pad_statistics_added_date DESC");
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
-
-}
-
-}
 ?>

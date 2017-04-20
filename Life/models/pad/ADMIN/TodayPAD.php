c3f2012c4e00b6a651a9b769eb1e88a879ef5aaf
Michael <michael@adl-crm.uk>
2017-04-20 12:35:52 +0100

    Corrected code for admin

--- Life/models/pad/ADMIN/TodayPAD.php
+++ Life/models/pad/ADMIN/TodayPAD.php
@@ -1,7 +1,5 @@
 <?php
 
-if (isset($datafrom)) {
-
     class ADMINTodayPadModal {
 
         protected $pdo;
@@ -11,7 +9,7 @@ if (isset($datafrom)) {
         }
 
         public function ADMINgetTodayPad($datefrom) {
-
+if (isset($datefrom)) {
             $stmt = $this->pdo->prepare("SELECT 
     pad_statistics_group,
     pad_statistics_id,
@@ -20,33 +18,19 @@ if (isset($datafrom)) {
     pad_statistics_notes,
     pad_statistics_status,
     pad_statistics_col,
-    DATE(pad_statistics_update_date) AS pad_statistics_update_date,
-    pad_statistics_group
+    DATE(pad_statistics_update_date) AS pad_statistics_update_date
 FROM
     pad_statistics
 WHERE
-    DATE(pad_statistics_added_date)=:datefrom AND pad_statistics_group='Admin'
-ORDER BY pad_statistics_added_date DESC");
+    DATE(pad_statistics_added_date)=:datefrom AND pad_statistics_group='Admin'");
             $stmt->bindParam(':datefrom', $datefrom, PDO::PARAM_STR);
             $stmt->execute();
             return $stmt->fetchAll(PDO::FETCH_ASSOC);
         }
+if (!isset($datefrom)) {
 
-    }
-
-} else {
-
-    class ADMINTodayPadModal {
-
-        protected $pdo;
-
-        public function __construct(PDO $pdo) {
-            $this->pdo = $pdo;
-        }
-
-        public function ADMINgetTodayPad() {
-
-            $stmt = $this->pdo->prepare("SELECT 
+    
+        $stmt = $this->pdo->prepare("SELECT 
     pad_statistics_group,
     pad_statistics_id,
     pad_statistics_lead,
@@ -54,18 +38,16 @@ ORDER BY pad_statistics_added_date DESC");
     pad_statistics_notes,
     pad_statistics_status,
     pad_statistics_col,
-    DATE(pad_statistics_update_date) AS pad_statistics_update_date,
-    pad_statistics_group
+    DATE(pad_statistics_update_date) AS pad_statistics_update_date
 FROM
     pad_statistics
 WHERE
-    pad_statistics_added_date >= CURDATE() AND pad_statistics_group='Admin'
-ORDER BY pad_statistics_added_date DESC");
-            $stmt->execute();
-            return $stmt->fetchAll(PDO::FETCH_ASSOC);
-        }
-
+    pad_statistics_added_date >= CURDATE() AND pad_statistics_group='Admin'");
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
 }
+    }
+
 ?>

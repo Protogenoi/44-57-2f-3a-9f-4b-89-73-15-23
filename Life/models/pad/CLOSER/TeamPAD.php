ed240c6a5a97450cd5dfcdc079e33ad4e09e21c5
Michael <michael@adl-crm.uk>
2017-04-20 12:17:22 +0100

    Corrected code for closers

--- Life/models/pad/CLOSER/TeamPAD.php
+++ Life/models/pad/CLOSER/TeamPAD.php
@@ -1,58 +1,43 @@
 <?php
 
-if (isset($datafrom)) {
-    
     class CLOSERTeamPadModal {
 
-    protected $pdo;
+        protected $pdo;
 
-    public function __construct(PDO $pdo) {
-        $this->pdo = $pdo;
-    }
+        public function __construct(PDO $pdo) {
+            $this->pdo = $pdo;
+        }
 
-    public function CLOSERgetTeamPad($datefrom) {
-
-        $stmt = $this->pdo->prepare("SELECT 
+        public function CLOSERgetTeamPad($datefrom) {
+if (isset($datefrom)) {
+            $stmt = $this->pdo->prepare("SELECT 
     SUM(pad_statistics_col) AS COMM,
     AVG(pad_statistics_col) AS AVG,
     pad_statistics_group
 FROM
     pad_statistics
 WHERE
-    pad_statistics_added_date=:datefrom AND pad_statistics_group='Closers' GROUP BY pad_statistics_group");
-        $stmt->bindParam(':datefrom', $datefrom, PDO::PARAM_STR);
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
+   DATE(pad_statistics_added_date)=:datefrom AND pad_statistics_group='Closers'");
+            $stmt->bindParam(':datefrom', $datefrom, PDO::PARAM_STR);
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+        
+        if (!isset($datefrom)) {
 
-}
-    
-} else {
-
-class CLOSERTeamPadModal {
-
-    protected $pdo;
-
-    public function __construct(PDO $pdo) {
-        $this->pdo = $pdo;
-    }
-
-    public function CLOSERgetTeamPad() {
-
-        $stmt = $this->pdo->prepare("SELECT 
+            $stmt = $this->pdo->prepare("SELECT 
     SUM(pad_statistics_col) AS COMM,
     AVG(pad_statistics_col) AS AVG,
     pad_statistics_group
 FROM
     pad_statistics
 WHERE
-    pad_statistics_added_date >= CURDATE() AND pad_statistics_group='Closers' GROUP BY pad_statistics_group");
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    pad_statistics_added_date >= CURDATE() AND pad_statistics_group='Closers'");
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+
+}
     }
-
-}
-
-}
-
 ?>

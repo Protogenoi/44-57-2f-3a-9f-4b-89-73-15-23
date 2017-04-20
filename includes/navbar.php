358c0a5465d2f91694bb9888087a5557b41983ed
Michael <michael@adl-crm.uk>
2017-04-20 14:29:18 +0100

    Added access_user_class to navbar to stop errors

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,4 +1,9 @@
 <?php
+require_once(__DIR__ . '../../classes/access_user/access_user_class.php');
+$page_protect = new Access_user;
+$page_protect->access_page($_SERVER['PHP_SELF'], "", 0);
+$hello_name = ($page_protect->user_full_name != "") ? $page_protect->user_full_name : $page_protect->user;
+
 require_once(__DIR__ . '../../includes/ADL_PDO_CON.php');
 require_once(__DIR__ . '../../includes/adl_features.php');
 

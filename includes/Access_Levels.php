969e353cbdf04c772914642419dbc5365e4fd5c1
Michael <michael@adl-crm.uk>
2017-04-21 09:44:54 +0100

    Changed include to require once

--- includes/Access_Levels.php
+++ includes/Access_Levels.php
@@ -1,56 +1,47 @@
 <?php
 
-include ($_SERVER['DOCUMENT_ROOT']."/includes/ADL_PDO_CON.php");
+require_once(__DIR__ . '../../includes/ADL_PDO_CON.php');
 
 $cnquery = $pdo->prepare("select company_name from company_details limit 1");
-                            $cnquery->execute()or die(print_r($query->errorInfo(), true));
-                            $companydetailsq=$cnquery->fetch(PDO::FETCH_ASSOC);
-                            
-                            $companynamere=$companydetailsq['company_name'];
-                            
-                            if($companynamere=='The Review Bureau') {
-                                $Level_10_Access = array("Michael", "Matt", "leighton","Nick");
-                                $Level_9_Access = array("Michael", "Matt", "leighton","Nick","carys");
-                                $Level_8_Access = array("Michael", "Matt", "leighton", "Nick", "Abbiek", "carys","Tina","Heidy","Nicola","Mike");
-                                $Level_3_Access = array("Michael", "Matt", "leighton", "Nick", "Abbiek", "carys","Jakob","Nicola","Tina",'Heidy','Amy',"Victoria","Christian","Audits","Keith");
-                                $Level_1_Access = array("Michael", "Matt", "leighton", "Nick", "Abbiek", "carys","Jakob","Nicola","Tina",'Heidy','Amy',"Victoria","Christian","Audits");
-                                $Task_Access = array("Michael", "Abbiek", "Victoria");
-                                $SECRET = array("Michael","Abbiek", "carys","Jakob","Nicola","Tina",'Amy',"Victoria","Christian");
-                                $Agent_Access = array ("111111111");
-                                $Closer_Access = array ("James","Hayley","David","Mike","Kyle","Sarah","Richard","Mike","Gavin");
-                                $Manager_Access = array ("Richard","Keith");
-                                $QA_Access = array ("Abbiek", "carys","Jakob","Nicola","Tina","Amy");
-                               
+$cnquery->execute()or die(print_r($query->errorInfo(), true));
+$companydetailsq = $cnquery->fetch(PDO::FETCH_ASSOC);
 
-                                
-                            }
-                            
-if($companynamere=='ADL_CUS') {
-    $Level_10_Access = array("Michael", "Dean", "Helen","Andrew","David");
-    $Level_8_Access = array("Michael", "Dean", "Helen","Andrew","David");
-    $Level_3_Access = array("Michael", "Dean", "Helen","Andrew","David");
-    $Level_1_Access = array("Michael", "Dean", "Helen","Andrew","David");
+$companynamere = $companydetailsq['company_name'];
+
+if ($companynamere == 'The Review Bureau') {
+    $Level_10_Access = array("Michael", "Matt", "leighton", "Nick");
+    $Level_9_Access = array("Michael", "Matt", "leighton", "Nick", "carys");
+    $Level_8_Access = array("Michael", "Matt", "leighton", "Nick", "Abbiek", "carys", "Tina", "Heidy", "Nicola", "Mike");
+    $Level_3_Access = array("Michael", "Matt", "leighton", "Nick", "Abbiek", "carys", "Jakob", "Nicola", "Tina", 'Heidy', 'Amy', "Victoria", "Christian", "Audits", "Keith");
+    $Level_1_Access = array("Michael", "Matt", "leighton", "Nick", "Abbiek", "carys", "Jakob", "Nicola", "Tina", 'Heidy', 'Amy', "Victoria", "Christian", "Audits");
+    $Task_Access = array("Michael", "Abbiek", "Victoria");
+    $SECRET = array("Michael", "Abbiek", "carys", "Jakob", "Nicola", "Tina", 'Amy', "Victoria", "Christian");
+    $Agent_Access = array("111111111");
+    $Closer_Access = array("James", "Hayley", "David", "Mike", "Kyle", "Sarah", "Richard", "Mike", "Gavin");
+    $Manager_Access = array("Richard", "Keith");
+    $QA_Access = array("Abbiek", "carys", "Jakob", "Nicola", "Tina", "Amy");
+}
+
+if ($companynamere == 'ADL_CUS') {
+    $Level_10_Access = array("Michael", "Dean", "Helen", "Andrew", "David");
+    $Level_8_Access = array("Michael", "Dean", "Helen", "Andrew", "David");
+    $Level_3_Access = array("Michael", "Dean", "Helen", "Andrew", "David");
+    $Level_1_Access = array("Michael", "Dean", "Helen", "Andrew", "David");
     $SECRET = array("Michael");
-    $Task_Access = array("Michael", "Dean", "Helen","Andrew","David");
+    $Task_Access = array("Michael", "Dean", "Helen", "Andrew", "David");
+}
 
-}                            
-                            
-                            if($companynamere=='Assura') {
-                                $Level_10_Access = array("Michael");
-                                $Level_8_Access = array("Michael", "Tina","Nathan","Charles");
-                                $Level_3_Access = array("Michael", "Tina","Nathan","Charles");
-                                
-                                $Level_1_Access = array("Michael", "Tina","Nathan","Charles");
-                                
-                            }
-                            
-                            if($companynamere=='HWIFS') {
-                                $Level_10_Access = array("Michael");
-                                $Level_8_Access = array("Michael");
-                                $Level_3_Access = array("Michael");
-                                
-                                $Level_1_Access = array("Michael");
-                                
-                            }
- 
-                            ?>
+if ($companynamere == 'Assura') {
+    $Level_10_Access = array("Michael");
+    $Level_8_Access = array("Michael");
+    $Level_3_Access = array("Michael");
+    $Level_1_Access = array("Michael");
+}
+
+if ($companynamere == 'HWIFS') {
+    $Level_10_Access = array("Michael");
+    $Level_8_Access = array("Michael");
+    $Level_3_Access = array("Michael");
+    $Level_1_Access = array("Michael");
+}
+?>

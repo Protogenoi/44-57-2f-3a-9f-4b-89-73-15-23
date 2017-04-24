b404b45e4f61a00e042e5cbaebe201653a38b0ad
Michael <michael@adl-crm.uk>
2017-04-24 17:58:11 +0100

    Tracking Keyfacts emails

---
+++ Life/JSON/KeyFacts.php
@@ -0,0 +1,66 @@
+<?php
+require_once(__DIR__ . '/../../classes/access_user/access_user_class.php');
+$page_protect = new Access_user;
+$page_protect->access_page($_SERVER['PHP_SELF'], "", 3);
+$hello_name = ($page_protect->user_full_name != "") ? $page_protect->user_full_name : $page_protect->user;
+
+
+$EXECUTE= filter_input(INPUT_GET, 'EXECUTE', FILTER_SANITIZE_NUMBER_INT);
+
+
+
+if(isset($EXECUTE)) {
+    require_once(__DIR__ . '/../../includes/ADL_PDO_CON.php');
+    if($EXECUTE=='1') {
+
+
+        $query = $pdo->prepare("SELECT 
+    keyfactsemail_email,
+    keyfactsemail_added_by,
+    keyfactsemail_added_date
+FROM
+    keyfactsemail
+    ORDER BY keyfactsemail_added_date DESC");
+$query->execute()or die(print_r($query->errorInfo(), true));
+json_encode($results['aaData']=$query->fetchAll(PDO::FETCH_ASSOC));
+
+echo json_encode($results);
+
+
+
+
+    }
+    
+        if($EXECUTE=='2') {
+
+
+        $query = $pdo->prepare("SELECT 
+    client_details.email,
+    client_details.submitted_date,
+    client_policy.closer,
+    client_details.client_id
+FROM
+    client_details
+    JOIN client_policy ON client_details.client_id=client_policy.client_id
+WHERE
+    client_details.submitted_date >= CURDATE()
+        AND client_details.email NOT IN (SELECT 
+            keyfactsemail_email
+        FROM
+            keyfactsemail
+        WHERE
+            keyfactsemail_added_date >= CURDATE())
+    GROUP BY client_details.email ORDER BY client_details.submitted_date DESC");
+$query->execute()or die(print_r($query->errorInfo(), true));
+json_encode($results['aaData']=$query->fetchAll(PDO::FETCH_ASSOC));
+
+echo json_encode($results);
+
+
+
+
+    }
+    
+}
+
+?>

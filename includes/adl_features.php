78760b7d3c756954610446ac2956ba4dafe00927
Michael <michael@adl-crm.uk>
2017-05-03 18:13:37 +0100

    Added new features EWS and Financials for better control

--- includes/adl_features.php
+++ includes/adl_features.php
@@ -1,7 +1,7 @@
 <?php
 require_once(__DIR__ . '/ADL_PDO_CON.php');
 
-$query = $pdo->prepare("SELECT trackers, dealsheets, employee, post_code, pba, error, twitter, gmaps, analytics, callbacks, dialler, intemails, clientemails, keyfactsemail, genemail, recemail, sms, calendar, audits, life, home, pension FROM adl_features LIMIT 1");
+$query = $pdo->prepare("SELECT ews, financials, trackers, dealsheets, employee, post_code, pba, error, twitter, gmaps, analytics, callbacks, dialler, intemails, clientemails, keyfactsemail, genemail, recemail, sms, calendar, audits, life, home, pension FROM adl_features LIMIT 1");
 $query->execute()or die(print_r($query->errorInfo(), true));
 $checkfeatures=$query->fetch(PDO::FETCH_ASSOC);
             
@@ -27,3 +27,5 @@ $checkfeatures=$query->fetch(PDO::FETCH_ASSOC);
             $ffemployee=$checkfeatures['employee'];
             $ffdealsheets=$checkfeatures['dealsheets'];
             $fftrackers=$checkfeatures['trackers'];
+            $ffews=$checkfeatures['ews'];
+            $fffinancials=$checkfeatures['finaicals'];

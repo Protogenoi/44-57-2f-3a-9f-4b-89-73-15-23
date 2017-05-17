ba626857f5e873e211a40a13a39ae68b94c4cdaa
Michael <michael@adl-crm.uk>
2017-05-17 14:44:47 +0100

    Updated MySQL statement to pull correct figures

--- Life/models/financials/LG/TotalExpectedWithDates.php
+++ Life/models/financials/LG/TotalExpectedWithDates.php
@@ -18,7 +18,8 @@ WHERE
     DATE(sale_date) BETWEEN :datefrom AND :dateto
         AND insurer = 'Legal and General'
         AND client_policy.policystatus NOT LIKE '%CANCELLED%'
-        AND client_policy.policystatus NOT IN ('Clawback' , 'DECLINED','On hold','Awaiting')");
+        AND client_policy.policystatus NOT IN ('Clawback' , 'DECLINED','On hold')
+        AND client_policy.policy_number NOT like '%DU%'");
         $stmt->bindParam(':datefrom', $datefrom, PDO::PARAM_STR);
         $stmt->bindParam(':dateto', $dateto, PDO::PARAM_STR);
         $stmt->execute();

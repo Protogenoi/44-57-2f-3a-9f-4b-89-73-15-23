b4135052739356f7c382c20ac35ab7e4bdff6874
Michael <michael@adl-crm.uk>
2017-05-18 15:54:16 +0100

    Removed old code

--- Life/JSON/gettask.php
+++ Life/JSON/gettask.php
@@ -6,8 +6,6 @@ include('../../includes/ADL_MYSQLI_CON.php');
 
 if(isset($agent)) {
 
-    #$sql = "select taskcheck.cyd, taskcheck.cancelled_dd, taskcheck.new_dd, taskcheck.tps, taskcheck.trust, taskcheck.upsells ,client_details.client_id, client_details.submitted_date, CONCAT(client_details.first_name, ' ', client_details.last_name) AS NAME, CONCAT(client_details.first_name2,' ',client_details.last_name2) AS NAME2 from client_details LEFT JOIN taskcheck on taskcheck.client_id = client_details.client_id";
-    #$sql = "select taskcheck.cyd, taskcheck.cancelled_dd, taskcheck.new_dd, taskcheck.tps, taskcheck.trust, taskcheck.upsells ,client_details.client_id, client_details.submitted_date, CONCAT(client_details.first_name, ' ', client_details.last_name) AS NAME, CONCAT(client_details.first_name2,' ',client_details.last_name2) AS NAME2 from client_details LEFT JOIN taskcheck on taskcheck.client_id = client_details.client_id WHERE client_details.client_id NOT IN (SELECT client_id from taskcheck)";
 $sql="SELECT Client_Tasks.id, Client_Tasks.client_id, DATE(Client_Tasks.date_added) AS date_added, Client_Tasks.Task, DATE(Client_Tasks.deadline) as deadline, CURDATE() as today, CONCAT(client_details.title, ' ', client_details.last_name) AS name from Client_Tasks JOIN client_details on Client_Tasks.client_id = client_details.client_id where complete='0' and assigned='$agent'";
 
     $result = mysqli_query($conn, $sql) or die("Error in Selecting " . mysqli_error($conn));

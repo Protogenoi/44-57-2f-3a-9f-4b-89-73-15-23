67b0976b4483d9e2ac5aa34826d2e4b26400bf55
Michael <michael@adl-crm.uk>
2017-05-22 17:51:31 +0100

    Life/Assura updated to $page_protect->access_page(filter_input(INPUT_SERVER,'PHP_SELF', FILTER_SANITIZE_SPECIAL_CHARS), "", 1);

--- Life/Assura/Financial_Reports.php
+++ Life/Assura/Financial_Reports.php
@@ -1,7 +1,7 @@
 <?php 
 include($_SERVER['DOCUMENT_ROOT']."/classes/access_user/access_user_class.php"); 
 $page_protect = new Access_user;
-$page_protect->access_page($_SERVER['PHP_SELF'], "", 7);
+$page_protect->access_page(filter_input(INPUT_SERVER,'PHP_SELF', FILTER_SANITIZE_SPECIAL_CHARS), "", 7);
 $hello_name = ($page_protect->user_full_name != "") ? $page_protect->user_full_name : $page_protect->user;
 
 if($fflife=='0') {

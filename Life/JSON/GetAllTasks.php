a898ca6c3f67e352ec1980b88a1f734902c6f94a
Michael <michael@adl-crm.uk>
2017-05-22 17:51:56 +0100

    Life/JSON updated to $page_protect->access_page(filter_input(INPUT_SERVER,'PHP_SELF', FILTER_SANITIZE_SPECIAL_CHARS), "", 1);

--- Life/JSON/GetAllTasks.php
+++ Life/JSON/GetAllTasks.php
@@ -1,7 +1,7 @@
 <?php
 include($_SERVER['DOCUMENT_ROOT']."/classes/access_user/access_user_class.php"); 
 $test_access_level = new Access_user;
-$test_access_level->access_page($_SERVER['PHP_SELF'], "", 2);
+$test_access_level->access_page(filter_input(INPUT_SERVER,'PHP_SELF', FILTER_SANITIZE_SPECIAL_CHARS), "", 2);
 $hello_name = ($test_access_level->user_full_name != "") ? $test_access_level->user_full_name : $test_access_level->user;
 
 $agent= filter_input(INPUT_GET, 'agent', FILTER_SANITIZE_SPECIAL_CHARS);

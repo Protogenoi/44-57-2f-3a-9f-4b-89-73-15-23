d4ad5e30cb1127f4686d639e5518806cd09cb8c2
Michael <michael@adl-crm.uk>
2017-05-24 12:11:26 +0100

    Updated user access code

--- Life/Templates/TrustGuide.php
+++ Life/Templates/TrustGuide.php
@@ -1,11 +1,21 @@
 <?php
-include($_SERVER['DOCUMENT_ROOT']."/classes/access_user/access_user_class.php"); 
+require_once(__DIR__ . '/../../classes/access_user/access_user_class.php'); 
 $page_protect = new Access_user;
-$page_protect->access_page(filter_input(INPUT_SERVER,'PHP_SELF', FILTER_SANITIZE_SPECIAL_CHARS), "", 1); 
+$page_protect->access_page(filter_input(INPUT_SERVER,'PHP_SELF', FILTER_SANITIZE_SPECIAL_CHARS), "", 2); 
 $hello_name = ($page_protect->user_full_name != "") ? $page_protect->user_full_name : $page_protect->user;
 
-include('../../includes/ADL_PDO_CON.php');
-                            require_once ('../../dompdf-master/autoload.inc.php');
+require_once(__DIR__ . '/../../dompdf-master/autoload.inc.php');
+require_once(__DIR__ . '/../../includes/ADL_PDO_CON.php');
+require_once(__DIR__ . '/../../includes/adlfunctions.php');
+
+if (isset($fferror)) {
+    if ($fferror == '1') {
+        ini_set('display_errors', 1);
+        ini_set('display_startup_errors', 1);
+        error_reporting(E_ALL);
+    }
+}
+                     
                             use Dompdf\Dompdf;
 
 $search= filter_input(INPUT_GET, 'search', FILTER_SANITIZE_SPECIAL_CHARS);

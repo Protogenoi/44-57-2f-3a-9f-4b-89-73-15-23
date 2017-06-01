c856a5611a122bc02cbf418a8d42ad7a4980aed2
Michael <michael@adl-crm.uk>
2017-06-01 17:05:41 +0100

    Cleared up code and added file uploads for compliance call recordings.

--- uploadsubmit.php
+++ uploadsubmit.php
@@ -21,12 +21,151 @@ if (isset($fferror)) {
         error_reporting(E_ALL);
     }
 }
+$EXECUTE= filter_input(INPUT_GET, 'EXECUTE', FILTER_SANITIZE_SPECIAL_CHARS);
+
+    if(isset($EXECUTE)) {
+        if($EXECUTE=='1') {
+            
+            if(isset($_FILES['file'])) {
+            
+            $RECORDING_DATE= filter_input(INPUT_POST, 'RECORDING_DATE', FILTER_SANITIZE_SPECIAL_CHARS);
+  
+            if($_FILES['file']['size'] > 40000000) {
+                header('Location: /Recordings.php?RETURN=UPMAX'); die;
+            }
+         
+  if (in_array($hello_name, $TRB_ACCESS, true)) { 
+    $COMPANY='TRB';
+    $COMPANY_NAME='The Review Bureau';
+    }
+        if (in_array($hello_name, $PFP_ACCESS, true)) { 
+    $COMPANY='PFP';
+    $COMPANY_NAME='Protect Family Plans';
+    }
+        if (in_array($hello_name, $PLL_ACCESS, true)) { 
+    $COMPANY='PLL';
+    $COMPANY_NAME='Protected Life Ltd';
+    }
+        if (in_array($hello_name, $WI_ACCESS, true)) { 
+    $COMPANY='WI';
+    $COMPANY_NAME='We Insure';
+    }
+        if (in_array($hello_name, $TFAC_ACCESS, true)) { 
+    $COMPANY='TFAC';
+    $COMPANY_NAME='The Financial Assessment Centre';
+    }
+        if (in_array($hello_name, $APM_ACCESS, true)) { 
+    $COMPANY='APM';
+    $COMPANY_NAME='Assured Protect and Mortgages';
+    }            
+
+$btnupload= filter_input(INPUT_POST, 'btn-upload', FILTER_SANITIZE_SPECIAL_CHARS);            
+ 
+
+if(isset($btnupload)) {    
+     
+ $file = $COMPANY."-".$_FILES['file']['name'];
+ $file_loc = $_FILES['file']['tmp_name'];
+ $file_size = $_FILES['file']['size'];
+ $file_type = $_FILES['file']['type'];
+ 
+ if (!file_exists("uploads/compliance/recordings/$COMPANY")) {
+    mkdir("uploads/compliance/recordings/$COMPANY", 0777, true);
+}
+
+ $folder="uploads/compliance/recordings/$COMPANY/";
+ 
+ $new_size = $file_size/1024;  
+ $new_file_name = strtolower($file);
+
+ $final_file=str_replace("'","",$new_file_name);
+ $LOCATION="uploads/compliance/recordings/$COMPANY/$final_file";
+ 
+ if(move_uploaded_file($file_loc,$folder.$final_file)) {
+
+     echo "$LOCATION, $hello_name, $RECORDING_DATE, $COMPANY_NAME";
+     
+$UPLOAD = $pdo->prepare("INSERT INTO compliance_recordings set compliance_recordings_company=:COMPANY, compliance_recordings_advisor=:ADVISOR, compliance_recordings_location=:LOCATION, compliance_recordings_recording_date=:DATE");
+$UPLOAD->bindParam(':LOCATION',$LOCATION, PDO::PARAM_STR);
+$UPLOAD->bindParam(':ADVISOR',$hello_name, PDO::PARAM_STR);
+$UPLOAD->bindParam(':DATE',$RECORDING_DATE, PDO::PARAM_STR);
+$UPLOAD->bindParam(':COMPANY',$COMPANY_NAME, PDO::PARAM_STR);
+$UPLOAD->execute();  
+
+ header('Location: /compliance/Recordings.php?RETURN=UPLOAD'); die;
+ }
+
+}
+
+header('Location: /compliance/Recordings.php?RETURN=UPFAIL'); die;
+            }
+            
+            else {
+              header('Location: /compliance/Recordings.php?RETURN=UPNO'); die;  
+            }
+            
+        }
+                if($EXECUTE=='10') {
+            
+            
+$REF= filter_input(INPUT_GET, 'REF', FILTER_SANITIZE_SPECIAL_CHARS);
+$uploadtype= filter_input(INPUT_POST, 'uploadtype', FILTER_SANITIZE_SPECIAL_CHARS);
+$btnupload= filter_input(INPUT_POST, 'btn-upload', FILTER_SANITIZE_SPECIAL_CHARS);            
+
+   if($_FILES['file']['size'] > 40000000) {
+               header('Location: /Staff/ViewEmployee.php?RETURN=FILEUPLOAD&fileuploadedfail=y&?fail&REF='.$REF.'#menu3'); die;
+            }
+
+if(isset($btnupload)) {    
+     
+ $file = $_FILES['file']['name'];
+ $file_loc = $_FILES['file']['tmp_name'];
+ $file_size = $_FILES['file']['size'];
+ $file_type = $_FILES['file']['type'];
+ 
+ if (!file_exists("uploads/employee/$REF")) {
+    mkdir("uploads/employee/$REF", 0777, true);
+}
+
+ $folder="uploads/employee/$REF/";
+ $new_size = $file_size/1024;  
+ $new_file_name = strtolower($file);
+ $final_file=str_replace("'","",$new_file_name);
+ 
+ if(move_uploaded_file($file_loc,$folder.$final_file)) {
+
+$TBL_query = $pdo->prepare("INSERT INTO employee_upload set file=:file, type=:type, size=:size, uploadtype=:uploadtype, employee_id=:REF");
+$TBL_query->bindParam(':REF',$REF, PDO::PARAM_INT);
+$TBL_query->bindParam(':file',$final_file, PDO::PARAM_STR,500);
+$TBL_query->bindParam(':type',$file_type, PDO::PARAM_STR, 100);
+$TBL_query->bindParam(':size',$new_size, PDO::PARAM_STR, 500);
+$TBL_query->bindParam(':uploadtype',$uploadtype, PDO::PARAM_STR, 255);
+$TBL_query->execute();  
+
+
+$message="$final_file ($uploadtype)";
+
+$query = $pdo->prepare("INSERT INTO employee_timeline set employee_id=:REF, added_by=:sent, note_type='File Upload', message=:message");
+$query->bindParam(':REF',$REF, PDO::PARAM_INT);
+$query->bindParam(':sent',$hello_name, PDO::PARAM_STR, 100);
+$query->bindParam(':message',$final_file, PDO::PARAM_STR, 2500);
+$query->execute();
+
+ header('Location: /Staff/ViewEmployee.php?RETURN=FILEUPLOAD&fileuploaded=y&?success&fileupname='.$uploadtype.'&REF='.$REF.'#menu3'); die;
+ }
+
+}
+
+header('Location: /Staff/ViewEmployee.php?RETURN=FILEUPLOAD&fileuploadedfail=y&?fail&REF='.$REF.'#menu3'); die;
+          
+            
+        }
+    }  
 
 $Home= filter_input(INPUT_GET, 'Home', FILTER_SANITIZE_SPECIAL_CHARS);
 
     if(isset($Home)) {
         if($Home=='y') {
-            require_once(__DIR__ . '/includes/ADL_PDO_CON.php');
             
 $CID= filter_input(INPUT_GET, 'CID', FILTER_SANITIZE_SPECIAL_CHARS);
 $uploadtype= filter_input(INPUT_POST, 'uploadtype', FILTER_SANITIZE_SPECIAL_CHARS);
@@ -93,9 +232,6 @@ $life= filter_input(INPUT_GET, 'life', FILTER_SANITIZE_SPECIAL_CHARS);
             if($_FILES['file']['size'] > 40000000) {
                 header('Location: /Life/ViewClient.php?UPLOAD=MAX&search='.$search.'#menu2'); die;
             }
-            
-            require_once(__DIR__ . '/includes/ADL_PDO_CON.php');
-            
 
 $uploadtype= filter_input(INPUT_POST, 'uploadtype', FILTER_SANITIZE_SPECIAL_CHARS);
 $btnupload= filter_input(INPUT_POST, 'btn-upload', FILTER_SANITIZE_SPECIAL_CHARS);            
@@ -149,63 +285,4 @@ header('Location: /Life/ViewClient.php?fileuploadedfail=y&?fail&search='.$search
             
         }
     }   
-    
-$EXECUTE= filter_input(INPUT_GET, 'EXECUTE', FILTER_SANITIZE_SPECIAL_CHARS);
-
-    if(isset($EXECUTE)) {
-        if($EXECUTE=='10') {
-            require_once(__DIR__ . '/includes/ADL_PDO_CON.php');
-            
-$REF= filter_input(INPUT_GET, 'REF', FILTER_SANITIZE_SPECIAL_CHARS);
-$uploadtype= filter_input(INPUT_POST, 'uploadtype', FILTER_SANITIZE_SPECIAL_CHARS);
-$btnupload= filter_input(INPUT_POST, 'btn-upload', FILTER_SANITIZE_SPECIAL_CHARS);            
-
-   if($_FILES['file']['size'] > 40000000) {
-               header('Location: /Staff/ViewEmployee.php?RETURN=FILEUPLOAD&fileuploadedfail=y&?fail&REF='.$REF.'#menu3'); die;
-            }
-
-if(isset($btnupload)) {    
-     
- $file = $_FILES['file']['name'];
- $file_loc = $_FILES['file']['tmp_name'];
- $file_size = $_FILES['file']['size'];
- $file_type = $_FILES['file']['type'];
- 
- if (!file_exists("uploads/employee/$REF")) {
-    mkdir("uploads/employee/$REF", 0777, true);
-}
-
- $folder="uploads/employee/$REF/";
- $new_size = $file_size/1024;  
- $new_file_name = strtolower($file);
- $final_file=str_replace("'","",$new_file_name);
- 
- if(move_uploaded_file($file_loc,$folder.$final_file)) {
-
-$TBL_query = $pdo->prepare("INSERT INTO employee_upload set file=:file, type=:type, size=:size, uploadtype=:uploadtype, employee_id=:REF");
-$TBL_query->bindParam(':REF',$REF, PDO::PARAM_INT);
-$TBL_query->bindParam(':file',$final_file, PDO::PARAM_STR,500);
-$TBL_query->bindParam(':type',$file_type, PDO::PARAM_STR, 100);
-$TBL_query->bindParam(':size',$new_size, PDO::PARAM_STR, 500);
-$TBL_query->bindParam(':uploadtype',$uploadtype, PDO::PARAM_STR, 255);
-$TBL_query->execute();  
-
-
-$message="$final_file ($uploadtype)";
-
-$query = $pdo->prepare("INSERT INTO employee_timeline set employee_id=:REF, added_by=:sent, note_type='File Upload', message=:message");
-$query->bindParam(':REF',$REF, PDO::PARAM_INT);
-$query->bindParam(':sent',$hello_name, PDO::PARAM_STR, 100);
-$query->bindParam(':message',$final_file, PDO::PARAM_STR, 2500);
-$query->execute();
-
- header('Location: /Staff/ViewEmployee.php?RETURN=FILEUPLOAD&fileuploaded=y&?success&fileupname='.$uploadtype.'&REF='.$REF.'#menu3'); die;
- }
-
-}
-
-header('Location: /Staff/ViewEmployee.php?RETURN=FILEUPLOAD&fileuploadedfail=y&?fail&REF='.$REF.'#menu3'); die;
-          
-            
-        }
-    }    
+ ?>

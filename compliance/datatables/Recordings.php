0d7d84062426b855d13080539401522a04bd7f5d
Michael <michael@adl-crm.uk>
2017-06-02 14:18:36 +0100

    Updated access levels.

--- compliance/datatables/Recordings.php
+++ compliance/datatables/Recordings.php
@@ -42,7 +42,7 @@ if(isset($EXECUTE)) {
     $COMPANY='Assured Protect and Mortgages';
     }
     
-            if (in_array($hello_name, $MASTER_ACCESS, true)) { 
+            if (in_array($hello_name, $COM_LVL_10_ACCESS, true)) { 
          $query = $pdo->prepare("SELECT compliance_recordings_status, compliance_recordings_id, compliance_recordings_advisor, compliance_recordings_grade, compliance_recordings_recording_date, compliance_recordings_company FROM compliance_recordings ORDER BY compliance_recordings_recording_date DESC");
         $query->bindParam(':COMPANY', $COMPANY, PDO::PARAM_STR);
         $query->execute()or die(print_r($query->errorInfo(), true));

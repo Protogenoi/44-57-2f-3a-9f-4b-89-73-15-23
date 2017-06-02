1ce5aa081a542688301a52db43830418939d3417
Michael <michael@adl-crm.uk>
2017-06-02 13:43:59 +0100

    Added new notification for completed call audits.

--- compliance/php/notifications.php
+++ compliance/php/notifications.php
@@ -39,6 +39,13 @@ if($RETURN=='UPLOAD') { ?>
     </div>
  <?php   
 }
+if($RETURN=='RECUPDATED') { ?>
+    <div class="notice notice-success">
+        <a href="#" class="close" data-dismiss="alert">&times;</a>
+        <strong>Updated:</strong> Call audit updated!
+    </div>
+ <?php   
+}
 }
 ?>
 

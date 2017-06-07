db30a9955d822da28636859b3509582a1798f3f3
Michael <michael@adl-crm.uk>
2017-06-07 15:43:14 +0100

    Updated for new compliance parts.

--- includes/NAV.php
+++ includes/NAV.php
@@ -71,88 +71,24 @@ if ($companynamere == 'HWIFS') {
   <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <a class="navbar-brand" href="#">ADL CRM</a>
+  <a class="navbar-brand" href="#">ADL</a>
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="#"><i class="fa fa-home">  Home</i> <span class="sr-only">(current)</span></a>
+          <a class="nav-link" href="/CRMmain.php"><i class="fa fa-home">  Home</i> <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#"><i class="fa fa-user-plus">  Add</i></a>
+          <a class="nav-link" href="/AddClient.php"><i class="fa fa-user-plus">  Add Client</i></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#"><i class="fa fa-search">  Search</i></a>
+          <a class="nav-link" href="/SearchClients.php"><i class="fa fa-search">  Search Clients</i></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#"><i class="fa fa-flash">  Quick Search</i></a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          CRM
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="/AddClient.php">Add Client</a>
-          <a class="dropdown-item" href="/SearchClients.php">Search Clients</a>
-          <a class="dropdown-item" href="/SearchPolicies.php?query=Life">Search Policies</a>
-          <?php if ($fflife == '1') { 
-          if (in_array($hello_name, $Level_8_Access, true)) { ?>
-              ?>
-          <a class="dropdown-item" href="/CRMReports.php">Reports</a>
-          <?php } } 
-          if ($ffdialler == '1') { ?>
-          <a class="dropdown-item" href="/dialer/Recordings.php">Recordings</a>
-          <?php } ?>
-          <a class="dropdown-item" href="/Emails.php">Emails</a>
-          <?php if($ffsms=='1') { ?>
-          <a class="dropdown-item"href="/Life/SMS/Report.php">SMS Report</a>
-          <?php  } 
-          if ($ffcalendar == '1') {
-          ?>          
-          <a class="dropdown-item" href="/calendar/calendar.php">Calendar</a>
-          <?php }
-          if ($hello_name == 'Michael') {
-          ?>
-          <a class="dropdown-item" href="/email/emailinbox.php">Email Inbox</a>
-          <?php } ?>
-        </div>
+          <a class="nav-link" href="/Staff/Search.php"><i class="fa fa-user-plus">  Employee's</i></a>
       </li>
       <?php if($ffaudits=='1') { ?>
-            <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Audits
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="/audits/main_menu.php">Main Menu</a>
-          <a class="dropdown-item" href="/audits/lead_gen_reports.php?step=New">Lead Audits</a>
-          <a class="dropdown-item" href="/audits/auditor_menu.php">Closer Audits</a>
-          <a class="dropdown-item" href="/audits/RoyalLondon/Menu.php">Royal London Audits</a>
-          <a class="dropdown-item" href="/audits/WOL/Menu.php">WOL Audits</a>
-          <a class="dropdown-item" href="/audits/reports_main.php">Reports</a>   
-        </div>
-      </li>
-      <?php } 
-      if (in_array($hello_name, $Level_9_Access, true)) {
-      ?>
-            <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Admin
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <?php if ($ffemployee == '1') { ?>
-          <a class="dropdown-item" href="/Staff/Main_Menu.php">Staff Database</a>
-          <a class="dropdown-item" href="/Staff/Search.php">Search Database</a>
-          <a class="dropdown-item" href="/Staff/Holidays/Calendar.php">Holidays</a>
-          <a class="dropdown-item" href="/Staff/Reports/RAG.php">RAG</a>
-            <?php } 
-            if (in_array($hello_name, $Level_10_Access, true)) {
-            ?>
-          <a class="dropdown-item" href="/admin/Admindash.php?admindash=y">Control Panel</a>
-            <?php } 
-            if($hello_name=="Michael") {
-            ?>
-          <a class="dropdown-item" href="/admin/users.php">User Accounts</a>            
-            <?php } ?>
-        </div>
+            <li class="nav-item">
+                <a class="nav-link" href="/audits/main_menu.php"><i class="fa fa-folder">  Audits</i></a>
       </li>
       <?php } ?>
             <li class="nav-item">

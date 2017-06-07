1e2fc0425fb2da6772b977655c987ab759cbcf8e
Michael <michael@adl-crm.uk>
2017-06-07 14:09:44 +0100

    Updated MySQL query to only pull closers.

--- compliance/JSON/Agents.php
+++ compliance/JSON/Agents.php
@@ -32,12 +32,12 @@ if(isset($EXECUTE)) {
         
         if (in_array($hello_name, $COM_LVL_10_ACCESS, true)) { 
 
-        $query = $pdo->prepare("SELECT CONCAT(firstname, ' ', lastname) AS FULL_NAME from employee_details ORDER BY lastname");
+        $query = $pdo->prepare("SELECT CONCAT(firstname, ' ', lastname) AS FULL_NAME from employee_details WHERE position='Closer' ORDER BY lastname");
         $query->execute()or die(print_r($query->errorInfo(), true));
         json_encode($results= $query->fetchAll(PDO::FETCH_ASSOC));
         
         } else {
-                    $query = $pdo->prepare("SELECT CONCAT(firstname, ' ', lastname) AS FULL_NAME from employee_details WHERE company=:COMPANY ORDER BY lastname");
+                    $query = $pdo->prepare("SELECT CONCAT(firstname, ' ', lastname) AS FULL_NAME from employee_details WHERE company=:COMPANY AND position='Closer' ORDER BY lastname");
         $query->bindParam(':COMPANY', $COMPANY, PDO::PARAM_STR);
                     $query->execute()or die(print_r($query->errorInfo(), true));
         json_encode($results= $query->fetchAll(PDO::FETCH_ASSOC));

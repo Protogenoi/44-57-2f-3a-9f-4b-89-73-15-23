6697b213118f67adabf24816b7e798dc474f2736
Michael <michael@adl-crm.uk>
2017-06-09 11:06:01 +0100

    Updated for access_levels.

--- compliance/JSON/Agents.php
+++ compliance/JSON/Agents.php
@@ -11,25 +11,6 @@ if(isset($EXECUTE)) {
     if($EXECUTE=='1') {
         require_once(__DIR__ . '/../../includes/ADL_PDO_CON.php');
         
-            if (in_array($hello_name, $TRB_ACCESS, true)) { 
-    $COMPANY='The Review Bureau';
-    }
-        if (in_array($hello_name, $PFP_ACCESS, true)) { 
-    $COMPANY='Protect Family Plans';
-    }
-        if (in_array($hello_name, $PLL_ACCESS, true)) { 
-    $COMPANY='Protected Life Ltd';
-    }
-        if (in_array($hello_name, $WI_ACCESS, true)) { 
-    $COMPANY='We Insure';
-    }
-        if (in_array($hello_name, $TFAC_ACCESS, true)) { 
-    $COMPANY='The Financial Assessment Centre';
-    }
-        if (in_array($hello_name, $APM_ACCESS, true)) { 
-    $COMPANY='Assured Protect and Mortgages';
-    }
-        
         if (in_array($hello_name, $COM_LVL_10_ACCESS, true)) { 
 
         $query = $pdo->prepare("SELECT CONCAT(firstname, ' ', lastname) AS FULL_NAME from employee_details WHERE position='Closer' ORDER BY lastname");
@@ -38,7 +19,7 @@ if(isset($EXECUTE)) {
         
         } else {
                     $query = $pdo->prepare("SELECT CONCAT(firstname, ' ', lastname) AS FULL_NAME from employee_details WHERE company=:COMPANY AND position='Closer' ORDER BY lastname");
-        $query->bindParam(':COMPANY', $COMPANY, PDO::PARAM_STR);
+        $query->bindParam(':COMPANY', $COMPANY_ENTITY, PDO::PARAM_STR);
                     $query->execute()or die(print_r($query->errorInfo(), true));
         json_encode($results= $query->fetchAll(PDO::FETCH_ASSOC));
             

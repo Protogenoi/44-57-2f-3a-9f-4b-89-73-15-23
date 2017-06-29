57f71aac1db113eba6ec4a9c989671cebb623ee0
Michael <michael@adl-crm.uk>
2017-06-29 20:51:31 +0100

    Updated for new company name.

--- Life/Templates/PostPackLetter.php
+++ Life/Templates/PostPackLetter.php
@@ -29,10 +29,10 @@ $cnquery = $pdo->prepare("select company_name from company_details limit 1");
                             $companynamere=$companydetailsq['company_name'];
                             
                             if(isset($companynamere)) {
-                                if($companynamere=='The Review Bureau') {
+                                if($companynamere=='Bluestone Protect') {
 
-    $main = $pdo->prepare("SELECT CONCAT(title,' ',first_name,' ', last_name) AS NAME, CONCAT(title2,' ', first_name2,' ', last_name2) AS NAMET, address1, address2, address3, town, post_code FROM client_details WHERE client_id = :searchplaceholder");
-    $main->bindParam(':searchplaceholder', $search, PDO::PARAM_STR, 12);
+    $main = $pdo->prepare("SELECT CONCAT(title,' ',first_name,' ', last_name) AS NAME, CONCAT(title2,' ', first_name2,' ', last_name2) AS NAMET, address1, address2, address3, town, post_code FROM client_details WHERE client_id = :SEARCH");
+    $main->bindParam(':SEARCH', $search, PDO::PARAM_INT);
     $main->execute();
     $data2=$main->fetch(PDO::FETCH_ASSOC);
     
@@ -161,7 +161,7 @@ function WriteHTML($html)
 {
     $this->SetY(-15);
     $this->SetFont('Arial','I',8);
-    $this->Cell(0,10,'The Review Bureau LTD. Registered in England and Wales with registered number 08519932..',0,0,'C');
+    $this->Cell(0,10,'Bluestone Protect LTD. Registered in England and Wales with registered number 08519932..',0,0,'C');
     $this->Ln( 5 );
     $this->Cell(0,10,'Registered office: 4th Floor The Post House, Adelaide Street, SA1 1SB.',0,0,'C');
     
@@ -172,10 +172,10 @@ function WriteHTML($html)
 
 
 $html = '
-<br /><br><p>As per our conversation please find enclosed the documents relating to your Legal & General life insurance, for your records I have enclosed the Person Quotation, Policy Terms and Conditions and Policy Summary.</p>
+<br /><br><p>As per our conversation please find enclosed the documents relating to your Legal & General life insurance, for your records I have enclosed the Personal Quotation, Policy Terms and Conditions and Policy Summary.</p>
 <br /><br><p>As discussed, you will now be able to create an online account with Legal & General so you can access a copy of your documents and your "Check your details" form. Please ensure you take some time to complete the form and ensure your information is accurate and either make any changes where needed or return the form stating your details are correct. If you do not complete the "Checking your details" form online Legal & General will send you a "Checking Your Details" form by post.</p>
 <br /><br><p>Please ensure that you do complete your "Check your details" form as quickly as possible either online or by post as this will help to avoid any delays in the future if you should need to claim on the policy.</p>
-<br /><br><p>If you have any queries or need any help, please do not hesitate to contact our customer care team Monday to Friday 10:00 - 18:30 on 0845 095 0041.</p>
+<br /><br><p>If you have any queries or need any help, please do not hesitate to contact our customer care team Monday to Friday 10:00 - 18:30 on 0330 010 0707.</p>
 
 <br /><br>
 <br /><br>';
@@ -187,15 +187,15 @@ $pdf = new PDF('P','mm','A4');
 $pdf->AddPage();
 $pdf->SetMargins(30, 20 ,30);
 $pdf->SetFont('Times','',12);
-if(file_exists("../../img/rblogonew.png")){ 
-$pdf->Image('../../img/rblogonew.png',140,6,40);
+if(file_exists("../../img/bluestone_protect_logo.png")){ 
+$pdf->Image('../../img/bluestone_protect_logo.png',80,6,40);
 }
 else{
  $pdf->Cell("COMPANY LOGO",140,6,40);   
 }
 $pdf->Ln( 5 );
 
-$pdf->Cell(0,12,"The Review Bureau", 0, 0,'R');
+$pdf->Cell(0,12,"Bluestone Protect", 0, 0,'R');
 $pdf->Ln( 5 );
 $pdf->Cell(0,12,"4th Floor The Post House", 0, 0,'R');
 $pdf->Ln( 5 );
@@ -329,236 +329,11 @@ $pdf->Cell(0,12,"$hello_name_full", 0, 0,'L');
 $pdf->Output();
 
         }
-    
-
-        if($companynamere=='Assura') {
-            $search= filter_input(INPUT_GET, 'search', FILTER_SANITIZE_SPECIAL_CHARS);
-
-    $main = $pdo->prepare("SELECT CONCAT(title,' ',first_name,' ', last_name) AS NAME, CONCAT(title2,' ', first_name2,' ', last_name2) AS NAMET, address1, address2, address3, town, post_code FROM client_details WHERE client_id = :searchplaceholder");
-    $main->bindParam(':searchplaceholder', $search, PDO::PARAM_STR, 12);
-    $main->execute();
-    $data2=$main->fetch(PDO::FETCH_ASSOC);
-    
-    $NAMEONE=$data2['NAME'];
-    $NAMETWO=$data2['NAMET'];
-    $ADD1=$data2['address1'];
-    $ADD2=$data2['address2'];
-    $ADD3=$data2['address3'];
-    $TOWN=$data2['town'];
-    $POSTCODE=$data2['post_code'];
-    
-    $TODAY = date("Y-m-d");
-    
-
- 
-
-class PDF extends FPDF
-{
-protected $B = 0;
-protected $I = 0;
-protected $U = 0;
-protected $HREF = '';
-
-function WriteHTML($html)
-    {
-        //HTML parser
-        $html=str_replace("\n",' ',$html);
-        $a=preg_split('/<(.*)>/U',$html,-1,PREG_SPLIT_DELIM_CAPTURE);
-        foreach($a as $i=>$e)
-        {
-            if($i%2==0)
-            {
-                //Text
-                if($this->HREF)
-                    $this->PutLink($this->HREF,$e);
-                elseif($this->ALIGN=='right')
-                    $this->Cell(0,5,$e,0,1,'R');
-                else
-                    $this->Write(5,$e);
-                
-            }
-            
-            
-            else
-            {
-                //Tag
-                if($e[0]=='/')
-                    $this->CloseTag(strtoupper(substr($e,1)));
-                else
-                {
-                    //Extract properties
-                    $a2=explode(' ',$e);
-                    $tag=strtoupper(array_shift($a2));
-                    $prop=array();
-                    foreach($a2 as $v)
-                    {
-                        if(preg_match('/([^=]*)=["\']?([^"\']*)/',$v,$a3))
-                            $prop[strtoupper($a3[1])]=$a3[2];
-                    }
-                    $this->OpenTag($tag,$prop);
-                }
-            }
-        }
-    }
-
-    function OpenTag($tag,$prop)
-    {
-        //Opening tag
-        if($tag=='B' || $tag=='I' || $tag=='U')
-            $this->SetStyle($tag,true);
-        if($tag=='A')
-            $this->HREF=$prop['HREF'];
-        if($tag=='BR')
-            $this->Ln(5);
-        if($tag=='P')
-            $this->ALIGN=$prop['ALIGN'];
-        if($tag=='HR')
-        {
-            if( !empty($prop['WIDTH']) )
-                $Width = $prop['WIDTH'];
-            else
-                $Width = $this->w - $this->lMargin-$this->rMargin;
-            $this->Ln(2);
-            $x = $this->GetX();
-            $y = $this->GetY();
-            $this->SetLineWidth(0.4);
-            $this->Line($x,$y,$x+$Width,$y);
-            $this->SetLineWidth(0.2);
-            $this->Ln(2);
-        }
-    }
-
-    function CloseTag($tag)
-    {
-        //Closing tag
-        if($tag=='B' || $tag=='I' || $tag=='U')
-            $this->SetStyle($tag,false);
-        if($tag=='A')
-            $this->HREF='';
-        if($tag=='P')
-            $this->ALIGN='';
-    }
-
-    function SetStyle($tag,$enable)
-    {
-        //Modify style and select corresponding font
-        $this->$tag+=($enable ? 1 : -1);
-        $style='';
-        foreach(array('B','I','U') as $s)
-            if($this->$s>0)
-                $style.=$s;
-        $this->SetFont('',$style);
-    }
-
-    function PutLink($URL,$txt)
-    {
-        //Put a hyperlink
-        $this->SetTextColor(0,0,255);
-        $this->SetStyle('U',true);
-        $this->Write(5,$txt,$URL);
-        $this->SetStyle('U',false);
-        $this->SetTextColor(0);
-    }
-
-    function Footer()
-{
-    $this->SetY(-15);
-    $this->SetFont('Arial','I',8);
-    $this->Cell(0,10,'Assura is a trading name of CJTD Limited investments, registered in England and Wales with registered number 08403633.',0,0,'C');
-    $this->Ln( 5 );
-    $this->Cell(0,10,'Registered office: Churchill house, 120 Bunns Lane, London, NW7 2AS.',0,0,'C');
-    
-    
-}
-    
-}
-
-
-$html = '
-<br /><br><p>Please find enclosed your Discretionary Trust Deed, as discussed you can use this to name the people you would like to leave the money to if something should happen to you within the term of your policy.</p>
-<br /><br><p>Please fill this form in using block capitals and in blue or black ink.</p>
-<br /><br><p>I have also included an optional Letter of Wishes should you wish to provide instructions for your Truestee/s.</p>
-<br /><br><p>Once fully completed, please sign and return using the cover letter at the back of the forms. Please note, the return address is freepost so you do not have to use a stamp.</p>
-<br /><br><p>If you have any queries please do not hesitate to contact our customer care team on 03334 432 484.</p>
-
-<br /><br>
-<br /><br>';
-
-$pdf = new PDF('P','mm','A4');
-
-
-// First page
-$pdf->AddPage();
-$pdf->SetMargins(30, 20 ,30);
-$pdf->SetFont('Times','',12);
-$pdf->Image('../../img/assuralogo.png',155,6,30);
-
-$pdf->Ln( 10 );
-$pdf->Cell(0,12,"Assura", 0, 0,'R');
-$pdf->Ln( 5 );
-$pdf->Cell(0,12,"4th Floor The Post House", 0, 0,'R');
-$pdf->Ln( 5 );
-$pdf->Cell(0,12,"Adelaide Street", 0, 0,'R');
-$pdf->Ln( 5 );
-$pdf->Cell(0,12,"Swansea", 0, 0,'R');
-$pdf->Ln( 5 );
-$pdf->Cell(0,12,"SA1 1SB", 0, 0,'R');
-$pdf->Ln( 10 );
-$pdf->Cell(0,12,"$TODAY", 0, 0,'R');
-$pdf->Ln( 5 );
-
-
-
-
-if(!empty($NAMETWO)) {
-$pdf->Cell(0,12,"$NAMEONE ", 0, 0,'L');
-$pdf->Ln( 5 );
-}
-else {
- $pdf->Cell(0,12,"$NAMEONE & $NAMETWO ", 0, 0,'L');  
-$pdf->Ln( 5 ); 
-}
-
-if(!empty($ADD1)) {
-    $pdf->Cell(0,12,"$ADD1 ", 0, 0,'L');
-    $pdf->Ln( 5 );
-}
-
-if(!empty($ADD2)) {
-    $pdf->Cell(0,12,"$ADD2 ", 0, 0,'L');
-    $pdf->Ln( 5 );
-}
-
-if(!empty($ADD3)) {
-    $pdf->Cell(0,12,"$ADD3 ", 0, 0,'L');
-    $pdf->Ln( 5 );
-}
-
-if(!empty($TOWN)) {
-    $pdf->Cell(0,12,"$TOWN ", 0, 0,'L');
-    $pdf->Ln( 5 );
-}
-
-if(!empty($POSTCODE)) {
-    $pdf->Cell(0,12,"$POSTCODE ", 0, 0,'L');
-    $pdf->Ln( 15 );
-}
-
-$pdf->WriteHTML($html);
-
-$pdf->Cell(0,12,"Many thanks", 0, 0,'L');
-$pdf->Ln( 10 );
-$pdf->Cell(0,12,"$hello_name", 0, 0,'L');
-
-$pdf->Output();
-
-        }
-
         
-                                if($companynamere=='ADL_CUS') {
+        if($companynamere=='ADL_CUS') {
 
-    $main = $pdo->prepare("SELECT CONCAT(title,' ',first_name,' ', last_name) AS NAME, CONCAT(title2,' ', first_name2,' ', last_name2) AS NAMET, address1, address2, address3, town, post_code FROM client_details WHERE client_id = :searchplaceholder");
-    $main->bindParam(':searchplaceholder', $search, PDO::PARAM_STR, 12);
+    $main = $pdo->prepare("SELECT CONCAT(title,' ',first_name,' ', last_name) AS NAME, CONCAT(title2,' ', first_name2,' ', last_name2) AS NAMET, address1, address2, address3, town, post_code FROM client_details WHERE client_id = :SEARCH");
+    $main->bindParam(':SEARCH', $search, PDO::PARAM_INT);
     $main->execute();
     $data2=$main->fetch(PDO::FETCH_ASSOC);
     
@@ -698,7 +473,7 @@ function WriteHTML($html)
 
 
 $html = '
-<br /><br><p>As per our conversation please find enclosed the documents relating to your Legal & General life insurance, for your records I have enclosed the Person Quotation, Policy Terms and Conditions and Policy Summary.</p>
+<br /><br><p>As per our conversation please find enclosed the documents relating to your Legal & General life insurance, for your records I have enclosed the Personal Quotation, Policy Terms and Conditions and Policy Summary.</p>
 <br /><br><p>As discussed, you will now be able to create an online account with Legal & General so you can access a copy of your documents and your "Check your details" form. Please ensure you take some time to complete the form and ensure your information is accurate and either make any changes where needed or return the form stating your details are correct. If you do not complete the "Checking your details" form online Legal & General will send you a "Checking Your Details" form by post.</p>
 <br /><br><p>Please ensure that you do complete your "Check your details" form as quickly as possible either online or by post as this will help to avoid any delays in the future if you should need to claim on the policy.</p>
 <br /><br><p>If you have any queries or need any help, please do not hesitate to contact our customer care team Monday to Thursday 10:00 - 18:30, Fridays 10:00 - 16:00 on 02036 349 515.</p>
--- Life/Templates/ReinstateLetter.php
+++ Life/Templates/ReinstateLetter.php
@@ -28,10 +28,10 @@ $cnquery = $pdo->prepare("select company_name from company_details limit 1");
                             $companynamere=$companydetailsq['company_name'];
                             
                             if(isset($companynamere)) {
-                                if($companynamere=='The Review Bureau') {
+                                if($companynamere=='Bluestone Protect') {
 
-    $main = $pdo->prepare("SELECT CONCAT(title,' ',first_name,' ', last_name) AS NAME, CONCAT(title2,' ', first_name2,' ', last_name2) AS NAMET, address1, address2, address3, town, post_code FROM client_details WHERE client_id = :searchplaceholder");
-    $main->bindParam(':searchplaceholder', $search, PDO::PARAM_STR, 12);
+    $main = $pdo->prepare("SELECT CONCAT(title,' ',first_name,' ', last_name) AS NAME, CONCAT(title2,' ', first_name2,' ', last_name2) AS NAMET, address1, address2, address3, town, post_code FROM client_details WHERE client_id = :SEARCH");
+    $main->bindParam(':SEARCH', $search, PDO::PARAM_STR, 12);
     $main->execute();
     $data2=$main->fetch(PDO::FETCH_ASSOC);
     
@@ -45,9 +45,6 @@ $cnquery = $pdo->prepare("select company_name from company_details limit 1");
     
     $TODAY = date("Y-m-d");
     
-
- 
-
 class PDF extends FPDF
 {
 protected $B = 0;
@@ -160,7 +157,7 @@ function WriteHTML($html)
 {
     $this->SetY(-15);
     $this->SetFont('Arial','I',8);
-    $this->Cell(0,10,'The Review Bureau LTD. Registered in England and Wales with registered number 08519932.',0,0,'C');
+    $this->Cell(0,10,'Bluestone Protect LTD. Registered in England and Wales with registered number 08519932.',0,0,'C');
     $this->Ln( 5 );
     $this->Cell(0,10,'Registered office: 4th Floor The Post House, Adelaide Street, SA1 1SB.',0,0,'C');
     
@@ -174,8 +171,8 @@ $html = '
 <br /><br><p>As you may be aware there has been an issue with the direct debit for your Legal & General life insurance. I have enclosed two forms that need to be completed in order for your policy to continue.</p>
 <br /><br><p>The first document is a Declaration of Reinstatement form; this must be completed to let Legal & General know if you have developed any health conditions or lifestyle changes that may affect the terms of your cover.</p>
 <br /><br><p>I have also enclosed a direct debit mandate for you to complete so your direct debit can be reinstated. Once received Legal and General will reinstate the direct debit and usually attempt a payment for any arrears in about 10-14 working days.</p>
-<br /><br><p>Please fill in these forms using block capitals and in blue or black ink and once fully completed please sign and return to the freepost address using the envelope enclosed.</p>
-<br /><br><p>If you have any queries please do not hesitate to contact our customer care team on 0845 095 0041.</p>
+<br /><br><p>Please fill in these forms using block capitals and in blue or black ink and once fully completed please sign and return using the envelope enclosed.</p>
+<br /><br><p>If you have any queries please do not hesitate to contact our customer care team on 0330 010 0707.</p>
 
 <br /><br>
 <br /><br>';
@@ -187,15 +184,15 @@ $pdf = new PDF('P','mm','A4');
 $pdf->AddPage();
 $pdf->SetMargins(30, 20 ,30);
 $pdf->SetFont('Times','',12);
-if(file_exists("../../img/rblogonew.png")){ 
-$pdf->Image('../../img/rblogonew.png',140,6,40);
+if(file_exists("../../img/bluestone_protect_logo.png")){ 
+$pdf->Image('../../img/bluestone_protect_logo.png',80,6,40);
 }
 else{
  $pdf->Cell("COMPANY LOGO",140,6,40);   
 }
 $pdf->Ln( 5 );
 
-$pdf->Cell(0,12,"The Review Bureau", 0, 0,'R');
+$pdf->Cell(0,12,"Bluestone Protect", 0, 0,'R');
 $pdf->Ln( 5 );
 $pdf->Cell(0,12,"4th Floor The Post House", 0, 0,'R');
 $pdf->Ln( 5 );
@@ -272,17 +269,8 @@ if(isset($hello_name)) {
          case "leighton":
              $hello_name_full="Leighton Morris";
              break;
-         case "Roxy":
-             $hello_name_full="Roxanne Studholme";
-             break;
          case "Nicola":
              $hello_name_full="Nicola Griffiths";
-             break;
-         case "Rhibayliss":
-             $hello_name_full="Rhiannon Bayliss";
-             break;
-         case "Amelia":
-             $hello_name_full="Amelia Pike";
              break;
          case "Abbiek":
              $hello_name_full="Abbie Kenyon";
@@ -302,12 +290,6 @@ if(isset($hello_name)) {
         case "Amy":
              $hello_name_full="Amy Clayfield";
              break;
-        case "Georgia":
-             $hello_name_full="Georgia Davies";
-             break;
-                           case "Victoria":
-             $hello_name_full="Victoria Hubbert";
-             break;
          default:
              $hello_name_full=$hello_name;
              
@@ -326,234 +308,11 @@ $pdf->Cell(0,12,"$hello_name_full", 0, 0,'L');
 $pdf->Output();
 
         }
-
-        if($companynamere=='Assura') {
-            $search= filter_input(INPUT_GET, 'search', FILTER_SANITIZE_SPECIAL_CHARS);
-
-    $main = $pdo->prepare("SELECT CONCAT(title,' ',first_name,' ', last_name) AS NAME, CONCAT(title2,' ', first_name2,' ', last_name2) AS NAMET, address1, address2, address3, town, post_code FROM client_details WHERE client_id = :searchplaceholder");
-    $main->bindParam(':searchplaceholder', $search, PDO::PARAM_STR, 12);
-    $main->execute();
-    $data2=$main->fetch(PDO::FETCH_ASSOC);
-    
-    $NAMEONE=$data2['NAME'];
-    $NAMETWO=$data2['NAMET'];
-    $ADD1=$data2['address1'];
-    $ADD2=$data2['address2'];
-    $ADD3=$data2['address3'];
-    $TOWN=$data2['town'];
-    $POSTCODE=$data2['post_code'];
-    
-    $TODAY = date("Y-m-d");
-    
-
- 
-
-class PDF extends FPDF
-{
-protected $B = 0;
-protected $I = 0;
-protected $U = 0;
-protected $HREF = '';
-
-function WriteHTML($html)
-    {
-        //HTML parser
-        $html=str_replace("\n",' ',$html);
-        $a=preg_split('/<(.*)>/U',$html,-1,PREG_SPLIT_DELIM_CAPTURE);
-        foreach($a as $i=>$e)
-        {
-            if($i%2==0)
-            {
-                //Text
-                if($this->HREF)
-                    $this->PutLink($this->HREF,$e);
-                elseif($this->ALIGN=='right')
-                    $this->Cell(0,5,$e,0,1,'R');
-                else
-                    $this->Write(5,$e);
-                
-            }
-            
-            
-            else
-            {
-                //Tag
-                if($e[0]=='/')
-                    $this->CloseTag(strtoupper(substr($e,1)));
-                else
-                {
-                    //Extract properties
-                    $a2=explode(' ',$e);
-                    $tag=strtoupper(array_shift($a2));
-                    $prop=array();
-                    foreach($a2 as $v)
-                    {
-                        if(preg_match('/([^=]*)=["\']?([^"\']*)/',$v,$a3))
-                            $prop[strtoupper($a3[1])]=$a3[2];
-                    }
-                    $this->OpenTag($tag,$prop);
-                }
-            }
-        }
-    }
-
-    function OpenTag($tag,$prop)
-    {
-        //Opening tag
-        if($tag=='B' || $tag=='I' || $tag=='U')
-            $this->SetStyle($tag,true);
-        if($tag=='A')
-            $this->HREF=$prop['HREF'];
-        if($tag=='BR')
-            $this->Ln(5);
-        if($tag=='P')
-            $this->ALIGN=$prop['ALIGN'];
-        if($tag=='HR')
-        {
-            if( !empty($prop['WIDTH']) )
-                $Width = $prop['WIDTH'];
-            else
-                $Width = $this->w - $this->lMargin-$this->rMargin;
-            $this->Ln(2);
-            $x = $this->GetX();
-            $y = $this->GetY();
-            $this->SetLineWidth(0.4);
-            $this->Line($x,$y,$x+$Width,$y);
-            $this->SetLineWidth(0.2);
-            $this->Ln(2);
-        }
-    }
-
-    function CloseTag($tag)
-    {
-        //Closing tag
-        if($tag=='B' || $tag=='I' || $tag=='U')
-            $this->SetStyle($tag,false);
-        if($tag=='A')
-            $this->HREF='';
-        if($tag=='P')
-            $this->ALIGN='';
-    }
-
-    function SetStyle($tag,$enable)
-    {
-        //Modify style and select corresponding font
-        $this->$tag+=($enable ? 1 : -1);
-        $style='';
-        foreach(array('B','I','U') as $s)
-            if($this->$s>0)
-                $style.=$s;
-        $this->SetFont('',$style);
-    }
-
-    function PutLink($URL,$txt)
-    {
-        //Put a hyperlink
-        $this->SetTextColor(0,0,255);
-        $this->SetStyle('U',true);
-        $this->Write(5,$txt,$URL);
-        $this->SetStyle('U',false);
-        $this->SetTextColor(0);
-    }
-
-    function Footer()
-{
-    $this->SetY(-15);
-    $this->SetFont('Arial','I',8);
-    $this->Cell(0,10,'Assura is a trading name of CJTD Limited investments, registered in England and Wales with registered number 08403633.',0,0,'C');
-    $this->Ln( 5 );
-    $this->Cell(0,10,'Registered office: Churchill house, 120 Bunns Lane, London, NW7 2AS.',0,0,'C');
-    
-    
-}
-    
-}
-
-
-$html = '
-<br /><br><p>Please find enclosed your Discretionary Trust Deed, as discussed you can use this to name the people you would like to leave the money to if something should happen to you within the term of your policy.</p>
-<br /><br><p>Please fill this form in using block capitals and in blue or black ink.</p>
-<br /><br><p>I have also included an optional Letter of Wishes should you wish to provide instructions for your Truestee/s.</p>
-<br /><br><p>Once fully completed, please sign and return using the cover letter at the back of the forms. Please note, the return address is freepost so you do not have to use a stamp.</p>
-<br /><br><p>If you have any queries please do not hesitate to contact our customer care team on 03334 432 484.</p>
-
-<br /><br>
-<br /><br>';
-
-$pdf = new PDF('P','mm','A4');
-
-
-// First page
-$pdf->AddPage();
-$pdf->SetMargins(30, 20 ,30);
-$pdf->SetFont('Times','',12);
-$pdf->Image('../../img/assuralogo.png',155,6,30);
-
-$pdf->Ln( 10 );
-$pdf->Cell(0,12,"Assura", 0, 0,'R');
-$pdf->Ln( 5 );
-$pdf->Cell(0,12,"4th Floor The Post House", 0, 0,'R');
-$pdf->Ln( 5 );
-$pdf->Cell(0,12,"Adelaide Street", 0, 0,'R');
-$pdf->Ln( 5 );
-$pdf->Cell(0,12,"Swansea", 0, 0,'R');
-$pdf->Ln( 5 );
-$pdf->Cell(0,12,"SA1 1SB", 0, 0,'R');
-$pdf->Ln( 10 );
-$pdf->Cell(0,12,"$TODAY", 0, 0,'R');
-$pdf->Ln( 5 );
-
-
-
-
-if(!empty($NAMETWO)) {
-$pdf->Cell(0,12,"$NAMEONE ", 0, 0,'L');
-$pdf->Ln( 5 );
-}
-else {
- $pdf->Cell(0,12,"$NAMEONE & $NAMETWO ", 0, 0,'L');  
-$pdf->Ln( 5 ); 
-}
-
-if(!empty($ADD1)) {
-    $pdf->Cell(0,12,"$ADD1 ", 0, 0,'L');
-    $pdf->Ln( 5 );
-}
-
-if(!empty($ADD2)) {
-    $pdf->Cell(0,12,"$ADD2 ", 0, 0,'L');
-    $pdf->Ln( 5 );
-}
-
-if(!empty($ADD3)) {
-    $pdf->Cell(0,12,"$ADD3 ", 0, 0,'L');
-    $pdf->Ln( 5 );
-}
-
-if(!empty($TOWN)) {
-    $pdf->Cell(0,12,"$TOWN ", 0, 0,'L');
-    $pdf->Ln( 5 );
-}
-
-if(!empty($POSTCODE)) {
-    $pdf->Cell(0,12,"$POSTCODE ", 0, 0,'L');
-    $pdf->Ln( 15 );
-}
-
-$pdf->WriteHTML($html);
-
-$pdf->Cell(0,12,"Many thanks", 0, 0,'L');
-$pdf->Ln( 10 );
-$pdf->Cell(0,12,"$hello_name", 0, 0,'L');
-
-$pdf->Output();
-
-        }
         
-                                if($companynamere=='ADL_CUS') {
+        if($companynamere=='ADL_CUS') {
 
-    $main = $pdo->prepare("SELECT CONCAT(title,' ',first_name,' ', last_name) AS NAME, CONCAT(title2,' ', first_name2,' ', last_name2) AS NAMET, address1, address2, address3, town, post_code FROM client_details WHERE client_id = :searchplaceholder");
-    $main->bindParam(':searchplaceholder', $search, PDO::PARAM_STR, 12);
+    $main = $pdo->prepare("SELECT CONCAT(title,' ',first_name,' ', last_name) AS NAME, CONCAT(title2,' ', first_name2,' ', last_name2) AS NAMET, address1, address2, address3, town, post_code FROM client_details WHERE client_id = :SEARCH");
+    $main->bindParam(':SEARCH', $search, PDO::PARAM_STR, 12);
     $main->execute();
     $data2=$main->fetch(PDO::FETCH_ASSOC);
     

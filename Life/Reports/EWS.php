06d1eccdb5f7ced66e8456f4f1fcb80c55199fea
Michael <michael@adl-crm.uk>
2017-06-30 18:08:32 +0100

    Added user tracking.

--- Life/Reports/EWS.php
+++ Life/Reports/EWS.php
@@ -4,6 +4,10 @@ $page_protect = new Access_user;
 $page_protect->access_page(filter_input(INPUT_SERVER,'PHP_SELF', FILTER_SANITIZE_SPECIAL_CHARS), "", 8);
 $hello_name = ($page_protect->user_full_name != "") ? $page_protect->user_full_name : $page_protect->user;
 
+$USER_TRACKING=0;
+
+require_once(__DIR__ . '/../../includes/user_tracking.php'); 
+
 include('../../includes/adl_features.php');
 include('../../includes/Access_Levels.php');
 include('../../includes/ADL_PDO_CON.php');

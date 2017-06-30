e78035eb04090f5f3507b79f66cc70293de03cbc
Michael <michael@adl-crm.uk>
2017-06-30 12:08:39 +0100

    Added new get variable to pass to ViewPolicy.

--- Life/views/LG-Policies.php
+++ Life/views/LG-Policies.php
@@ -118,7 +118,7 @@
             echo "<td> </td>";
         }
 
-        echo "<td><a href='ViewPolicy.php?policyID=$PID&search=$search' class='btn btn-info btn-xs'><i class='fa fa-eye'></i> </a></td>";
+        echo "<td><a href='ViewPolicy.php?policyID=$PID&search=$search&WHICH_COMPANY=$WHICH_COMPANY' class='btn btn-info btn-xs'><i class='fa fa-eye'></i> </a></td>";
         echo "<td><a href='EditPolicy.php?id=$PID&search=$search&name=$POL_HOLDER' class='btn btn-warning btn-xs'><i class='fa fa-edit'></i> </a></td>";
 
         if ($companynamere == 'Bluestone Protect') {
--- Life/views/WOL-Policies.php
+++ Life/views/WOL-Policies.php
@@ -102,7 +102,7 @@
             echo "<td>" . $WOL_Policies['wol_comm'] . " </td>";
         }
 
-        echo "<td><a href='ViewPolicy.php?policyID=$PID&search=$search' class='btn btn-info btn-xs'><i class='fa fa-eye'></i> </a></td>";
+        echo "<td><a href='ViewPolicy.php?policyID=$PID&search=$search&WHICH_COMPANY=$WHICH_COMPANY' class='btn btn-info btn-xs'><i class='fa fa-eye'></i> </a></td>";
         echo "<td><a href='EditPolicy.php?id=$PID&search=$search&name=$POL_HOLDER' class='btn btn-warning btn-xs'><i class='fa fa-edit'></i> </a></td>";
 
         if ($companynamere == 'Bluestone Protect') {

506d28cd93bf07c145171a79750310c982e2d2c8
Michael <michael@adl-crm.uk>
2017-07-05 17:39:13 +0100

    Made changes to the expect SUM query

--- Life/models/financials/LG/TotalExpectedWithDates.php
+++ Life/models/financials/LG/TotalExpectedWithDates.php
@@ -15,13 +15,24 @@ class TotalExpectedWithDatesModal {
 FROM
     client_policy
 WHERE
-    DATE(sale_date) BETWEEN :datefrom AND :dateto
+DATE(submitted_date) BETWEEN :datefrom AND :dateto
         AND insurer = 'Legal and General'
         AND client_policy.policystatus NOT LIKE '%CANCELLED%'
-        AND client_policy.policystatus NOT IN ('Clawback' , 'DECLINED','On hold')
-        AND client_policy.policy_number NOT like '%DU%'");
+        AND client_policy.policystatus NOT IN ('Clawback' , 'SUBMITTED-NOT-LIVE',
+        'DECLINED',
+        'On hold')
+        AND client_policy.policy_number NOT LIKE '%DU%'
+        OR DATE(sale_date) BETWEEN :datefrom2 AND :dateto2
+        AND insurer = 'Legal and General'
+        AND client_policy.policystatus NOT LIKE '%CANCELLED%'
+        AND client_policy.policystatus NOT IN ('Clawback' , 'SUBMITTED-NOT-LIVE',
+        'DECLINED',
+        'On hold')
+        AND client_policy.policy_number NOT LIKE '%DU%'");
         $stmt->bindParam(':datefrom', $datefrom, PDO::PARAM_STR);
         $stmt->bindParam(':dateto', $dateto, PDO::PARAM_STR);
+                $stmt->bindParam(':datefrom2', $datefrom, PDO::PARAM_STR);
+        $stmt->bindParam(':dateto2', $dateto, PDO::PARAM_STR);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }

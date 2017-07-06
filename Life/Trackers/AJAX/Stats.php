838908fb796e11b5c7a3e34530353277730aa18e
Michael <michael@adl-crm.uk>
2017-07-06 14:35:26 +0100

    Added PARAMS.

--- Life/Trackers/AJAX/Stats.php
+++ Life/Trackers/AJAX/Stats.php
@@ -1,4 +1,9 @@
 <?php
+
+$EXECUTE = filter_input(INPUT_GET, 'EXECUTE', FILTER_SANITIZE_SPECIAL_CHARS);
+
+if(isset($EXECUTE) && $EXECUTE=='1') {
+
 require_once(__DIR__ . '/../../../includes/ADL_PDO_CON.php');
  //CALCULATE TRACKER STATS
     require_once(__DIR__ . '/../models/CLOSERS/WARNING.php');
@@ -6,4 +11,7 @@ require_once(__DIR__ . '/../../../includes/ADL_PDO_CON.php');
     $TRACKER_WARNINGList = $TRACKER_WARNING->getTRACKER_WARNING();
     require_once(__DIR__ . '/../views/CLOSERS/WARNING.php');                            
     //END OF CALCULATION
+    
+}
+
  ?>

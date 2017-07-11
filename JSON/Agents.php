b73a4096279b4adeb0b54e670fcc4cf13774ece2
Michael <michael@adl-crm.uk>
2017-07-11 12:40:02 +0100

    Names agents names pulled from employee database to JSON array.

--- JSON/Agents.php
+++ JSON/Agents.php
@@ -5,7 +5,15 @@ if(isset($EXECUTE)) {
     if($EXECUTE=='1') {
         include('../includes/ADL_PDO_CON.php');
 
-        $query = $pdo->prepare("SELECT dialer_agents_name as full_name from dialer_agents WHERE dialer_agents_group='Agent' ORDER BY dialer_agents_name");
+        $query = $pdo->prepare("SELECT 
+    CONCAT(firstname, ' ', lastname) AS full_name
+FROM
+    employee_details
+WHERE
+    position = 'Life Lead Gen'
+        AND company = 'The Review Bureau'
+        AND employed = '1'
+ORDER BY full_name");
         $query->execute()or die(print_r($query->errorInfo(), true));
         json_encode($results= $query->fetchAll(PDO::FETCH_ASSOC));
 

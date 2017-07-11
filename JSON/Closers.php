6f3bb67b12c2b73f31a4b7bda6c0cb810dce6e83
Michael <michael@adl-crm.uk>
2017-07-11 12:40:08 +0100

    Names closers names pulled from employee database to JSON array.

---
+++ JSON/Closers.php
@@ -0,0 +1,26 @@
+<?php
+$EXECUTE = filter_input(INPUT_GET, 'EXECUTE', FILTER_SANITIZE_NUMBER_INT);
+
+if(isset($EXECUTE)) {
+    if($EXECUTE=='1') {
+        include('../includes/ADL_PDO_CON.php');
+
+        $query = $pdo->prepare("SELECT 
+    CONCAT(firstname, ' ', lastname) AS full_name
+FROM
+    employee_details
+WHERE
+    position IN('Closer','Admin')
+        AND company = 'Bluestone Protect'
+        AND employed = '1'
+ORDER BY full_name");
+        $query->execute()or die(print_r($query->errorInfo(), true));
+        json_encode($results= $query->fetchAll(PDO::FETCH_ASSOC));
+
+        echo json_encode($results);
+        
+}
+
+}
+  
+?>

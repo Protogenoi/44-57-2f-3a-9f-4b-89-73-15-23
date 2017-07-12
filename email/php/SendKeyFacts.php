db505f9f5eeab5e607268708eafbfcd7c822e12e
Michael <michael@adl-crm.uk>
2017-07-12 16:50:19 +0100

    Fixed issue with header redirect. Added option to debug failed emails.

--- email/php/SendKeyFacts.php
+++ email/php/SendKeyFacts.php
@@ -76,6 +76,7 @@ $body = $message;
 $body .= $sig;
 $mail             = new PHPMailer();
 $mail->IsSMTP();
+#$mail->SMTPDebug  = 2;
 $mail->CharSet = 'UTF-8';
 $mail->Host       = "$SMTP_HOST";                
 $mail->SMTPAuth   = true;                  
@@ -136,7 +137,7 @@ $mail->Body    = $body;
 if(!$mail->Send()) {
   echo "Mailer Error: " . $mail->ErrorInfo;
   
-  header('Location: ../../KeyFactsEmail.php?emailfailed'); die;
+  header('Location: ../../email/KeyFactsEmail.php?emailfailed'); die;
   
 } else {
        
@@ -152,116 +153,6 @@ header('Location: ../../email/KeyFactsEmail.php?emailsent&emailto='.$email); die
 }
 
 
-if($companynamere=='ADL_CUS') {
-
-$target_dir = "../../uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-
-if ($_FILES["fileToUpload"]["size"] > 700000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" && $imageFileType != "pdf" ) {
-    echo "<div class=\"notice notice-info fade in\">
-        <a href=\"#\" class=\"close\" data-dismiss=\"alert\">&times;</a>
-        <strong>Success!</strong> Sorry, only JPG, JPEG, PNG, PDF & GIF files are allowed.
-    </div>";
-    $uploadOk = 0;
-}
-
-$message ="<img src='cid:KeyFacts'>";
-$sig = "<br>-- \n
-<br>
-<br>
-<br>
-
-$signat";
-$body = $message;
-$body .= $sig;
-
-$mail             = new PHPMailer();
-
-$mail->IsSMTP(); 
-$mail->CharSet = 'UTF-8';
-$mail->Host       = "$emailsmtpdb"; 
-$mail->SMTPAuth   = true;                
-$mail->SMTPSecure = "ssl"; 
-$mail->Port       = $emailsmtpportdb;           
-$mail->Username   = "$emaildb"; 
-$mail->Password   = "$passworddb"; 
-
-$mail->AddEmbeddedImage('../../img/KeyFacts.jpg', 'KeyFacts');
-$mail->AddEmbeddedImage('../../uploads/Login Logo.png', 'logo');
-
-if (isset($_FILES["fileToUpload"]) &&
-    $_FILES["fileToUpload"]["error"] == UPLOAD_ERR_OK) {
-    $mail->AddAttachment($_FILES["fileToUpload"]["tmp_name"],
-                         $_FILES["fileToUpload"]["name"]);
-}
-
-if (isset($_FILES["fileToUpload2"]) &&
-    $_FILES["fileToUpload2"]["error"] == UPLOAD_ERR_OK) {
-    $mail->AddAttachment($_FILES["fileToUpload2"]["tmp_name"],
-                         $_FILES["fileToUpload2"]["name"]);
-}
-
-if (isset($_FILES["fileToUpload3"]) &&
-    $_FILES["fileToUpload3"]["error"] == UPLOAD_ERR_OK) {
-    $mail->AddAttachment($_FILES["fileToUpload3"]["tmp_name"],
-                         $_FILES["fileToUpload3"]["name"]);
-}
-
-if (isset($_FILES["fileToUpload4"]) &&
-    $_FILES["fileToUpload4"]["error"] == UPLOAD_ERR_OK) {
-    $mail->AddAttachment($_FILES["fileToUpload4"]["tmp_name"],
-                         $_FILES["fileToUpload4"]["name"]);
-}
-
-if (isset($_FILES["fileToUpload5"]) &&
-    $_FILES["fileToUpload5"]["error"] == UPLOAD_ERR_OK) {
-    $mail->AddAttachment($_FILES["fileToUpload5"]["tmp_name"],
-                         $_FILES["fileToUpload5"]["name"]);
-}
-
-if (isset($_FILES["fileToUpload6"]) &&
-    $_FILES["fileToUpload6"]["error"] == UPLOAD_ERR_OK) {
-    $mail->AddAttachment($_FILES["fileToUpload6"]["tmp_name"],
-                         $_FILES["fileToUpload6"]["name"]);
-}
-
-
-$mail->SetFrom("$emailfromdb", "$emaildisplaynamedb");
-$mail->AddReplyTo("$emailreplydb","$emaildisplaynamedb");
-$mail->AddBCC("$emailbccdb", "$emaildisplaynamedb");
-$mail->Subject    = "$emailsubjectdb";
-$mail->IsHTML(true); 
-
-$mail->AltBody    = "To view the message, please use an HTML compatible email viewer!";
-
-$mail->AddAddress($email, $recipient);
-
-$mail->Body    = $body;
-
-if(!$mail->Send()) {
-  echo "Mailer Error: " . $mail->ErrorInfo;
-  
-  header('Location: ../../email/KeyFactsEmail.php?emailfailed'); die;
-  
-} else {
-   
-   $INSERT = $pdo->prepare("INSERT INTO keyfactsemail set keyfactsemail_email=:email, keyfactsemail_added_by=:hello");
-   $INSERT->bindParam(':email', $email, PDO::PARAM_STR);
-   $INSERT->bindParam(':hello', $hello_name, PDO::PARAM_STR);
-   $INSERT->execute()or die(print_r($INSERT->errorInfo(), true));
-
-header('Location: ../../email/KeyFactsEmail.php?emailsent&emailto='.$email); die;
-  
-}
-
-}
 
 }
 header('Location: ../../email/KeyFactsEmail.php?emailfailed'); die;

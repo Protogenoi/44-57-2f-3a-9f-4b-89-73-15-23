7f59a842963e68bd61543b302fc0120c19289a1d
Michael <michael@adl-crm.uk>
2017-07-13 14:36:30 +0100

    Changed query to just pull submitted date.

--- Life/Financials.php
+++ Life/Financials.php
@@ -323,17 +323,9 @@ WHERE
         'DECLINED',
         'On hold')
         AND client_policy.policy_number NOT LIKE '%DU%'
-        OR DATE(sale_date) BETWEEN :datefrom2 AND :dateto2
-        AND insurer = 'Legal and General'
-        AND client_policy.policystatus NOT LIKE '%CANCELLED%'
-        AND client_policy.policystatus NOT IN ('Clawback' , 'SUBMITTED-NOT-LIVE',
-        'DECLINED',
-        'On hold')
-        AND client_policy.policy_number NOT LIKE '%DU%'");
+        ");
                             $EXPECTED_SUM_QRY->bindParam(':datefrom', $datefrom, PDO::PARAM_STR, 100);
                             $EXPECTED_SUM_QRY->bindParam(':dateto', $dateto, PDO::PARAM_STR, 100);
-                            $EXPECTED_SUM_QRY->bindParam(':datefrom2', $datefrom, PDO::PARAM_STR, 100);
-                            $EXPECTED_SUM_QRY->bindParam(':dateto2', $dateto, PDO::PARAM_STR, 100);
                             $EXPECTED_SUM_QRY->execute()or die(print_r($EXPECTED_SUM_QRY->errorInfo(), true));
                             $EXPECTED_SUM_QRY_RS = $EXPECTED_SUM_QRY->fetch(PDO::FETCH_ASSOC);
                             $ORIG_EXPECTED_SUM = $EXPECTED_SUM_QRY_RS['commission'];

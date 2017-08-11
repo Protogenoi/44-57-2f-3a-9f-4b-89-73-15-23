e7dab0f212f8b602d6c5e42503201b8c4ea49c54
Michael <michael@adl-crm.uk>
2017-08-11 15:22:23 +0100

    Added user tracking and Access Level check to replace Access arrays.

--- CRMReports.php
+++ CRMReports.php
@@ -1,8 +1,12 @@
 <?php
 require_once(__DIR__ . '/classes/access_user/access_user_class.php');
 $page_protect = new Access_user;
-$page_protect->access_page(filter_input(INPUT_SERVER,'PHP_SELF', FILTER_SANITIZE_SPECIAL_CHARS), "", 6);
+$page_protect->access_page(filter_input(INPUT_SERVER,'PHP_SELF', FILTER_SANITIZE_SPECIAL_CHARS), "", 8);
 $hello_name = ($page_protect->user_full_name != "") ? $page_protect->user_full_name : $page_protect->user;
+
+$USER_TRACKING=0;
+
+require_once(__DIR__ . '/includes/user_tracking.php');
 
 require_once(__DIR__ . '/includes/adl_features.php');
 require_once(__DIR__ . '/includes/Access_Levels.php');
@@ -20,22 +24,32 @@ if (isset($fferror)) {
     }
 }
 
-if (!in_array($hello_name, $Level_8_Access, true)) {
-
-    header('Location: /CRMmain.php');
-    die;
-}
+        require_once(__DIR__ . '/classes/database_class.php');
+        require_once(__DIR__ . '/class/login/login.php');
+        $CHECK_USER_LOGIN = new UserActions($hello_name,"NoToken");
+        $CHECK_USER_LOGIN->CheckAccessLevel();
+        
+        $USER_ACCESS_LEVEL=$CHECK_USER_LOGIN->CheckAccessLevel();
+        
+        $ACCESS_LEVEL=$USER_ACCESS_LEVEL['ACCESS_LEVEL'];
+        
+        if($ACCESS_LEVEL < 8) {
+            
+        header('Location: index.php?AccessDenied&USER='.$hello_name.'&COMPANY='.$COMPANY_ENTITY);
+        die;    
+            
+        }
 ?>
 <!DOCTYPE html>
 <html lang="en">
     <title>ADL | Reports</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="styles/layoutcrm.css" type="text/css" />
-    <link rel="stylesheet" href="bootstrap-3.3.5-dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="bootstrap-3.3.5-dist/css/bootstrap-theme.min.css">
-    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
-    <link href="img/favicon.ico" rel="icon" type="image/x-icon" />
+    <link rel="stylesheet" href="/styles/layoutcrm.css" type="text/css" />
+    <link rel="stylesheet" href="/bootstrap-3.3.5-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/bootstrap-3.3.5-dist/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="/font-awesome/css/font-awesome.min.css">
+    <link href="/img/favicon.ico" rel="icon" type="image/x-icon" />
 </head>
 <body>
 
@@ -58,7 +72,7 @@ if (!in_array($hello_name, $Level_8_Access, true)) {
 
                     <?php
                     if ($fflife == '1') {
-                        if (in_array($hello_name, $Level_8_Access, true)) {
+                        if ($ACCESS_LEVEL >= 8) {
                             if($fffinancials=='1' || $ffews=='1') {
                             ?>
                             <li>
@@ -94,8 +108,7 @@ if (!in_array($hello_name, $Level_8_Access, true)) {
                             <?php
                         }
 
-                        if (in_array($hello_name, $Level_10_Access, true)) {
-                            if ($fffinancials == '1') {
+                        if ($ACCESS_LEVEL == 10) {
                                 ?>
 
                                 <li>
@@ -118,8 +131,6 @@ if (!in_array($hello_name, $Level_8_Access, true)) {
                                     </a>
                                 </li>
 
-                            <?php } if($companynamere == 'Bluestone Protect') { ?>
-
                                 <li>
                                     <a href="Export.php">
                                         <span class="ca-icon"><i class="fa fa-cloud-download"></i></span>
@@ -140,7 +151,6 @@ if (!in_array($hello_name, $Level_8_Access, true)) {
                                     </a>
                                 </li>
                                 <?php
-                            }
                         }
                     }
 

d8e7dbd2388e2404d01f29154e8bcf4489e72d4e
Michael <michael@adl-crm.uk>
2017-08-11 17:40:00 +0100

    Added token for search, new access method and user tracking.

--- Staff/datatables/Search.php
+++ Staff/datatables/Search.php
@@ -1,13 +1,12 @@
 <?php
 require_once(__DIR__ . '../../../classes/access_user/access_user_class.php');
 $page_protect = new Access_user;
-$page_protect->access_page(filter_input(INPUT_SERVER,'PHP_SELF', FILTER_SANITIZE_SPECIAL_CHARS), "", 2);
+$page_protect->access_page(filter_input(INPUT_SERVER,'PHP_SELF', FILTER_SANITIZE_SPECIAL_CHARS), "", 10);
 $hello_name = ($page_protect->user_full_name != "") ? $page_protect->user_full_name : $page_protect->user;
 
 require_once(__DIR__ . '../../../includes/adl_features.php');
 require_once(__DIR__ . '../../../includes/Access_Levels.php');
 require_once(__DIR__ . '../../../includes/adlfunctions.php');
-require_once(__DIR__ . '../../../classes/database_class.php');
 require_once(__DIR__ . '../../../includes/ADL_PDO_CON.php');
 
 if (isset($fferror)) {
@@ -17,6 +16,35 @@ if (isset($fferror)) {
         error_reporting(E_ALL);
     }
 }
+
+$USER= filter_input(INPUT_GET, 'USER', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$TOKEN= filter_input(INPUT_GET, 'TOKEN', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+if(isset($USER) && $TOKEN) {
+    
+    require_once(__DIR__ . '../../../classes/database_class.php');
+    require_once(__DIR__ . '../../../class/login/login.php');
+
+        $CHECK_USER_LOGIN = new UserActions($USER,$TOKEN);
+        $CHECK_USER_LOGIN->CheckToken();
+        $OUT=$CHECK_USER_LOGIN->CheckToken();
+        
+        $USER_ACCESS_LEVEL=$CHECK_USER_LOGIN->CheckAccessLevel();
+        
+        $ACCESS_LEVEL=$USER_ACCESS_LEVEL['ACCESS_LEVEL'];
+        
+        if($ACCESS_LEVEL < 10) {
+            
+        header('Location: /../index.php?AccessDenied&USER='.$hello_name.'&COMPANY='.$COMPANY_ENTITY);
+        die;    
+            
+        }        
+        
+        if(isset($OUT['TOKEN_CHECK']) && $OUT['TOKEN_CHECK']=='Bad') {
+         echo "BAD";   
+        }
+
+        if(isset($OUT['TOKEN_CHECK']) && $OUT['TOKEN_CHECK']=='Good') {
 
 $EXECUTE= filter_input(INPUT_GET, 'EXECUTE', FILTER_SANITIZE_NUMBER_INT);
 
@@ -62,6 +90,6 @@ if(isset($EXECUTE)) {
         
     }
     
-    }
+} } }
     
     ?>

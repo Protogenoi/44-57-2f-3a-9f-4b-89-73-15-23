f7f69ad1c44b47759bbe358cf0bb2484cbc35bc0
Michael <michael@adl-crm.uk>
2017-08-18 13:48:48 +0100

    Added summary for user history tracking.

---
+++ admin/views/users/AllOverview.php
@@ -0,0 +1,70 @@
+
+
+    <?php foreach ($AllOvervievList as $AllOverviev): ?>
+    
+        <?php
+
+        $LG_Audit = $AllOverviev['LG_Audit'];
+        $Lead_Audit = $AllOverviev['Lead_Audit'];
+        $Aviva_Audit = $AllOverviev['Aviva_Audit'];
+        $WOL_Audit= $AllOverviev['WOL_Audit'];
+        
+        $Client_Views= $AllOverviev['Client_Views'];
+        $Add_Client= $AllOverviev['Add_Client'];
+        $Edit_Client= $AllOverviev['Edit_Client'];
+        $Edit_Policy= $AllOverviev['Edit_Policy'];
+        $Client_Notes= $AllOverviev['Client_Notes'];
+        $Add_Policy= $AllOverviev['Add_Policy'];
+        
+        $Email_Client= $AllOverviev['Email_Client'];
+        $Sent_SMS= $AllOverviev['Sent_SMS'];
+        $View_Policy= $AllOverviev['View_Policy'];
+        $Uploads= $AllOverviev['Uploads'];
+        $Advanced_Client_Search= $AllOverviev['Advanced_Client_Search'];
+        
+        $Basic_Client_Search= $AllOverviev['Basic_Client_Search'];
+        $Advanced_Policy_Search= $AllOverviev['Advanced_Policy_Search'];
+        $Keyfacts_Email= $AllOverviev['Keyfacts_Email'];
+        $Tracker_Added= $AllOverviev['Tracker_Added'];
+        
+
+           echo  "
+        <div class='col-xs-12'>
+<div class='row'>
+        <div class='col-xs-2'><center><strong>Client Views</strong><br>$Client_Views</center></div>
+        <div class='col-xs-2'><center><strong>Client Adds</strong><br> $Add_Client</center></div>
+        <div class='col-xs-2'><center><strong>Client Edits</strong><br> $Edit_Client</center></div>
+        <div class='col-xs-2'><center><strong>Policy Edits</strong><br> $Edit_Policy</center></div>
+        <div class='col-xs-2'><center><strong>Client Notes</strong><br> $Client_Notes</center></div> 
+        <div class='col-xs-2'><center><strong>Added Policy</strong><br> $Add_Policy</center></div>
+            <div class='col-xs-2'><center><strong>Client Uploads</strong><br> $Uploads</center></div> 
+        <div class='col-xs-2'><center><strong>Client Emails</strong><br> $Email_Client</center></div> 
+        <div class='col-xs-2'><center><strong>Client SMS</strong><br> $Sent_SMS</center></div> 
+        <div class='col-xs-2'><center><strong>View Policy</strong><br> $View_Policy</center></div> 
+        <div class='col-xs-2'><center><strong>Advanced Search</strong><br> $Advanced_Client_Search</center></div> 
+        <div class='col-xs-2'><center><strong>Basic Search</strong><br> $Basic_Client_Search</center></div>
+        <div class='col-xs-2'><center><strong>Policy Search</strong><br> $Advanced_Policy_Search</center></div>
+            </div>
+        </div>    
+          
+<div class='col-xs-12'>
+<div class='row'>
+        <div class='col-xs-2'><center><strong>LG Audits</strong><br>$LG_Audit</center></div>
+        <div class='col-xs-2'><center><strong>Lead Audits</strong><br>$Lead_Audit</center></div>
+        <div class='col-xs-2'><center><strong>Aviva Audits</strong><br>$Aviva_Audit</center></div>
+        <div class='col-xs-2'><center><strong>WOL Audits</strong><br>$WOL_Audit</center></div>
+            </div>
+        </div>
+        
+        <div class='col-xs-12'>
+<div class='row'>
+        <div class='col-xs-2'><center><strong>Tracker Added</strong><br>$Tracker_Added</center></div>
+        <div class='col-xs-2'><center><strong>Keyfacts Email</strong><br>$Keyfacts_Email</center></div>
+            </div>
+        </div>
+                   ";  
+
+    
+        ?>
+
+    <?php endforeach ?>

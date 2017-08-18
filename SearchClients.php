8c10fe303be6eadd330139b6baf98fc3d87af5da
Michael <michael@adl-crm.uk>
2017-08-18 15:48:53 +0100

    Added target="_blank"

--- SearchClients.php
+++ SearchClients.php
@@ -218,7 +218,7 @@ $EXECUTE = filter_input(INPUT_GET, 'EXECUTE', FILTER_SANITIZE_SPECIAL_CHARS);
                         {"data": "company"},
                         {"data": "client_id",
                             "render": function (data, type, full, meta) {
-                                return '<a href="/Life/ViewClient.php?search=' + data + '">View</a>';
+                                return '<a href="/Life/ViewClient.php?search=' + data + '" target="_blank">View</a>';
                             }},
                         {"data": "client_id",
                             "render": function (data, type, full, meta) {

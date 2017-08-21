c4b6d1eaf84b366b6c17fd6ee8c7f04e84a78369
Michael <michael@adl-crm.uk>
2017-08-21 13:26:09 +0100

    Correct permission check for Financials link. And removed features description.

--- includes/navbar.php
+++ includes/navbar.php
@@ -165,7 +165,7 @@ $companynamere = $companydetailsq['company_name'];
                         <ul role='menu' class='dropdown-menu'>
                             <li><a href="https://www20.landg.com/PolicyEnquiriesIFACentre/ENTRY_POINT_RESOURCE?domain=adviser" target="_blank">LG Policy Summary Search</a></li>
                             <?php if(in_array($hello_name, $Level_10_Access, true)) { 
-                            if ($fffinancials == '0') {  ?>
+                            if ($fffinancials == '1') {  ?>
                             <li><a href="/Life/Financials/Menu.php">Financials</a></li> 
                             <?php }
                             if ($ffews == '1') { ?>
@@ -225,116 +225,4 @@ $companynamere = $companydetailsq['company_name'];
 
         t = setInterval(refresh_div, 1000);
     </script>
-<?php } 
-
-        if (isset($FEATURE)) { ?>
-    <div class="container">
-           <?php if ($FEATURE== 'DEALSHEETS') { ?>
-        <div class='notice notice-info' role='alert'><strong><h2>Dealsheets Integration</h2></strong>
-            <br>
-            <li>Integrate your dealsheet into ADL.</li>
-            <li>Search previously submitted dealsheets.</li>
-            <li>Set search criteria.</li>
-            <li>Dealsheet's can be vetted before being put onto ADL (Agent -> Closer -> Quality Control -> ADL).</li>
-            <li>With the data inputted it can be uploaded to ADL with a click of a button.</li>
-        </div>
-              
-        <?php } ?>
-           <?php if ($FEATURE== 'EMPLOYEE') { ?>
-        <div class='notice notice-info' role='alert'><strong><h2>Employee database</h2></strong>
-            <br>
-            <li>Manage your employees via ADL.</li>
-            <li>Upload their files/documents on a secure server.</li>
-            <li>Holiday management - Have a clear overview of who is off, when and why.</li>
-            <li>Each employee has their own employee profile (employee overview).</li>
-            <li>Anyone with the correct access can check on any employee.</li>
-            <li>Employee register - Mark someone down as AWOL, sick, on holiday, in work etc.. .</li>
-        </div>
-              
-        <?php } ?> 
-           <?php if ($FEATURE== 'ASSETS') { ?>
-        <div class='notice notice-info' role='alert'><strong><h2>Asset Management</h2></strong>
-            <br>
-            <li>Maintain an inventory of all company assets (computers, office supplies etc...).</li>
-            <li>Help control inventory of stock levels.</li>
-            <li>Check why particular hardware is failing (common faults).</li>
-            <li>Assign hardware (headsets) to employees.</li>
-            <li>Track how much you are spending per week/month/year on a product.</li>
-        </div>
-              
-        <?php } ?>    
-           <?php if ($FEATURE== 'DIALER') { ?>
-        <div class='notice notice-info' role='alert'><strong><h2>Dialer Integration</h2></strong>
-            <br>
-            <li>Integrate ADL into your dialer (Bluetelecoms, Connex etc...).</li>
-            <li>Easier call recordings search.</li>
-            <li>Customised wallboards.</li>
-            <li>Customised reports can be created.</li>
-        </div>
-              
-        <?php } ?>        
-           <?php if ($FEATURE== 'TRACKERS') { ?>
-        <div class='notice notice-info' role='alert'><strong><h2>Trackers</h2></strong>
-            <br>
-            <li>Closers can fill in an ADL tracker (client name, lead gen name, result of call (sale, no quote etc..).</li>
-            <li>Get a clear overview day by day of agent and closer performance.</li>
-            <li>Search employee performance by date or by date and Closer/Lead gen.</li>
-            <li>Trackers can be viewed on a wall board.</li>
-        </div>
-              
-        <?php } ?>
-           <?php if ($FEATURE== 'KEYFACTSEMAIL') { ?>
-        <div class='notice notice-info' role='alert'><strong><h2>Keyfacts Email</h2></strong>
-            <br>
-            <li>Send your Keyfacts email via ADL.</li>
-            <li>Easily track who is sending or who is not sending the Keyfacts email.</li>
-            <li>A note will be added to a clients profile (if they are already on ADL).</li>
-            <li>Track failed/bounced emails.</li>
-            <li>Get read receipts.</li>
-        </div>
-              
-        <?php } ?>            
-           <?php if ($FEATURE== 'SMS') { ?>
-        <div class='notice notice-info' role='alert'><strong><h2>SMS Integration</h2></strong>
-            <br>
-            <li>Send SMS messages from ADL (templates or free text).</li>
-            <li>Get delivery status reports (failed/sent/delivered).</li>
-            <li>A note will be added to the clients timeline when a message fails or is delivered successfully.</li>
-            <li>Clients can text back. Notes will be added to the clients timeline.</li>
-            <li>An alert will appear on ADL when a message has been received from a client.</li>
-            <li>A customised message can be played if a client attempts to ring the SMS number.</li>
-            <li>ADL ensures that your SMS messaging service complies with the Telephone Consumer Protection Act (TCPA).</li>
-        </div>
-              
-        <?php } ?>  
-           <?php if ($FEATURE== 'FINANCIALS') { ?>
-        <div class='notice notice-info' role='alert'><strong><h2>Financials</h2></strong>
-            <br>
-            <li>ADL can give projected earning across a date range (Gross, net etc..).</li>
-            <li>Upload RAW COMMs directly to ADL to search through data.</li>
-            <li>Create customised criteria to search through data.</li>
-            <li>Identify missing policy numbers.</li>
-            <li>Identify policies that you have not been paid on.</li>
-            <li>Identify policy statuses.</li>
-            <li>Display financials statistics from RAW COMM uploads.</li>
-            <li>Compare the RAW COMMS with what should of been paid.</li>
-        </div>
-              
-        <?php } ?>      
-           <?php if ($FEATURE== 'EWS') { ?>
-        <div class='notice notice-info' role='alert'><strong><h2>Early Warning System (EWS)</h2></strong>
-            <br>
-            <li>Upload EWS directly to ADL to search through data.</li>
-            <li>Assign EWS warnings to employee's to deal with.</li>
-            <li>Build up a complete history from the EWS data.</li>
-            <li>EWS warning will link directly to the client profile.</li>
-            <li>ADL will provide a good overview of the EWS data and can be further customised.</li>
-        </div>
-              
-        <?php } ?>         
-    </div> 
- <?php }
-?>
-    
-    
-<?php } ?>
+<?php }  } ?>

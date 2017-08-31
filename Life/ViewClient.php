e75e6d6756816aad9b75c849aa6ddf6097ad8b00
Michael <michael@adl-crm.uk>
2017-08-31 16:11:47 +0100

    Changed styling for OLP/LG doc links.

--- Life/ViewClient.php
+++ Life/ViewClient.php
@@ -550,10 +550,10 @@ $OLD_COMPANY_ARRAY=array("The Review Bureau","TRB Vitality","TRB WOL","TRB Royal
                                         $POL_NORMAL=$result['policy_number'];
                                         
                                         if (file_exists("../uploads/LG/PolSummary/$POL_LOCATION")) { ?>
-        <a target="_blank" class="btn btn-default" href='../uploads/LG/PolSummary/<?php echo $POL_LOCATION; ?>' > <i class='fa fa-file-pdf-o'></i> LG Summary (<?php echo $POL_LOCATION; ?>)</a>
+        <a target="_blank" class="btn btn-default" href='../uploads/LG/PolSummary/<?php echo $POL_LOCATION; ?>' > <i class='fa fa-folder-open'></i> LG Summary (<?php echo "<strong>$POL_LOCATION</strong>"; ?>)</a>
       <?php  }
                                               if (file_exists("../uploads/LG/OLPSummary/$POL_NORMAL")) { ?>
-        <a target="_blank" class="btn btn-default" href='../uploads/LG/OLPSummary/<?php echo $POL_NORMAL; ?>' > <i class='fa fa-file-pdf-o'></i> OLP Status (<?php echo $POL_NORMAL; ?>)</a>
+        <a target="_blank" class="btn btn-default" href='../uploads/LG/OLPSummary/<?php echo $POL_NORMAL; ?>' > <i class='fa fa-folder-open-o'></i> OLP Status (<?php echo "<strong>$POL_NORMAL</strong>"; ?>)</a>
       <?php  }
                                     }                               
                                 

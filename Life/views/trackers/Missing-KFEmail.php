80647d01923b18f3534b1395898ffb3f2032fd16
Michael <michael@adl-crm.uk>
2017-09-08 10:55:07 +0100

    Added Keyfacts email tracker for closers.

---
+++ Life/views/trackers/Missing-KFEmail.php
@@ -0,0 +1,54 @@
+
+    <button class="btn btn-warning btn-sm list-group-item" data-toggle="modal" data-target="#KeyFactsModal">Keyfacts emails not sent!</button>
+    
+    <div id="KeyFactsModal" class="modal fade" role="dialog">
+  <div class="modal-dialog modal-lg"">
+
+
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Keyfacts Email Tracker</h4>
+      </div>
+      <div class="modal-body">
+          <div class='notice notice-info' role='alert' id='HIDEGLEAD'><strong><i class='fa fa-exclamation fa-lg'></i> Info:</strong> <b>Make sure the email address is correct on the dealsheet and on OLP.</b></div>
+
+          <table class="table table-hover">
+              <thead>
+        <tr>
+            <th colspan='12'>Keyfacts not sent!</th>
+        </tr>
+        <tr>
+            <th>Date</th>
+            <th>Client</th>
+            <th>Email</th>
+        </tr>
+    </thead> 
+    <?php foreach ($MissingKFEmailList as $MISSING_KF_VARS): ?>
+
+
+        <?php
+        $EMAIL = $MISSING_KF_VARS['email'];
+        $SUB_DATE = $MISSING_KF_VARS['submitted_date'];
+        $NAME = $MISSING_KF_VARS['NAME'];
+ 
+        ?>
+    
+    
+    <tr>
+        <td><?php if(isset($SUB_DATE)) { echo $SUB_DATE; } ?></td>
+        <td><?php if(isset($NAME)) { echo $NAME; } ?></td>
+        <td><?php if(isset($EMAIL)) { echo $EMAIL; } ?></td>
+    </tr>
+
+
+    <?php endforeach ?>
+              </table>
+ </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>

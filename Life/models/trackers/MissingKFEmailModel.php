ce0cf4c05926f2f2bfdbd012f86e52ee1efb595d
Michael <michael@adl-crm.uk>
2017-09-08 11:09:47 +0100

    Added switch statement for closer full names.

--- Life/models/trackers/MissingKFEmailModel.php
+++ Life/models/trackers/MissingKFEmailModel.php
@@ -10,6 +10,41 @@ class MissingKFEmailModal {
 
     public function getMissingKFEmail($hello_name) {
 
+        switch ($hello_name):
+            case "James":
+                $CLOSER_NAME = "James Adams";
+                break;
+            case "Mike":
+                $CLOSER_NAME = "Michael Lloyd";
+                break;
+            case "Sarah":
+                $CLOSER_NAME = "Sarah Wallace";
+                break;
+            case "Gavin":
+                $CLOSER_NAME = "Gavin Fulford";
+                break;
+            case "Richard";
+                $CLOSER_NAME = "Richard Michaels";
+                break;
+            case "Hayley":
+                $CLOSER_NAME = "Hayley Hutchinson";
+                break;
+            case "Martin";
+                $CLOSER_NAME="Martin Smith";
+                break;
+            case "Corey";
+                $CLOSER_NAME="Corey Divetta";
+                break;    
+            case "Kyle";
+                $CLOSER_NAME="Kyle Barnett";
+                break; 
+            case "carys";
+                $CLOSER_NAME="Carys Riley";
+                break;             
+            default:
+                $CLOSER_NAME = $hello_name;
+        endswitch;
+
         $stmt = $this->pdo->prepare("SELECT 
     client_details.email,
     client_details.submitted_date,
@@ -24,9 +59,9 @@ WHERE
             keyfactsemail_email
         FROM
             keyfactsemail)
-            AND client_policy.closer=:CLOSER
+            AND client_policy.closer =:CLOSER
     GROUP BY client_details.email ORDER BY client_details.submitted_date DESC");
-        $stmt->bindParam(':CLOSER', $hello_name, PDO::PARAM_STR);
+        $stmt->bindParam(':CLOSER', $CLOSER_NAME, PDO::PARAM_STR);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }

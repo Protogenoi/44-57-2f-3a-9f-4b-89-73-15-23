d3b901fd26a399ec8477b1ae3dc1d51e826ba362
Michael <michael@adl-crm.uk>
2017-09-08 16:04:13 +0100

    Added more upload types.

--- Life/JSON/Uploads.php
+++ Life/JSON/Uploads.php
@@ -28,8 +28,8 @@ if(isset($USER) && $TOKEN) {
 
 if(isset($EXECUTE)) {
     require_once(__DIR__ . '/../../includes/ADL_PDO_CON.php');
+    
     if($EXECUTE=='1') {
-
 
         $query = $pdo->prepare("
 SELECT 
@@ -62,10 +62,148 @@ json_encode($results['aaData']=$query->fetchAll(PDO::FETCH_ASSOC));
 
 echo json_encode($results);
 
-
-
-
     }
+    
+    if($EXECUTE=='2') {
+
+        $query = $pdo->prepare("
+SELECT 
+    client_details.client_id,
+    client_details.submitted_date,
+    CONCAT(title, ' ', first_name, ' ', last_name) AS Name,
+    CONCAT(title2,
+            ' ',
+            first_name2,
+            ' ',
+            last_name2) AS Name2,
+    post_code,
+    client_details.company
+FROM
+    client_details
+        JOIN
+    client_note ON client_note.client_id = client_details.client_id
+WHERE
+    client_details.client_id NOT IN (SELECT 
+            client_id
+        FROM
+            client_note
+        WHERE
+            note_type = 'Closer Call Recording')
+        AND DATE(client_details.submitted_date) >= '2017-09-07'
+GROUP BY client_id
+    ORDER BY client_details.submitted_date DESC");
+$query->execute()or die(print_r($query->errorInfo(), true));
+json_encode($results['aaData']=$query->fetchAll(PDO::FETCH_ASSOC));
+
+echo json_encode($results);
+
+    }  
+    
+    if($EXECUTE=='3') {
+
+        $query = $pdo->prepare("
+SELECT 
+    client_details.client_id,
+    client_details.submitted_date,
+    CONCAT(title, ' ', first_name, ' ', last_name) AS Name,
+    CONCAT(title2,
+            ' ',
+            first_name2,
+            ' ',
+            last_name2) AS Name2,
+    post_code,
+    client_details.company
+FROM
+    client_details
+        JOIN
+    client_note ON client_note.client_id = client_details.client_id
+WHERE
+    client_details.client_id NOT IN (SELECT 
+            client_id
+        FROM
+            client_note
+        WHERE
+            note_type = 'Agent Call Recording')
+        AND DATE(client_details.submitted_date) >= '2017-09-07'
+GROUP BY client_id
+    ORDER BY client_details.submitted_date DESC");
+$query->execute()or die(print_r($query->errorInfo(), true));
+json_encode($results['aaData']=$query->fetchAll(PDO::FETCH_ASSOC));
+
+echo json_encode($results);
+
+    }   
+    
+    if($EXECUTE=='4') {
+
+        $query = $pdo->prepare("
+SELECT 
+    client_details.client_id,
+    client_details.submitted_date,
+    CONCAT(title, ' ', first_name, ' ', last_name) AS Name,
+    CONCAT(title2,
+            ' ',
+            first_name2,
+            ' ',
+            last_name2) AS Name2,
+    post_code,
+    client_details.company
+FROM
+    client_details
+        JOIN
+    client_note ON client_note.client_id = client_details.client_id
+WHERE
+    client_details.client_id NOT IN (SELECT 
+            client_id
+        FROM
+            client_note
+        WHERE
+            note_type = 'Dealsheet')
+        AND DATE(client_details.submitted_date) >= '2017-09-07'
+GROUP BY client_id
+    ORDER BY client_details.submitted_date DESC");
+$query->execute()or die(print_r($query->errorInfo(), true));
+json_encode($results['aaData']=$query->fetchAll(PDO::FETCH_ASSOC));
+
+echo json_encode($results);
+
+    }   
+    
+    if($EXECUTE=='5') {
+
+        $query = $pdo->prepare("
+SELECT 
+    client_details.client_id,
+    client_details.submitted_date,
+    CONCAT(title, ' ', first_name, ' ', last_name) AS Name,
+    CONCAT(title2,
+            ' ',
+            first_name2,
+            ' ',
+            last_name2) AS Name2,
+    post_code,
+    client_details.company
+FROM
+    client_details
+        JOIN
+    client_note ON client_note.client_id = client_details.client_id
+WHERE
+    client_details.client_id NOT IN (SELECT 
+            client_id
+        FROM
+            client_note
+        WHERE
+            note_type = 'LGpolicy')
+        AND DATE(client_details.submitted_date) >= '2017-09-07'
+        AND client_details.company ='Bluestone Protect'
+GROUP BY client_id
+    ORDER BY client_details.submitted_date DESC");
+$query->execute()or die(print_r($query->errorInfo(), true));
+json_encode($results['aaData']=$query->fetchAll(PDO::FETCH_ASSOC));
+
+echo json_encode($results);
+
+    }     
     
 }
 

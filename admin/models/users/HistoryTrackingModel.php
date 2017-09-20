68b85b0c7d7263497506c0d7f2f071a7d6bbae4b
Michael <michael@adl-crm.uk>
2017-09-20 17:26:13 +0100

    Updated tracking history to select date from as well as date to.

--- admin/models/users/HistoryTrackingModel.php
+++ admin/models/users/HistoryTrackingModel.php
@@ -8,7 +8,7 @@ class HistoryTrackingModal {
         $this->pdo = $pdo;
     }
 
-    public function getHistoryTracking($TRACKING_USER,$TRACKING_DATE) {
+    public function getHistoryTracking($TRACKING_USER,$TRACKING_DATE, $TRACKING_DATE_TO) {
 
         $stmt = $this->pdo->prepare("SELECT 
 tracking_history_user
@@ -19,13 +19,14 @@ tracking_history_user
 FROM
    tracking_history
 WHERE
-    DATE(tracking_history_date)=:DATE
+    DATE(tracking_history_date) BETWEEN :DATE AND :DATETO
 AND
     tracking_history_user=:USER
     ORDER BY
         tracking_history_date");
         $stmt->bindParam(':USER', $TRACKING_USER, PDO::PARAM_STR);
         $stmt->bindParam(':DATE', $TRACKING_DATE, PDO::PARAM_STR);
+        $stmt->bindParam(':DATETO', $TRACKING_DATE_TO, PDO::PARAM_STR);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }

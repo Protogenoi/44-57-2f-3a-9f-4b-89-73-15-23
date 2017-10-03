4edfb7434f8fb2e6f04b2adc76efb8616d75cc09
Michael <michael@adl-crm.uk>
2017-10-03 11:01:26 +0100

    Updated link to new directory for PHP class combat_cal

--- W40K/views/results/results-view.php
+++ W40K/views/results/results-view.php
@@ -1,7 +1,7 @@
               <div class="notice notice-info fade in"><a href="#" class="close" data-dismiss="alert">&times;</a><center><strong>Results:</strong></center></div>
 
               <?php 
-              include('../W40K/classes/combat_cal.php');
+              include('../W40K/class/combat_cal.php');
 $combat_cal = new combat_cal();
               $HITS =$MODELS_TO_FIRE-1;
              

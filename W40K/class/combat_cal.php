5cccf4db5e0229611bd711f958bed53bc3560e5b
Michael <michael@adl-crm.uk>
2017-10-03 17:52:57 +0100

    Added tactical squad weapons.

--- W40K/class/combat_cal.php
+++ W40K/class/combat_cal.php
@@ -181,6 +181,136 @@ class combat_cal {
         
     }
     
+ if($UNIT=='Tatical Squad') {
+        $U_BS=3;
+        
+        if($UNIT_WEAPON=='Boltgun') {
+            $WEAPON_RANGE=24;
+            $WEAPON_TYPE='Rapid Fire 1';
+            $WEAPON_STR=4;
+            $WEAPON_AP=0;
+            $WEAPON_DAMAGE="1";          
+        }
+        
+        if($UNIT_WEAPON=='Bolt Pistol') {
+            $WEAPON_RANGE=12;
+            $WEAPON_TYPE='Pistol 1';
+            $WEAPON_STR=4;
+            $WEAPON_AP=0;
+            $WEAPON_DAMAGE="1";          
+        }       
+        
+        if($UNIT_WEAPON=='Heavy Bolter') {
+            $WEAPON_RANGE=36;
+            $WEAPON_TYPE='Heavy 3';
+            $WEAPON_STR=5;
+            $WEAPON_AP=1;
+            $WEAPON_DAMAGE="1";  
+ 
+        if($MOVEMENT=='Moved') {
+            $U_BS=4;
+        }            
+            
+        }    
+        
+        if($UNIT_WEAPON=='Frag Missile Launcher') {
+            $WEAPON_RANGE=48;
+            $WEAPON_TYPE='Heavy D6';
+            $WEAPON_STR=4;
+            $WEAPON_AP=0;
+            $WEAPON_DAMAGE="1";   
+
+        if($MOVEMENT=='Moved') {
+            $U_BS=4;
+        }             
+            
+        }  
+        
+        if($UNIT_WEAPON=='Krak Missile Launcher') {
+            $WEAPON_RANGE=48;
+            $WEAPON_TYPE='Heavy 1';
+            $WEAPON_STR=8;
+            $WEAPON_AP=2;
+            $WEAPON_DAMAGE="1D6"; 
+
+        if($MOVEMENT=='Moved') {
+            $U_BS=4;
+        }             
+            
+        }    
+        
+        if($UNIT_WEAPON=='Frag Grenade') {
+            $WEAPON_RANGE=6;
+            $WEAPON_TYPE='Grenade D6';
+            $WEAPON_STR=3;
+            $WEAPON_AP=0;
+            $WEAPON_DAMAGE="1";          
+        }        
+        
+        if($UNIT_WEAPON=='Krak Grenade') {
+            $WEAPON_RANGE=6;
+            $WEAPON_TYPE='Grenade 1';
+            $WEAPON_STR=6;
+            $WEAPON_AP=1;
+            $WEAPON_DAMAGE="1D3";          
+        }
+
+        if($UNIT_WEAPON=='Flamer') {
+            $WEAPON_RANGE=8;
+            $WEAPON_TYPE='Assualt 1D6';
+            $WEAPON_STR=4;
+            $WEAPON_AP=0;
+            $WEAPON_DAMAGE="1";   
+ 
+        if($MOVEMENT=='Advanced') {
+            $U_BS=4;
+        }             
+            
+        }    
+        
+        if($UNIT_WEAPON=='Grav-gun') {
+            $WEAPON_RANGE=18;
+            $WEAPON_TYPE='Rapid Fire 1';
+            $WEAPON_STR=5;
+            $WEAPON_AP=3;
+            $WEAPON_DAMAGE="1";          
+        }
+        
+        if($UNIT_WEAPON=='Meltagun') {
+            $WEAPON_RANGE=12;
+            $WEAPON_TYPE='Assualt 1';
+            $WEAPON_STR=8;
+            $WEAPON_AP=4;
+            $WEAPON_DAMAGE="D6";   
+            
+            //HALF RANGE ROLL TWO D6 for damage and discard the lowest.
+ 
+        if($MOVEMENT=='Advanced') {
+            $U_BS=4;
+        }         
+        
+    } 
+    
+        if($UNIT_WEAPON=='Plasma Gun') {
+            $WEAPON_RANGE=24;
+            $WEAPON_TYPE='Rapid Fire 1';
+            $WEAPON_STR=7;
+            $WEAPON_AP=3;
+            $WEAPON_DAMAGE="1";           
+        
+    }
+
+        if($UNIT_WEAPON=='Supercharged Plasma Gun') {
+            $WEAPON_RANGE=24;
+            $WEAPON_TYPE='Rapid Fire 1';
+            $WEAPON_STR=8;
+            $WEAPON_AP=3;
+            $WEAPON_DAMAGE="2";           
+        
+    }    
+    
+        }    
+    
         }
     
     if($ENEMY_FACTION=='Deathguard') {

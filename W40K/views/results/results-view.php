38388f7e5b1ebf9aa9b1fb0380e9f0ddf7225f16
Michael <michael@adl-crm.uk>
2017-10-03 11:00:25 +0100

    Call to PHP class combat_cal

---
+++ W40K/views/results/results-view.php
@@ -0,0 +1,10 @@
+              <div class="notice notice-info fade in"><a href="#" class="close" data-dismiss="alert">&times;</a><center><strong>Results:</strong></center></div>
+
+              <?php 
+              include('../W40K/classes/combat_cal.php');
+$combat_cal = new combat_cal();
+              $HITS =$MODELS_TO_FIRE-1;
+             
+$combat_cal->roll(6,$HITS,$UNIT,$TARGET_UNIT,$UNIT_WEAPON,$RAPID_FIRE,$FACTION,$ENEMY_FACTION);
+
+              ?>

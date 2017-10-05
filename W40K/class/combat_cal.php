d760b69c5f1f57e9fab6f6af9d394ba8315ff5ca
Michael <michael@adl-crm.uk>
2017-10-05 12:41:23 +0100

    Added Crusader Squad

--- W40K/class/combat_cal.php
+++ W40K/class/combat_cal.php
@@ -243,8 +243,6 @@ class combat_cal {
             $WEAPON_STR=8;
             $WEAPON_AP=4;
             $WEAPON_DAMAGE="1D6";   
-            
-            //HALF RANGE ROLL TWO D6 for damage and discard the lowest.
  
         if($MOVEMENT=='Advanced') {
             $U_BS=4;
@@ -340,9 +338,7 @@ class combat_cal {
             $WEAPON_TYPE='Heavy 1';
             $WEAPON_STR=8;
             $WEAPON_AP=4;
-            $WEAPON_DAMAGE="1D6"; 
-            
-        //HALF RANGE ROLL TWO D6 for damage and discard the lowest.    
+            $WEAPON_DAMAGE="1D6";    
 
         if($MOVEMENT=='Moved') {
             $U_BS=4;
@@ -377,6 +373,212 @@ class combat_cal {
         }          
     
         }    
+        
+if(strpos($UNIT,"Crusader Squad") !== false) {
+        $U_BS=3;
+        
+        if($UNIT_WEAPON=='Astartes Shotgun') {
+            $WEAPON_RANGE=12;
+            $WEAPON_TYPE='Assault 1';
+            $WEAPON_STR=4;
+            $WEAPON_AP=0;
+            $WEAPON_DAMAGE="1"; 
+
+        if($MOVEMENT=='Advanced') {
+            $U_BS=4;
+        }            
+            
+        }         
+        
+        if($UNIT_WEAPON=='Boltgun') {
+            $WEAPON_RANGE=24;
+            $WEAPON_TYPE='Rapid Fire 1';
+            $WEAPON_STR=4;
+            $WEAPON_AP=0;
+            $WEAPON_DAMAGE="1";          
+        }
+        
+        if($UNIT_WEAPON=='Bolt Pistol') {
+            $WEAPON_RANGE=12;
+            $WEAPON_TYPE='Pistol 1';
+            $WEAPON_STR=4;
+            $WEAPON_AP=0;
+            $WEAPON_DAMAGE="1";          
+        }           
+        
+        if($UNIT_WEAPON=='Frag Grenade') {
+            $WEAPON_RANGE=6;
+            $WEAPON_TYPE='Grenade D6';
+            $WEAPON_STR=3;
+            $WEAPON_AP=0;
+            $WEAPON_DAMAGE="1";          
+        }        
+        
+        if($UNIT_WEAPON=='Krak Grenade') {
+            $WEAPON_RANGE=6;
+            $WEAPON_TYPE='Grenade 1';
+            $WEAPON_STR=6;
+            $WEAPON_AP=1;
+            $WEAPON_DAMAGE="1D3";          
+        }
+
+        if($UNIT_WEAPON=='Flamer') {
+            $WEAPON_RANGE=8;
+            $WEAPON_TYPE='Assualt 1D6';
+            $WEAPON_STR=4;
+            $WEAPON_AP=0;
+            $WEAPON_DAMAGE="1";   
+ 
+        if($MOVEMENT=='Advanced') {
+            $U_BS=4;
+        }             
+            
+        }    
+        
+        if($UNIT_WEAPON=='Grav-gun') {
+            $WEAPON_RANGE=18;
+            $WEAPON_TYPE='Rapid Fire 1';
+            $WEAPON_STR=5;
+            $WEAPON_AP=3;
+            $WEAPON_DAMAGE="1";          
+        }
+        
+        if($UNIT_WEAPON=='Meltagun') {
+            $WEAPON_RANGE=12;
+            $WEAPON_TYPE='Assualt 1';
+            $WEAPON_STR=8;
+            $WEAPON_AP=4;
+            $WEAPON_DAMAGE="1D6";   
+ 
+        if($MOVEMENT=='Advanced') {
+            $U_BS=4;
+        }         
+        
+    } 
+    
+        if($UNIT_WEAPON=='Plasma Gun') {
+            $WEAPON_RANGE=24;
+            $WEAPON_TYPE='Rapid Fire 1';
+            $WEAPON_STR=7;
+            $WEAPON_AP=3;
+            $WEAPON_DAMAGE="1";           
+        
+    }
+
+        if($UNIT_WEAPON=='Supercharged Plasma Gun') {
+            $WEAPON_RANGE=24;
+            $WEAPON_TYPE='Rapid Fire 1';
+            $WEAPON_STR=8;
+            $WEAPON_AP=3;
+            $WEAPON_DAMAGE="2";           
+        
+    }  
+    
+        if($UNIT_WEAPON=='Grav-cannon and grav-amp') {
+            $WEAPON_RANGE=24;
+            $WEAPON_TYPE='Heavy 4';
+            $WEAPON_STR=5;
+            $WEAPON_AP=3;
+            $WEAPON_DAMAGE="1";  
+
+        if($MOVEMENT=='Moved') {
+            $U_BS=4;
+        }            
+            
+        }     
+  
+        if($UNIT_WEAPON=='Heavy Bolter') {
+            $WEAPON_RANGE=36;
+            $WEAPON_TYPE='Heavy 3';
+            $WEAPON_STR=5;
+            $WEAPON_AP=1;
+            $WEAPON_DAMAGE="1";  
+ 
+        if($MOVEMENT=='Moved') {
+            $U_BS=4;
+        }            
+            
+        }
+        
+        if($UNIT_WEAPON=='Lascannon') {
+            $WEAPON_RANGE=48;
+            $WEAPON_TYPE='Heavy 1';
+            $WEAPON_STR=9;
+            $WEAPON_AP=3;
+            $WEAPON_DAMAGE="1D6";  
+ 
+        if($MOVEMENT=='Moved') {
+            $U_BS=4;
+        }            
+            
+        }   
+        
+        if($UNIT_WEAPON=='Frag Missile Launcher') {
+            $WEAPON_RANGE=48;
+            $WEAPON_TYPE='Heavy 1D6';
+            $WEAPON_STR=4;
+            $WEAPON_AP=0;
+            $WEAPON_DAMAGE="1";   
+
+        if($MOVEMENT=='Moved') {
+            $U_BS=4;
+        }             
+            
+        }  
+        
+        if($UNIT_WEAPON=='Krak Missile Launcher') {
+            $WEAPON_RANGE=48;
+            $WEAPON_TYPE='Heavy 1';
+            $WEAPON_STR=8;
+            $WEAPON_AP=2;
+            $WEAPON_DAMAGE="1D6"; 
+
+        if($MOVEMENT=='Moved') {
+            $U_BS=4;
+        }             
+            
+        }   
+        
+        if($UNIT_WEAPON=='Multi-melta') {
+            $WEAPON_RANGE=24;
+            $WEAPON_TYPE='Heavy 1';
+            $WEAPON_STR=8;
+            $WEAPON_AP=4;
+            $WEAPON_DAMAGE="1D6";    
+
+        if($MOVEMENT=='Moved') {
+            $U_BS=4;
+        }             
+            
+        } 
+
+        if($UNIT_WEAPON=='Plasma Cannon') {
+            $WEAPON_RANGE=36;
+            $WEAPON_TYPE='Heavy 1D3';
+            $WEAPON_STR=7;
+            $WEAPON_AP=3;
+            $WEAPON_DAMAGE="1";    
+
+        if($MOVEMENT=='Moved') {
+            $U_BS=4;
+        }             
+            
+        } 
+
+        if($UNIT_WEAPON=='Supercharged Plasma Cannon') {
+            $WEAPON_RANGE=36;
+            $WEAPON_TYPE='Heavy 1D3';
+            $WEAPON_STR=8;
+            $WEAPON_AP=3;
+            $WEAPON_DAMAGE="2";    
+
+        if($MOVEMENT=='Moved') {
+            $U_BS=4;
+        }             
+            
+        }          
+    
+        }          
     
         }        
         

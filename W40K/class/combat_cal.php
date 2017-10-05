5419c7c187ee85d5d671733f30b37f526056938c
Michael <michael@adl-crm.uk>
2017-10-05 21:05:46 +0100

    Added Hellblaster squad and equipment.

--- W40K/class/combat_cal.php
+++ W40K/class/combat_cal.php
@@ -244,6 +244,104 @@ class combat_cal {
     
     }
     
+    if($UNIT=='Hellblaster Squad') {
+        $U_BS=3;
+         if($UNIT_WEAPON=='Bolt Pistol') {
+            $WEAPON_RANGE=12;
+            $WEAPON_TYPE='Pistol 1';
+            $WEAPON_STR=4;
+            $WEAPON_AP=0;
+            $WEAPON_DAMAGE="1";          
+        }      
+    
+        if($UNIT_WEAPON=='Frag Grenade') {
+            $WEAPON_RANGE=6;
+            $WEAPON_TYPE='Grenade D6';
+            $WEAPON_STR=3;
+            $WEAPON_AP=0;
+            $WEAPON_DAMAGE="1";          
+        }        
+        
+        if($UNIT_WEAPON=='Krak Grenade') {
+            $WEAPON_RANGE=6;
+            $WEAPON_TYPE='Grenade 1';
+            $WEAPON_STR=6;
+            $WEAPON_AP=1;
+            $WEAPON_DAMAGE="1D3";          
+        }        
+
+        if($UNIT_WEAPON=='Assualt Plasma Incinerator') {
+            $WEAPON_RANGE=24;
+            $WEAPON_TYPE='Assualt 2';
+            $WEAPON_STR=6;
+            $WEAPON_AP=4;
+            $WEAPON_DAMAGE="1";  
+            
+         if($MOVEMENT=='Advanced') {
+            $U_BS=4;
+        }           
+            
+        }
+        
+        if($UNIT_WEAPON=='Supercharged Assualt Plasma Incinerator') {
+            $WEAPON_RANGE=24;
+            $WEAPON_TYPE='Assualt 2';
+            $WEAPON_STR=7;
+            $WEAPON_AP=4;
+            $WEAPON_DAMAGE="2";  
+            
+         if($MOVEMENT=='Advanced') {
+            $U_BS=4;
+        }           
+            
+        }   
+        
+        if($UNIT_WEAPON=='Heavy Plasma Incinerator') {
+            $WEAPON_RANGE=36;
+            $WEAPON_TYPE='Heavy 1';
+            $WEAPON_STR=8;
+            $WEAPON_AP=4;
+            $WEAPON_DAMAGE="1";  
+            
+         if($MOVEMENT=='Moved') {
+            $U_BS=4;
+        }           
+            
+        }
+        
+        if($UNIT_WEAPON=='Supercharged Heavy Plasma Incinerator') {
+            $WEAPON_RANGE=36;
+            $WEAPON_TYPE='Heavy 1';
+            $WEAPON_STR=9;
+            $WEAPON_AP=4;
+            $WEAPON_DAMAGE="2";  
+            
+         if($MOVEMENT=='Moved') {
+            $U_BS=4;
+        }           
+            
+        }        
+        
+        if($UNIT_WEAPON=='Plasma Incinerator') {
+            $WEAPON_RANGE=30;
+            $WEAPON_TYPE='Rapid Fire 1';
+            $WEAPON_STR=7;
+            $WEAPON_AP=4;
+            $WEAPON_DAMAGE="1";           
+            
+        }
+        
+        if($UNIT_WEAPON=='Supercharged Plasma Incinerator') {
+            $WEAPON_RANGE=30;
+            $WEAPON_TYPE='Rapid Fire 1';
+            $WEAPON_STR=8;
+            $WEAPON_AP=4;
+            $WEAPON_DAMAGE="2";             
+            
+        }         
+        
+    }
+    
     if($UNIT=='Scout Squad') {
         $U_BS=3;
         

5903db94a72931e44e26f127e535a1efa6d68b26
Michael <michael@adl-crm.uk>
2017-10-05 14:15:04 +0100

    Added bolt pistol for Primaris Lieutenants.

--- W40K/views/factions/ultramarines-view.php
+++ W40K/views/factions/ultramarines-view.php
@@ -44,6 +44,7 @@
                         <?php if(isset($UNIT_WEAPON)) {
                             if($UNIT=='Primaris Lieutenants') { ?>
                         <option disabled>─────Wargear─────</option>
+                        <option value="Bolt Pistol" <?php if($UNIT_WEAPON=='Bolt Pistol') { echo "selected"; } ?> >Bolt Pistol</option>
                         <option value='Master Crafted Auto Bolt Rifle' <?php if($UNIT_WEAPON=='Master Crafted Auto Bolt Rifle') { echo "selected"; } ?> >Master Crafted Auto Bolt Rifle</option>
                         <option value='Master Crafted Stalker Bolt Rifle' <?php if($UNIT_WEAPON=='Master Crafted Stalker Bolt Rifle') { echo "selected"; } ?> >Master Crafted Stalker Bolt Rifle</option>
                         <option value='Bolt Pistol' <?php if($UNIT_WEAPON=='Frag Grenade') { echo "selected"; } ?> >Bolt Pistol</option><option value='Frag Grenade'>Frag Grenade</option>

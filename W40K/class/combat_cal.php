7c83201128d748e7506f1ed079c033dd5c3a8d42
Michael <michael@adl-crm.uk>
2017-10-07 14:26:00 +0100

    Added some Space Marine Units for target faction.

--- W40K/class/combat_cal.php
+++ W40K/class/combat_cal.php
@@ -1276,6 +1276,127 @@ function results($sides, $TOTAL_HITS,$TARGET_UNIT,$WEAPON_STR,$WEAPON_DAMAGE,$FA
         }
     }
     
+if($ENEMY_FACTION || $FACTION =='Ultramarines') {
+    
+    if($TARGET_UNIT=='Captain in Gravis armour') {
+        $T_MOVE=5;
+        $T_WS=2;
+        $T_BS=2;
+        $T_STR=4;
+        $T_TOUGHNESS=5;
+        $T_WOUNDS=6;
+        $T_ATTACKS=5;
+        $T_LD=9;
+        $T_SAVE=3; 
+        $T_INVUL =4;        
+    }
+    
+    if($TARGET_UNIT=='Primaris Lieutenants') {
+        $T_MOVE=6;
+        $T_WS=2;
+        $T_BS=3;
+        $T_STR=4;
+        $T_TOUGHNESS=4;
+        $T_WOUNDS=5;
+        $T_ATTACKS=4;
+        $T_LD=8;
+        $T_SAVE=3; 
+        $T_INVUL =0;        
+    }    
+    
+    if($TARGET_UNIT=='Primaris Ancient') {
+        $T_MOVE=6;
+        $T_WS=3;
+        $T_BS=3;
+        $T_STR=4;
+        $T_TOUGHNESS=4;
+        $T_WOUNDS=5;
+        $T_ATTACKS=4;
+        $T_LD=8;
+        $T_SAVE=3; 
+        $T_INVUL =0;        
+    } 
+    
+    if($TARGET_UNIT=='Intercessor Squad') {
+        $T_MOVE=6;
+        $T_WS=3;
+        $T_BS=3;
+        $T_STR=4;
+        $T_TOUGHNESS=4;
+        $T_WOUNDS=2;
+        $T_ATTACKS=2;
+        $T_LD=7;
+        $T_SAVE=3; 
+        $T_INVUL =0;        
+    }  
+    
+    if($TARGET_UNIT=='Intercessor Sergeant') {
+        $T_MOVE=6;
+        $T_WS=3;
+        $T_BS=3;
+        $T_STR=4;
+        $T_TOUGHNESS=4;
+        $T_WOUNDS=2;
+        $T_ATTACKS=3;
+        $T_LD=8;
+        $T_SAVE=3; 
+        $T_INVUL =0;        
+    }   
+    
+    if($TARGET_UNIT=='Tactical Squad') {
+        $T_MOVE=6;
+        $T_WS=3;
+        $T_BS=3;
+        $T_STR=4;
+        $T_TOUGHNESS=4;
+        $T_WOUNDS=1;
+        $T_ATTACKS=1;
+        $T_LD=7;
+        $T_SAVE=3; 
+        $T_INVUL =0;        
+    } 
+
+    if($TARGET_UNIT=='Tactical Marine Sergeant') {
+        $T_MOVE=6;
+        $T_WS=3;
+        $T_BS=3;
+        $T_STR=4;
+        $T_TOUGHNESS=4;
+        $T_WOUNDS=1;
+        $T_ATTACKS=2;
+        $T_LD=8;
+        $T_SAVE=3; 
+        $T_INVUL =0;        
+    } 
+
+    if($TARGET_UNIT=='Scout Squad') {
+        $T_MOVE=6;
+        $T_WS=3;
+        $T_BS=3;
+        $T_STR=4;
+        $T_TOUGHNESS=4;
+        $T_WOUNDS=1;
+        $T_ATTACKS=1;
+        $T_LD=7;
+        $T_SAVE=4; 
+        $T_INVUL =0;        
+    } 
+
+    if($TARGET_UNIT=='Scout Sergeant') {
+        $T_MOVE=6;
+        $T_WS=3;
+        $T_BS=3;
+        $T_STR=4;
+        $T_TOUGHNESS=4;
+        $T_WOUNDS=1;
+        $T_ATTACKS=2;
+        $T_LD=8;
+        $T_SAVE=4; 
+        $T_INVUL =0;        
+    }     
+    
+}    
+    
     if($ENEMY_FACTION || $FACTION =='Eldar') {
         
     if($TARGET_UNIT=='Eldrad Ulthran') {

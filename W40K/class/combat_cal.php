7f9614aa3ce6d941665df4188b1a3094d4218420
Michael <michael@adl-crm.uk>
2017-10-07 09:46:34 +0100

    Added basic Eldar stats for enemy faction.

--- W40K/class/combat_cal.php
+++ W40K/class/combat_cal.php
@@ -1275,6 +1275,114 @@ function results($sides, $TOTAL_HITS,$TARGET_UNIT,$WEAPON_STR,$WEAPON_DAMAGE,$FA
             $DIE_SIX++;
         }
     }
+    
+    if($ENEMY_FACTION || $FACTION =='Eldar') {
+        
+    if($TARGET_UNIT=='Eldrad Ulthran') {
+        $T_WS=2;
+        $T_BS=2;
+        $T_STR=3;
+        $T_TOUGHNESS=4;
+        $T_WOUNDS=6;
+        $T_ATTACKS=2;
+        $T_LD=9;
+        $T_SAVE=6; 
+        $T_INVUL =3;
+        
+    } 
+    
+    if($TARGET_UNIT=='Warlock') {
+        $T_WS=3;
+        $T_BS=3;
+        $T_STR=3;
+        $T_TOUGHNESS=3;
+        $T_WOUNDS=2;
+        $T_ATTACKS=2;
+        $T_LD=8;
+        $T_SAVE=6; 
+        $T_INVUL =4;
+        
+    }     
+        
+    if($TARGET_UNIT=='Guardian Defenders') {
+        $T_WS=3;
+        $T_BS=3;
+        $T_STR=3;
+        $T_TOUGHNESS=3;
+        $T_WOUNDS=1;
+        $T_ATTACKS=1;
+        $T_LD=7;
+        $T_SAVE=5; 
+        $T_INVUL =0;
+        
+    }   
+    
+    if($TARGET_UNIT=='Rangers') {
+        $T_WS=3;
+        $T_BS=3;
+        $T_STR=3;
+        $T_TOUGHNESS=3;
+        $T_WOUNDS=1;
+        $T_ATTACKS=1;
+        $T_LD=7;
+        $T_SAVE=5; 
+        $T_INVUL =0;
+        
+    }  
+    
+    if($TARGET_UNIT=='Dire Avengers') {
+        $T_WS=3;
+        $T_BS=3;
+        $T_STR=3;
+        $T_TOUGHNESS=3;
+        $T_WOUNDS=1;
+        $T_ATTACKS=1;
+        $T_LD=8;
+        $T_SAVE=4; 
+        $T_INVUL =0;
+        
+    } 
+
+    if($TARGET_UNIT=='Dire Avengers Exarch') {
+        $T_WS=3;
+        $T_BS=3;
+        $T_STR=3;
+        $T_TOUGHNESS=3;
+        $T_WOUNDS=2;
+        $T_ATTACKS=2;
+        $T_LD=8;
+        $T_SAVE=4; 
+        $T_INVUL =0;
+        
+    }    
+    
+    if($TARGET_UNIT=='Dark Reapers') {
+        $T_WS=3;
+        $T_BS=3;
+        $T_STR=3;
+        $T_TOUGHNESS=3;
+        $T_WOUNDS=1;
+        $T_ATTACKS=1;
+        $T_LD=8;
+        $T_SAVE=3; 
+        $T_INVUL =0;
+        
+    }    
+    
+    if($TARGET_UNIT=='Dark Reapers Exarch') {
+        $T_WS=3;
+        $T_BS=3;
+        $T_STR=3;
+        $T_TOUGHNESS=3;
+        $T_WOUNDS=2;
+        $T_ATTACKS=2;
+        $T_LD=8;
+        $T_SAVE=3; 
+        $T_INVUL =0;
+        
+    }    
+    
+    }
         
     if($ENEMY_FACTION || $FACTION =='Deathguard') {
     

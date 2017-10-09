c40bb7fabf9cf53cac5629e7929fa5593446894a
Michael <michael@adl-crm.uk>
2017-10-09 11:31:05 +0100

    Moved target stats to their own PHP files.

---
+++ W40K/target_stats/csm-stats.php
@@ -0,0 +1,254 @@
+<?php
+
+    if($TARGET_UNIT=='Kharn the Betrayer') {
+        $T_WS=2;
+        $T_BS=2;
+        $T_WOUNDS=5;
+        $T_SAVE=3;
+        $T_TOUGHNESS=4; 
+        
+    }     
+    
+    if($TARGET_UNIT=='Abaddon the Despoiler') {
+        $T_WS=2;
+        $T_BS=2;
+        $T_WOUNDS=7;
+        $T_SAVE=2;
+        $T_TOUGHNESS=5; 
+        
+    } 
+
+    if($TARGET_UNIT=='Daemon Prince') {
+        $T_WS=2;
+        $T_BS=2;
+        $T_WOUNDS=8;
+        $T_SAVE=3;
+        $T_TOUGHNESS=6; 
+        
+    }   
+    
+    if($TARGET_UNIT=='Khorne Bezerkers') {
+        $T_WS=3;
+        $T_BS=3;
+        $T_WOUNDS=1;
+        $T_SAVE=3;
+        $T_TOUGHNESS=4; 
+        
+    }    
+    
+    if($TARGET_UNIT=='Bezerker Champion') {
+        $T_WS=3;
+        $T_BS=3;
+        $T_WOUNDS=1;
+        $T_SAVE=3;
+        $T_TOUGHNESS=4; 
+        
+    }    
+    
+    if($TARGET_UNIT=='Rubric Marines') {
+        $T_WS=3;
+        $T_BS=3;
+        $T_WOUNDS=1;
+        $T_SAVE=3;
+        $T_TOUGHNESS=4; 
+        
+    }
+    
+    if($TARGET_UNIT=='Aspiring Sorcerer') {
+        $T_WS=3;
+        $T_BS=3;
+        $T_WOUNDS=1;
+        $T_SAVE=3;
+        $T_TOUGHNESS=4; 
+        
+    }    
+
+    if($TARGET_UNIT=='Plague Marines') {
+        $T_WS=3;
+        $T_BS=3;
+        $T_WOUNDS=1;
+        $T_SAVE=3;
+        $T_TOUGHNESS=5; 
+        
+    }   
+    
+    if($TARGET_UNIT=='Plague Champion') {
+        $T_WS=3;
+        $T_BS=3;
+        $T_WOUNDS=1;
+        $T_SAVE=3;
+        $T_TOUGHNESS=5; 
+        
+    }    
+    
+    if($TARGET_UNIT=='Chaos Terminator') {
+        $T_WS=3;
+        $T_BS=3;
+        $T_WOUNDS=2;
+        $T_SAVE=2;
+        $T_TOUGHNESS=4; 
+        
+    }     
+
+    if($TARGET_UNIT=='Terminator Champion') {
+        $T_WS=3;
+        $T_BS=3;
+        $T_WOUNDS=2;
+        $T_SAVE=2;
+        $T_TOUGHNESS=4; 
+        
+    } 
+    
+    if($TARGET_UNIT=='Helbrute') {
+        $T_WS=3;
+        $T_BS=3;
+        $T_WOUNDS=8;
+        $T_SAVE=3;
+        $T_TOUGHNESS=7; 
+        
+    } 
+    
+    if($TARGET_UNIT=='Havocs') {
+        $T_WS=3;
+        $T_BS=3;
+        $T_WOUNDS=1;
+        $T_SAVE=3;
+        $T_TOUGHNESS=4; 
+        
+    }   
+    
+    if($TARGET_UNIT=='Chaos Land Raider') {
+        $T_WS=6;
+        $T_BS=0;
+        $T_WOUNDS=16;
+        $T_SAVE=2;
+        $T_TOUGHNESS=8; 
+        
+    }  
+    
+    if($TARGET_UNIT=='Chaos Predator') {
+        $T_WS=6;
+        $T_BS=0;
+        $T_WOUNDS=11;
+        $T_SAVE=3;
+        $T_TOUGHNESS=7; 
+        
+    }     
+    
+    if($TARGET_UNIT=='Chaos Space Marines') {
+        $T_WS=3;
+        $T_BS=3;
+        $T_WOUNDS=1;
+        $T_SAVE=3;
+        $T_TOUGHNESS=4; 
+        
+    }  
+
+    if($TARGET_UNIT=='Aspiring Champion') {
+        $T_WS=3;
+        $T_BS=3;
+        $T_WOUNDS=1;
+        $T_SAVE=3;
+        $T_TOUGHNESS=4; 
+        
+    }     
+
+    if($TARGET_UNIT=='Chaos Cultists') {
+        $T_WS=4;
+        $T_BS=4;
+        $T_WOUNDS=1;
+        $T_SAVE=6;
+        $T_TOUGHNESS=3; 
+        
+    } 
+    
+    if($TARGET_UNIT=='Cultist Champion') {
+        $T_WS=4;
+        $T_BS=4;
+        $T_WOUNDS=1;
+        $T_SAVE=6;
+        $T_TOUGHNESS=3; 
+        
+    }     
+
+    if($TARGET_UNIT=='Bloodletters') {
+        $T_WS=3;
+        $T_BS=3;
+        $T_WOUNDS=1;
+        $T_SAVE=6;
+        $T_TOUGHNESS=3; 
+        
+    }
+    
+    if($TARGET_UNIT=='Bloodreaper') {
+        $T_WS=3;
+        $T_BS=3;
+        $T_WOUNDS=1;
+        $T_SAVE=6;
+        $T_TOUGHNESS=3; 
+        
+    }    
+    
+    if($TARGET_UNIT=='Pink Horrors') {
+        $T_WS=4;
+        $T_BS=4;
+        $T_WOUNDS=1;
+        $T_SAVE=6;
+        $T_TOUGHNESS=3; 
+        
+    }    
+
+    if($TARGET_UNIT=='Blue Horrors') {
+        $T_WS=5;
+        $T_BS=0;
+        $T_WOUNDS=1;
+        $T_SAVE=6;
+        $T_TOUGHNESS=3; 
+        
+    }
+    
+    if($TARGET_UNIT=='Pair of Brimstone Horrors') {
+        $T_WS=5;
+        $T_BS=0;
+        $T_WOUNDS=1;
+        $T_SAVE=6;
+        $T_TOUGHNESS=3; 
+        
+    }    
+    
+    if($TARGET_UNIT=='Plaguebearers') {
+        $T_WS=4;
+        $T_BS=4;
+        $T_WOUNDS=1;
+        $T_SAVE=6;
+        $T_TOUGHNESS=4; 
+        
+    }  
+    
+    if($TARGET_UNIT=='Plagueridden') {
+        $T_WS=4;
+        $T_BS=4;
+        $T_WOUNDS=1;
+        $T_SAVE=6;
+        $T_TOUGHNESS=4; 
+        
+    }       
+
+    if($TARGET_UNIT=='Daemonettes') {
+        $T_WS=3;
+        $T_BS=3;
+        $T_WOUNDS=1;
+        $T_SAVE=6;
+        $T_TOUGHNESS=3; 
+        
+    }  
+
+    if($TARGET_UNIT=='Alluress') {
+        $T_WS=3;
+        $T_BS=3;
+        $T_WOUNDS=1;
+        $T_SAVE=6;
+        $T_TOUGHNESS=3; 
+        
+    } 
+

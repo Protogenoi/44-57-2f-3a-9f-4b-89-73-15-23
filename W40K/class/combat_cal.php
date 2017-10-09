2f8bca06079240f209b34a21fb1079c576386118
Michael <michael@adl-crm.uk>
2017-10-09 11:44:47 +0100

    Removed default comments on top of file.

--- W40K/class/combat_cal.php
+++ W40K/class/combat_cal.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of combat_cal
- *
- * @author michael
- */
 class combat_cal { 
     
     function weapon_stats ($sides, $number,$UNIT,$TARGET_UNIT,$UNIT_WEAPON,$RANGE_BONUS,$FACTION,$ENEMY_FACTION,$MODELS_TO_FIRE,$MOVEMENT) {

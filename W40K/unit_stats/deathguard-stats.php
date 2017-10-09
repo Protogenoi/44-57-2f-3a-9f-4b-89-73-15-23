f0ab22412ab48869be8030ac9b948814fbb2a8e9
Michael <michael@adl-crm.uk>
2017-10-09 11:21:28 +0100

    Added Deathguard stats.

---
+++ W40K/unit_stats/deathguard-stats.php
@@ -0,0 +1,85 @@
+<?php
+        
+    if($UNIT=='Lord of Contagion') {
+        $U_WS=2;
+        $U_BS=2;
+        $U_STR=4;
+        $U_TOUGHNESS=5;
+        $U_WOUNDS=6;
+        $U_ATTACKS=4;
+        $U_LD=9;
+        $U_SAVE=2; 
+        
+    }
+    
+    if($UNIT=='Malignant Plaguecaster') {
+        $U_WS=3;
+        $U_BS=3;
+        $U_STR=4;
+        $U_TOUGHNESS=5;
+        $U_WOUNDS=4;
+        $U_ATTACKS=3;
+        $U_LD=8;
+        $U_SAVE=3; 
+        
+    } 
+
+    if($UNIT=='Noxious Blightbringer') {
+        $U_WS=3;
+        $U_BS=3;
+        $U_STR=4;
+        $U_TOUGHNESS=5;
+        $U_WOUNDS=4;
+        $U_ATTACKS=3;
+        $U_LD=8;
+        $U_SAVE=3; 
+        
+    }     
+        
+    if($UNIT=='Plague Marines') {
+        $U_WS=3;
+        $U_BS=3;
+        $U_STR=4;
+        $U_TOUGHNESS=5;
+        $U_WOUNDS=1;
+        $U_ATTACKS=1;
+        $U_LD=7;
+        $U_SAVE=3; 
+        
+    } 
+    
+    if($UNIT=='Plague Champion') {
+        $U_WS=3;
+        $U_BS=3;
+        $U_STR=4;
+        $U_TOUGHNESS=5;
+        $U_WOUNDS=1;
+        $U_ATTACKS=2;
+        $U_LD=8;
+        $U_SAVE=3; 
+        
+    }     
+    
+    if($UNIT=='Pox walkers') {
+        $U_WS=5;
+        $U_BS=6;
+        $U_STR=3;
+        $U_TOUGHNESS=3;
+        $U_WOUNDS=1;
+        $U_ATTACKS=2;
+        $U_LD=4;
+        $U_SAVE=7; 
+        
+    }
+    
+    if($UNIT=='Foetid Bloat-Drone') {
+        $U_WS=4;
+        $U_BS=4;
+        $U_STR=6;
+        $U_TOUGHNESS=7;
+        $U_WOUNDS=10;
+        $U_ATTACKS=3;
+        $U_LD=49;
+        $U_SAVE=3; 
+        
+    } 

469c6b7ee09fa4b703713869adb0becf583ef7a6
Michael <michael@adl-crm.uk>
2017-10-09 10:38:41 +0100

    Ultramarines unit stats.

---
+++ W40K/unit_stats/ultramarines-stats.php
@@ -0,0 +1,119 @@
+<?php
+    
+    if($TARGET_UNIT=='Captain in Gravis armour') {
+        $U_MOVE=5;
+        $U_WS=2;
+        $U_BS=2;
+        $U_STR=4;
+        $U_TOUGHNESS=5;
+        $U_WOUNDS=6;
+        $U_ATTACKS=5;
+        $U_LD=9;
+        $U_SAVE=3; 
+        $U_INVUL =4;        
+    }
+    
+    if($TARGET_UNIT=='Primaris Lieutenants') {
+        $U_MOVE=6;
+        $U_WS=2;
+        $U_BS=3;
+        $U_STR=4;
+        $U_TOUGHNESS=4;
+        $U_WOUNDS=5;
+        $U_ATTACKS=4;
+        $U_LD=8;
+        $U_SAVE=3; 
+        $U_INVUL =0;        
+    }    
+    
+    if($TARGET_UNIT=='Primaris Ancient') {
+        $U_MOVE=6;
+        $U_WS=3;
+        $U_BS=3;
+        $U_STR=4;
+        $U_TOUGHNESS=4;
+        $U_WOUNDS=5;
+        $U_ATTACKS=4;
+        $U_LD=8;
+        $U_SAVE=3; 
+        $U_INVUL =0;        
+    } 
+    
+    if($TARGET_UNIT=='Intercessor Squad') {
+        $U_MOVE=6;
+        $U_WS=3;
+        $U_BS=3;
+        $U_STR=4;
+        $U_TOUGHNESS=4;
+        $U_WOUNDS=2;
+        $U_ATTACKS=2;
+        $U_LD=7;
+        $U_SAVE=3; 
+        $U_INVUL =0;        
+    }  
+    
+    if($TARGET_UNIT=='Intercessor Sergeant') {
+        $U_MOVE=6;
+        $U_WS=3;
+        $U_BS=3;
+        $U_STR=4;
+        $U_TOUGHNESS=4;
+        $U_WOUNDS=2;
+        $U_ATTACKS=3;
+        $U_LD=8;
+        $U_SAVE=3; 
+        $U_INVUL =0;        
+    }   
+    
+    if($TARGET_UNIT=='Tactical Squad') {
+        $U_MOVE=6;
+        $U_WS=3;
+        $U_BS=3;
+        $U_STR=4;
+        $U_TOUGHNESS=4;
+        $U_WOUNDS=1;
+        $U_ATTACKS=1;
+        $U_LD=7;
+        $U_SAVE=3; 
+        $U_INVUL =0;        
+    } 
+
+    if($TARGET_UNIT=='Tactical Marine Sergeant') {
+        $U_MOVE=6;
+        $U_WS=3;
+        $U_BS=3;
+        $U_STR=4;
+        $U_TOUGHNESS=4;
+        $U_WOUNDS=1;
+        $U_ATTACKS=2;
+        $U_LD=8;
+        $U_SAVE=3; 
+        $U_INVUL =0;        
+    } 
+
+    if($TARGET_UNIT=='Scout Squad') {
+        $U_MOVE=6;
+        $U_WS=3;
+        $U_BS=3;
+        $U_STR=4;
+        $U_TOUGHNESS=4;
+        $U_WOUNDS=1;
+        $U_ATTACKS=1;
+        $U_LD=7;
+        $U_SAVE=4; 
+        $U_INVUL =0;        
+    } 
+
+    if($TARGET_UNIT=='Scout Sergeant') {
+        $U_MOVE=6;
+        $U_WS=3;
+        $U_BS=3;
+        $U_STR=4;
+        $U_TOUGHNESS=4;
+        $U_WOUNDS=1;
+        $U_ATTACKS=2;
+        $U_LD=8;
+        $U_SAVE=4; 
+        $U_INVUL =0;        
+    }     
+

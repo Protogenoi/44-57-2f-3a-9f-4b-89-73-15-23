0fb3b8e14c7400f0f4431eca7701d31618e1e2d1
Michael <michael@adl-crm.uk>
2017-10-10 16:22:08 +0100

    Made changes while trying to fix if(file_exists) issue.

--- Life/ViewClient.php
+++ Life/ViewClient.php
@@ -548,11 +548,11 @@ $OLD_COMPANY_ARRAY=array("The Review Bureau","TRB Vitality","TRB WOL","TRB Royal
                                         
                                         $POL_LOCATION = substr($result['policy_number'], 0, 9);
                                         $POL_NORMAL=$result['policy_number'];
-                                        
+                                       
                                         if (file_exists("../uploads/LG/PolSummary/$POL_LOCATION")) { ?>
         <a target="_blank" class="btn btn-default" href='../uploads/LG/PolSummary/<?php echo $POL_LOCATION; ?>' > <i class='fa fa-folder-open'></i> LG Summary (<?php echo "<strong>$POL_LOCATION</strong>"; ?>)</a>
-      <?php  }
-                                              if (file_exists("../uploads/LG/OLPSummary/$POL_NORMAL")) { ?>
+      <?php  } 
+      if (file_exists("../uploads/LG/OLPSummary/$POL_NORMAL")) { ?>
         <a target="_blank" class="btn btn-default" href='../uploads/LG/OLPSummary/<?php echo $POL_NORMAL; ?>' > <i class='fa fa-folder-open-o'></i> OLP Status (<?php echo "<strong>$POL_NORMAL</strong>"; ?>)</a>
       <?php  }
                                     }                               

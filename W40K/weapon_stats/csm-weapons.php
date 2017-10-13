a34c510c0bb028c91e868a0daa8d9194f4b4b3eb
Michael <michael@adl-crm.uk>
2017-10-13 12:39:31 +0100

    Added Rubic Marine and Aspiring Sorcerer weapons.

--- W40K/weapon_stats/csm-weapons.php
+++ W40K/weapon_stats/csm-weapons.php
@@ -258,4 +258,58 @@
             $WEAPON_AP=3;
             $WEAPON_DAMAGE="1";  
                                 
-    }     
+    } 
+     
+        if($UNIT_WEAPON=='Inferno Bolt Pistol') {
+            $WEAPON_RANGE=12;
+            $WEAPON_TYPE='Pistol 1';
+            $WEAPON_STR=4;
+            $WEAPON_AP=2;
+            $WEAPON_DAMAGE="1";          
+        }  
+        
+        if($UNIT_WEAPON=='Warp Flame Pistol') {
+            $WEAPON_RANGE=6;
+            $WEAPON_TYPE='Pistol 1D6';
+            $WEAPON_STR=3;
+            $WEAPON_AP=2;
+            $WEAPON_DAMAGE="1";
+            
+            //THIS WEAPON AUTO HITS
+        }  
+
+        if($UNIT_WEAPON=='Force Stave') {
+            $WEAPON_RANGE='Melee';
+            $WEAPON_TYPE='Melee';
+            $WEAPON_STR=+2;
+            $WEAPON_AP=1;
+            $WEAPON_DAMAGE="1D3";          
+        }  
+        
+        if($UNIT_WEAPON=='Inferno Boltgun') {
+            $WEAPON_RANGE=24;
+            $WEAPON_TYPE='Rapid Fire 1';
+            $WEAPON_STR=4;
+            $WEAPON_AP=2;
+            $WEAPON_DAMAGE="1";          
+        } 
+        
+        if($UNIT_WEAPON=='Warpflamer') {
+            $WEAPON_RANGE=8;
+            $WEAPON_TYPE='Assualt 1D6';
+            $WEAPON_STR=4;
+            $WEAPON_AP=2;
+            $WEAPON_DAMAGE="1";
+            
+            //THIS WEAPON AUTO HITS
+        } 
+
+        if($UNIT_WEAPON=='Soulreaper Cannon') {
+            $WEAPON_RANGE=24;
+            $WEAPON_TYPE='Heavy 4';
+            $WEAPON_STR=5;
+            $WEAPON_AP=3;
+            $WEAPON_DAMAGE="1";          
+        }         
+
+        

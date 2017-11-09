adde93ed849cd7778f822625d3e14fb01d3e7260
Michael <michael@adl-crm.uk>
2017-11-09 13:36:50 +0000

    Removed old compliance code.

--- Staff/datatables/Search.php
+++ Staff/datatables/Search.php
@@ -50,25 +50,9 @@ $EXECUTE= filter_input(INPUT_GET, 'EXECUTE', FILTER_SANITIZE_NUMBER_INT);
 
 if(isset($EXECUTE)) { 
     if($EXECUTE=='1') {
-        
-            if (in_array($hello_name, $TRB_ACCESS, true)) { 
+
     $COMPANY='Bluestone Protect';
-    }
-        if (in_array($hello_name, $PFP_ACCESS, true)) { 
-    $COMPANY='Protect Family Plans';
-    }
-        if (in_array($hello_name, $PLL_ACCESS, true)) { 
-    $COMPANY='Protected Life Ltd';
-    }
-        if (in_array($hello_name, $WI_ACCESS, true)) { 
-    $COMPANY='We Insure';
-    }
-        if (in_array($hello_name, $TFAC_ACCESS, true)) { 
-    $COMPANY='The Financial Assessment Centre';
-    }
-        if (in_array($hello_name, $APM_ACCESS, true)) { 
-    $COMPANY='Assured Protect and Mortgages';
-    }        
+       
 
     if (in_array($hello_name, $COM_LVL_10_ACCESS, true)) { 
                 $query = $pdo->prepare("SELECT employee_details.employed, employee_details.company, employee_details.employee_id, CONCAT(employee_details.firstname,' ', employee_details.lastname) AS NAME, employee_contact.mob, employee_contact.tel, employee_details.position FROM employee_details JOIN employee_contact ON employee_details.employee_id = employee_contact.employee_id ORDER BY employee_details.added_date DESC");

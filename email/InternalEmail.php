918ddcd685a889d8b9d3a188c80b5e8d5c92844b
Michael <michael@adl-crm.uk>
2017-11-10 14:07:41 +0000

    Updated for timelock code and locked down InternalEmail.php

--- email/InternalEmail.php
+++ email/InternalEmail.php
@@ -1,4 +1,19 @@
 <?php 
+require_once(__DIR__ . '../../classes/access_user/access_user_class.php');
+$page_protect = new Access_user;
+$page_protect->access_page(filter_input(INPUT_SERVER,'PHP_SELF', FILTER_SANITIZE_SPECIAL_CHARS), "", 1);
+$hello_name = ($page_protect->user_full_name != "") ? $page_protect->user_full_name : $page_protect->user;
+
+$USER_TRACKING=0;
+
+require_once(__DIR__ . '../../includes/user_tracking.php'); 
+
+require_once(__DIR__ . '/../includes/time.php');
+
+if(isset($FORCE_LOGOUT) && $FORCE_LOGOUT== 1) {
+    $page_protect->log_out();
+}
+
 require_once(__DIR__ . '../../includes/adl_features.php');
 require_once(__DIR__ . '../../includes/adlfunctions.php');
 

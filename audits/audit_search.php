35bdd951d371fd831f65d545e2eb93d332469401
Michael <michael@adl-crm.uk>
2017-11-10 11:27:21 +0000

    Updated to pass user token to database JSON request.

--- audits/audit_search.php
+++ audits/audit_search.php
@@ -12,6 +12,33 @@ require_once(__DIR__ . '/../includes/adl_features.php');
 require_once(__DIR__ . '/../includes/Access_Levels.php');
 require_once(__DIR__ . '/../includes/adlfunctions.php');
 
+    require_once(__DIR__ . '../../classes/database_class.php');
+    require_once(__DIR__ . '../../class/login/login.php');
+
+        $CHECK_USER_LOGIN = new UserActions($hello_name,"NoToken");
+        
+        $CHECK_USER_LOGIN->SelectToken();
+        $CHECK_USER_LOGIN->CheckAccessLevel();
+   
+        $OUT=$CHECK_USER_LOGIN->SelectToken();
+        
+        if(isset($OUT['TOKEN_SELECT']) && $OUT['TOKEN_SELECT']!='NoToken') {
+        
+        $TOKEN=$OUT['TOKEN_SELECT'];
+                
+        }
+        
+        $USER_ACCESS_LEVEL=$CHECK_USER_LOGIN->CheckAccessLevel();
+        
+        $ACCESS_LEVEL=$USER_ACCESS_LEVEL['ACCESS_LEVEL'];
+        
+        if($ACCESS_LEVEL < 2) {
+            
+        header('Location: /../index.php?AccessDenied&USER='.$hello_name.'&COMPANY='.$COMPANY_ENTITY);
+        die;    
+            
+        }
+
 if ($ffanalytics == '1') {
     require_once(__DIR__ . '/../php/analyticstracking.php');
 }
@@ -29,23 +56,6 @@ if ($ffaudits == '0') {
     header('Location: /../CRMmain.php');
     die;
 }
-
-
-        require_once(__DIR__ . '/../classes/database_class.php');
-        require_once(__DIR__ . '/../class/login/login.php');
-        $CHECK_USER_LOGIN = new UserActions($hello_name,"NoToken");
-        $CHECK_USER_LOGIN->CheckAccessLevel();
-        
-        $USER_ACCESS_LEVEL=$CHECK_USER_LOGIN->CheckAccessLevel();
-        
-        $ACCESS_LEVEL=$USER_ACCESS_LEVEL['ACCESS_LEVEL'];
-        
-        if($ACCESS_LEVEL < 2) {
-            
-        header('Location: /../index.php?AccessDenied&USER='.$hello_name.'&COMPANY='.$COMPANY_ENTITY);
-        die;    
-            
-        }
 ?>
 <!DOCTYPE html>
 <!-- 
@@ -156,7 +166,7 @@ require_once(__DIR__ . '/../includes/navbar.php');
                     "processing": "<div></div><div></div><div></div><div></div><div></div>"
 
                 },
-                "ajax": "datatables/AuditSearch.php?AuditType=Closer",
+                "ajax": "datatables/AuditSearch.php?AuditType=Closer&USER=<?php echo $hello_name; ?>&TOKEN=<?php echo $TOKEN; ?>",
                 "columns": [
                     {
                         "className": 'details-control',

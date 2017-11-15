09411f5e62a23da7249fe6cb257e78facf5f64fa
Michael <michael@adl-crm.uk>
2017-11-15 17:20:40 +0000

    Renamed layoutcrm.css to main.css and update file structure.
    /resources/templates/ADL/main.css

--- Staff/Holidays/Main_Menu.php
+++ Staff/Holidays/Main_Menu.php
@@ -20,7 +20,7 @@ include('../includes/adlfunctions.php');
 <title>ADL | Main Menu</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="/styles/layoutcrm.css" type="text/css" />
+<link rel="stylesheet" href="/resources/templates/ADL/main.css" type="text/css" />
 <link rel="stylesheet" href="/bootstrap-3.3.5-dist/css/bootstrap.min.css">
 <link rel="stylesheet" href="/bootstrap-3.3.5-dist/css/bootstrap-theme.min.css">
 <link rel="stylesheet" href="/bootstrap-3.3.5-dist/css/bootstrap.css">
--- Staff/Search.php
+++ Staff/Search.php
@@ -63,7 +63,7 @@ if (isset($fferror)) {
 <title>ADL | Search Employee Database</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="/styles/layoutcrm.css" type="text/css" />
+<link rel="stylesheet" href="/resources/templates/ADL/main.css" type="text/css" />
     <link rel="stylesheet" href="/bootstrap-3.3.5-dist/cosmo/bootstrap.min.css">
     <link rel="stylesheet" href="/bootstrap-3.3.5-dist/cosmo/bootstrap.css">
     <link rel="stylesheet" href="/resources/templates/font-awesome/css/font-awesome.min.css">

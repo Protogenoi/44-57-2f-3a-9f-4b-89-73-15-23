6381732c0f51ba2c0f53f60bd4d7bf7a118b485c
Michael <michael@adl-crm.uk>
2017-11-15 16:59:46 +0000

    Renamed layoutcrm.css to main.css and update file structure.
    /resources/templates/ADL/main.css

--- PBA/Main_Menu.php
+++ PBA/Main_Menu.php
@@ -52,7 +52,7 @@ if (isset($fferror)) {
 <title>PBA Menu</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="/datatables/css/layoutcrm.css" type="text/css" />
+<link rel="stylesheet" href="/resources/templates/ADL/main.css" type="text/css" />
 <link rel="stylesheet" href="/bootstrap-3.3.5-dist/css/bootstrap.min.css">
 <link rel="stylesheet" href="/bootstrap-3.3.5-dist/css/bootstrap-theme.min.css">
 <link rel="stylesheet" href="/bootstrap-3.3.5-dist/css/bootstrap.css">

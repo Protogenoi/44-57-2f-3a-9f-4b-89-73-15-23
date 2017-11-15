dc81f7c936131861ad91aab6dd4be5a37d7d71d7
Michael <michael@adl-crm.uk>
2017-11-15 10:43:28 +0000

    Changes to app directory structure (EasyAutoComplete).

--- Life/Reports/EWSAgentPerformance.php
+++ Life/Reports/EWSAgentPerformance.php
@@ -29,12 +29,12 @@ if (!in_array($hello_name,$Level_8_Access, true)) {
     <link rel="stylesheet" href="/bootstrap-3.3.5-dist/css/bootstrap-theme.min.css">
     <link rel="stylesheet" href="/font-awesome/css/font-awesome.min.css">
       <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
-      <link rel="stylesheet" href="/EasyAutocomplete-1.3.3/easy-autocomplete.min.css"> 
+      <link rel="stylesheet" href="/resources/lib/EasyAutocomplete-1.3.3/easy-autocomplete.min.css"> 
       <link href="/img/favicon.ico" rel="icon" type="image/x-icon" />
       <script type="text/javascript" language="javascript" src="/js/jquery/jquery-3.0.0.min.js"></script>
 <script type="text/javascript" language="javascript" src="/js/jquery-ui-1.11.4/jquery-ui.min.js"></script>
 <script src="/bootstrap-3.3.5-dist/js/bootstrap.min.js"></script>
-<script src="/EasyAutocomplete-1.3.3/jquery.easy-autocomplete.min.js"></script> 
+<script src="/resources/lib/EasyAutocomplete-1.3.3/jquery.easy-autocomplete.min.js"></script> 
     <style>
         .label-purple {
   background-color: #8e44ad;

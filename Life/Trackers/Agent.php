c20d7160a18ae87d0d92ae4370c3f9ad180087ce
Michael <michael@adl-crm.uk>
2017-11-15 15:29:03 +0000

    Changes to app directory structure (font-awesome).

--- Life/Trackers/Agent.php
+++ Life/Trackers/Agent.php
@@ -43,7 +43,7 @@ $Today_TIME = date("h:i:s");
     <link rel="stylesheet" type="text/css" href="/bootstrap-3.3.5-dist/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="/bootstrap-3.3.5-dist/css/bootstrap-theme.min.css">
     <link rel="stylesheet" type="text/css" href="/styles/Notices.css">
-    <link rel="stylesheet" type="text/css" href="/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="/resources/templates/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="/styles/sweet-alert.min.css" />
     <link rel="stylesheet" type="text/css" href="/js/jquery-ui-1.11.4/jquery-ui.min.css" />
     <link rel="stylesheet" href="/resources/lib/EasyAutocomplete-1.3.3/easy-autocomplete.min.css">

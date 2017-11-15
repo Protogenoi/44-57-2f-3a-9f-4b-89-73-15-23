0f2bbd89e049c23eed7edf573e7a5eeef4c47bd3
Michael <michael@adl-crm.uk>
2017-11-15 15:21:55 +0000

    Changes to app directory structure (font-awesome).

--- Home/ViewClient.php
+++ Home/ViewClient.php
@@ -76,7 +76,7 @@ if(empty($CID)) {
     <link rel="stylesheet" href="/styles/Timeline.css" type="text/css" />
     <link rel="stylesheet" href="/bootstrap-3.3.5-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="/bootstrap-3.3.5-dist/css/bootstrap-theme.min.css">
-    <link rel="stylesheet" href="/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/resources/templates/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="/styles/sweet-alert.min.css" />
     <link rel="stylesheet" href="/js/jquery-ui-1.11.4/jquery-ui.min.css" />
     <link rel="stylesheet" type="text/css" href="/resources/lib/clockpicker-gh-pages/dist/jquery-clockpicker.min.css">
--- Home/ViewPolicy.php
+++ Home/ViewPolicy.php
@@ -62,7 +62,7 @@ if (isset($fferror)) {
 <script type="text/javascript" language="javascript" src="/js/jquery.dataTables.min.js"></script>
 <link rel="stylesheet" href="/bootstrap-3.3.5-dist/css/bootstrap.min.css">
 <link rel="stylesheet" href="/bootstrap-3.3.5-dist/css/bootstrap-theme.min.css">
-<link rel="stylesheet" href="/font-awesome/css/font-awesome.min.css">
+<link rel="stylesheet" href="/resources/templates/font-awesome/css/font-awesome.min.css">
 <link rel="stylesheet" href="/styles/sweet-alert.min.css" />
 <link href="/img/favicon.ico" rel="icon" type="image/x-icon" />
 <style type="text/css">

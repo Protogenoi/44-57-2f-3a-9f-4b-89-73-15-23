5679560afe8de77ba7589079d862c6b32745c3ac
Michael <michael@adl-crm.uk>
2017-11-15 17:28:55 +0000

    Renamed layoutcrm.css to main.css and update file structure.
    /resources/templates/ADL/main.css

--- Life/SMS/Menu.php
+++ Life/SMS/Menu.php
@@ -42,7 +42,7 @@ if ($ffsms == '0') {
     <title>ADL | SMS Menu</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="/styles/layoutcrm.css" type="text/css" />
+    <link rel="stylesheet" href="/resources/templates/ADL/main.css" type="text/css" />
     <link rel="stylesheet" href="/bootstrap-3.3.5-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="/bootstrap-3.3.5-dist/css/bootstrap-theme.min.css">
     <link rel="stylesheet" href="/resources/templates/font-awesome/css/font-awesome.min.css">
--- Life/SMS/Report.php
+++ Life/SMS/Report.php
@@ -33,7 +33,7 @@ if (!in_array($hello_name, $Level_3_Access, true)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <head>
-        <link rel="stylesheet" href="/styles/layoutcrm.css" type="text/css" />
+        <link rel="stylesheet" href="/resources/templates/ADL/main.css" type="text/css" />
         <link rel="stylesheet" href="/bootstrap-3.3.5-dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="/bootstrap-3.3.5-dist/css/bootstrap-theme.min.css">
         <link rel="stylesheet" href="/resources/templates/font-awesome/css/font-awesome.min.css">

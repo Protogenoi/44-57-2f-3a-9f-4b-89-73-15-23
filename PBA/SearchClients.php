497d359eae027a8459ae4a3a7b5ddbcbbafeb870
Michael <michael@adl-crm.uk>
2017-11-15 17:17:58 +0000

    Renamed layoutcrm.css to main.css and update file structure.
    /resources/templates/ADL/main.css

--- PBA/SearchClients.php
+++ PBA/SearchClients.php
@@ -72,7 +72,7 @@ $EXECUTE = filter_input(INPUT_GET, 'EXECUTE', FILTER_SANITIZE_SPECIAL_CHARS);
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <head>
-<link rel="stylesheet" href="/styles/layoutcrm.css" type="text/css" />
+<link rel="stylesheet" href="/resources/templates/ADL/main.css" type="text/css" />
 <link rel="stylesheet" href="/bootstrap-3.3.5-dist/css/bootstrap.min.css">
 <link rel="stylesheet" href="/bootstrap-3.3.5-dist/css/bootstrap-theme.min.css">
 <link rel="stylesheet" type="text/css" href="/resources/lib/DataTable/datatables.min.css"/>

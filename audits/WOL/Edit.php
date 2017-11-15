65faed849163e6473f93245d9b2e6a3d71b37058
Michael <michael@adl-crm.uk>
2017-11-15 15:01:28 +0000

    Changes to app directory structure (font-awesome).

--- audits/WOL/Edit.php
+++ audits/WOL/Edit.php
@@ -299,7 +299,7 @@ if(isset($EXECUTE)) {
 <link rel="stylesheet" href="/styles/layout.css" type="text/css" />
 <link rel="stylesheet" href="/bootstrap-3.3.5-dist/css/bootstrap.min.css">
 <link rel="stylesheet" href="/bootstrap-3.3.5-dist/css/bootstrap-theme.min.css">
-<link rel="stylesheet" href="/font-awesome/css/font-awesome.min.css">
+<link rel="stylesheet" href="/resources/templates/font-awesome/css/font-awesome.min.css">
 <link href="/img/favicon.ico" rel="icon" type="image/x-icon" />
 
 <script type="text/javascript" language="javascript" src="/js/jquery/jquery-3.0.0.min.js"></script>

15b852f1f62914578620c3d4d4ae53a852ed6f94
Michael <michael@adl-crm.uk>
2017-11-15 14:53:06 +0000

    Changes to app directory structure (font-awesome).

--- messenger/Main.php
+++ messenger/Main.php
@@ -74,7 +74,7 @@ $EXECUTE = filter_input(INPUT_GET, 'EXECUTE', FILTER_SANITIZE_SPECIAL_CHARS);
     <link rel="stylesheet" href="/styles/layoutcrm.css" type="text/css" />
     <link rel="stylesheet" href="/bootstrap-3.3.5-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="/bootstrap-3.3.5-dist/css/bootstrap-theme.min.css">
-    <link rel="stylesheet" href="/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/resources/templates/font-awesome/css/font-awesome.min.css">
      <link rel="stylesheet" href="/summernote-master/dist/summernote.css">
      <link rel="stylesheet" href="/styles/sweet-alert.min.css" />
     <link href="/img/favicon.ico" rel="icon" type="image/x-icon" />

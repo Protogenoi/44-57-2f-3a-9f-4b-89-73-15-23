b01229ccc60ddbb7cebc797b19f98357f0a55365
Michael <michael@adl-crm.uk>
2017-11-15 17:14:06 +0000

    Renamed layoutcrm.css to main.css and update file structure.
    /resources/templates/ADL/main.css

--- Upload.php
+++ Upload.php
@@ -51,7 +51,7 @@ if (isset($fferror)) {
     <title>ADL | Upload</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="/styles/layoutcrm.css" type="text/css" />
+    <link rel="stylesheet" href="/resources/templates/ADL/main.css" type="text/css" />
     <link rel="stylesheet" href="/bootstrap-3.3.5-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="/bootstrap-3.3.5-dist/css/bootstrap-theme.min.css">
     <link rel="stylesheet" href="/resources/templates/font-awesome/css/font-awesome.min.css">

4bb7164460de0c4ccc74c6550271593bfa4f4262
Michael <michael@adl-crm.uk>
2017-11-16 14:02:16 +0000

    Changes to app directory structure (bootstrap-3.3.5-dist).

--- tests/index.php
+++ tests/index.php
@@ -33,7 +33,7 @@ $error = $my_access->the_msg;
         <meta charset="UTF-8">
         <title>ADL CRM | Login</title>
         <link rel="stylesheet" href="/resources/templates/ADL/loginpage.css" type="text/css" />
-        <link rel="stylesheet" href="/bootstrap-3.3.5-dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/resources/templates/bootstrap-3.3.5-dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
         <link href="/img/favicon.ico" rel="icon" type="image/x-icon" />
         <style>

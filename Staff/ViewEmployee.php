eab570e9c44817d0b2008ce2520880ff75d97da2
Michael <michael@adl-crm.uk>
2017-11-16 12:08:52 +0000

    Changes to app directory structure (sweet-alert-min.css).

--- Staff/ViewEmployee.php
+++ Staff/ViewEmployee.php
@@ -97,7 +97,7 @@ $HOL_REF= filter_input(INPUT_GET, 'HOL_REF', FILTER_SANITIZE_SPECIAL_CHARS);
 <link rel="stylesheet" href="/bootstrap-3.3.5-dist/cosmo/bootstrap.min.css">
 <link rel="stylesheet" href="/bootstrap-3.3.5-dist/cosmo/bootstrap.css">
 <link rel="stylesheet" href="/resources/templates/font-awesome/css/font-awesome.min.css">
-<link rel="stylesheet" href="/styles/sweet-alert.min.css" />
+<link rel="stylesheet" href="/resources/lib/sweet-alert/sweet-alert.min.css" />
 <link rel="stylesheet" href="/js/jquery-ui-1.11.4/jquery-ui.min.css" />
 <link rel="stylesheet" href="/styles/Notices.css" />
 <link rel="stylesheet" type="text/css" href="/resources/lib/clockpicker-gh-pagesdist/jquery-clockpicker.min.css">

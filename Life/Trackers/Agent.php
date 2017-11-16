9a2c0d1e9abc3bb9fadc2b1968b50cbdd6ab4c59
Michael <michael@adl-crm.uk>
2017-11-16 13:12:08 +0000

    Changes to app directory structure (Notices.css).

--- Life/Trackers/Agent.php
+++ Life/Trackers/Agent.php
@@ -42,7 +42,7 @@ $Today_TIME = date("h:i:s");
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="/bootstrap-3.3.5-dist/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="/bootstrap-3.3.5-dist/css/bootstrap-theme.min.css">
-    <link rel="stylesheet" type="text/css" href="/styles/Notices.css">
+    <link rel="stylesheet" type="text/css" href="/resources/templates/ADL/Notices.css">
     <link rel="stylesheet" type="text/css" href="/resources/templates/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="/resources/lib/sweet-alert/sweet-alert.min.css" />
     <link rel="stylesheet" type="text/css" href="/js/jquery-ui-1.11.4/jquery-ui.min.css" />

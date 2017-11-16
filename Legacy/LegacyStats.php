a31d22dc100108940679b17cd87d7e059cbe67b1
Michael <michael@adl-crm.uk>
2017-11-16 17:47:12 +0000

    Changes to app directory structure (jquery-ui-1.11.4/).

--- Legacy/LegacyStats.php
+++ Legacy/LegacyStats.php
@@ -87,7 +87,7 @@ if (isset($fferror)) {
         <link rel="stylesheet" href="/resources/templates/bootstrap-3.3.5-dist/css/bootstrap-theme.min.css">
         <link rel="stylesheet" type="text/css" href="/resources/lib/DataTable/datatables.min.css"/>
         <link rel="stylesheet" href="/resources/templates/font-awesome/css/font-awesome.min.css">
-        <link rel="stylesheet" type="text/css" href="/js/jquery-ui-1.11.4/jquery-ui.min.css" />
+        <link rel="stylesheet" type="text/css" href="/resources/lib/jquery-ui-1.11.4/jquery-ui.min.css" />
     <link rel="stylesheet" type="text/css" href="//cdn.oesmith.co.uk/morris-0.5.1.css">
     <style>.ui-datepicker { 
             z-index:1151 !important;
@@ -234,7 +234,7 @@ print("<br><div class=\"notice notice-danger\" role=\"alert\"><strong><i class=\
     
     
         <script type="text/javascript" language="javascript" src="/js/jquery/jquery-3.0.0.min.js"></script>
-        <script type="text/javascript" language="javascript" src="/js/jquery-ui-1.11.4/external/jquery/jquery.js"></script>
+        <script type="text/javascript" language="javascript" src="/resources/lib/jquery-ui-1.11.4/external/jquery/jquery.js"></script>
         <script type="text/javascript" src="/resources/lib/DataTable/datatables.min.js"></script>
         <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
         <script src="/resources/templates/bootstrap-3.3.5-dist/js/bootstrap.min.js"></script> 

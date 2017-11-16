098e313fc8407fb1790ceea02fc310385f200eff
Michael <michael@adl-crm.uk>
2017-11-16 17:49:09 +0000

    Changes to app directory structure (jquery-ui-1.11.4/).

--- email/KeyFactsEmail.php
+++ email/KeyFactsEmail.php
@@ -316,7 +316,7 @@ if ($ffkeyfactsemail == '0') {
         </div>
     </div>
     <script type="text/javascript" language="javascript" src="/js/jquery/jquery-3.0.0.min.js"></script>
-    <script type="text/javascript" language="javascript" src="/js/jquery-ui-1.11.4/jquery-ui.min.js"></script>
+    <script type="text/javascript" language="javascript" src="/resources/lib/jquery-ui-1.11.4/jquery-ui.min.js"></script>
     <script src="/resources/templates/bootstrap-3.3.5-dist/js/bootstrap.min.js"></script> 
     <script src="/js/sweet-alert.min.js"></script>
     <script>

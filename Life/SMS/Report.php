50b2d808ec2e0cc87a823e28749426f3b197d852
Michael <michael@adl-crm.uk>
2017-11-16 14:07:57 +0000

    Changes to app directory structure (bootstrap-3.3.5-dist).

--- Life/SMS/Report.php
+++ Life/SMS/Report.php
@@ -34,8 +34,8 @@ if (!in_array($hello_name, $Level_3_Access, true)) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <head>
         <link rel="stylesheet" href="/resources/templates/ADL/main.css" type="text/css" />
-        <link rel="stylesheet" href="/bootstrap-3.3.5-dist/css/bootstrap.min.css">
-        <link rel="stylesheet" href="/bootstrap-3.3.5-dist/css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="/resources/templates/bootstrap-3.3.5-dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/resources/templates/bootstrap-3.3.5-dist/css/bootstrap-theme.min.css">
         <link rel="stylesheet" href="/resources/templates/font-awesome/css/font-awesome.min.css">
         <link href="/img/favicon.ico" rel="icon" type="image/x-icon" />
     </head>
@@ -182,7 +182,7 @@ WHERE
         <script type="text/javascript" language="javascript" src="/js/jquery/jquery-3.0.0.min.js"></script>
         <script type="text/javascript" language="javascript" src="/js/jquery-ui-1.11.4/jquery-ui.min.js"></script>
         <script type="text/javascript" language="javascript" src="/js/jquery-ui-1.11.4/external/jquery/jquery.js"></script>
-        <script src="/bootstrap-3.3.5-dist/js/bootstrap.min.js"></script> 
+        <script src="/resources/templates/bootstrap-3.3.5-dist/js/bootstrap.min.js"></script> 
         <script type="text/javascript">
             $(document).ready(function () {
                 $('#LOADING').modal('show');

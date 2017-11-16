13927b37b6dd5dd8f9fc1698bedf09018e91f952
Michael <michael@adl-crm.uk>
2017-11-16 17:30:29 +0000

    Changes to app directory structure (jquery-ui-1.11.4/).

--- CRMmain.php
+++ CRMmain.php
@@ -137,8 +137,8 @@ if (in_array($hello_name, $Closer_Access, true)) {
         #map { height: 300px; width: 100% }
     </style>
     <script type="text/javascript" language="javascript" src="js/jquery/jquery-3.0.0.min.js"></script>
-    <script type="text/javascript" language="javascript" src="js/jquery-ui-1.11.4/jquery-ui.min.js"></script>
-    <script type="text/javascript" language="javascript" src="js/jquery-ui-1.11.4/external/jquery/jquery.js"></script>
+    <script type="text/javascript" language="javascript" src="/resources/lib/jquery-ui-1.11.4/jquery-ui.min.js"></script>
+    <script type="text/javascript" language="javascript" src="/resources/lib/jquery-ui-1.11.4/external/jquery/jquery.js"></script>
     <script src="/resources/templates/bootstrap-3.3.5-dist/js/bootstrap.min.js"></script>
 </head>
 <body>

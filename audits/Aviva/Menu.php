1f065ccb5617b8db983f0b6f1264c30d083f96b8
Michael <michael@adl-crm.uk>
2017-11-16 12:14:42 +0000

    Changes to app directory structure (sweet-alert-min.css).

--- audits/Aviva/Menu.php
+++ audits/Aviva/Menu.php
@@ -79,7 +79,7 @@ $RETURN= filter_input(INPUT_GET, 'RETURN', FILTER_SANITIZE_SPECIAL_CHARS);
 <link rel="stylesheet" href="/bootstrap-3.3.5-dist/css/bootstrap.min.css">
 <link rel="stylesheet" href="/bootstrap-3.3.5-dist/css/bootstrap-theme.min.css">
 <link rel="stylesheet" href="/resources/templates/font-awesome/css/font-awesome.min.css">
-<link rel="stylesheet" href="/styles/sweet-alert.min.css" />
+<link rel="stylesheet" href="/resources/lib/sweet-alert/sweet-alert.min.css" />
 <link href="/img/favicon.ico" rel="icon" type="image/x-icon" />
 </head>
 <body>

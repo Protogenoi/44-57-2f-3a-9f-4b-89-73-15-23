d91ef95e21a289411f7f1c081378ce65d764a519
Michael <michael@adl-crm.uk>
2017-11-17 12:51:11 +0000

    Changes to app directory structure (jquery).

--- PBA/AddNewClient.php
+++ PBA/AddNewClient.php
@@ -88,7 +88,7 @@ if (isset($fferror)) {
 <link rel="stylesheet" href="/resources/templates/font-awesome/css/font-awesome.min.css">
 <link href="/img/favicon.ico" rel="icon" type="image/x-icon" />
 
-<script type="text/javascript" language="javascript" src="/js/jquery/jquery-3.0.0.min.js"></script>
+<script type="text/javascript" language="javascript" src="/resources/lib/jquery/jquery-3.0.0.min.js"></script>
 <script src="/resources/templates/bootstrap-3.3.5-dist/js/bootstrap.min.js"></script>
 
 

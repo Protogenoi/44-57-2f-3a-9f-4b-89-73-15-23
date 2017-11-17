e6a1045647979b502491431fff76124ce1517ac2
Michael <michael@adl-crm.uk>
2017-11-17 12:45:47 +0000

    Changes to app directory structure (jquery).

--- error/404.php
+++ error/404.php
@@ -92,7 +92,7 @@ include('../includes/adlfunctions.php'); ?>
         </div>
     </div>
 </div>
-<script type="text/javascript" language="javascript" src="/js/jquery/jquery-3.0.0.min.js"></script>
+<script type="text/javascript" language="javascript" src="/resources/lib/jquery/jquery-3.0.0.min.js"></script>
 <script src="/resources/templates/bootstrap-3.3.5-dist/js/bootstrap.min.js"></script>
 </body>
 </html>

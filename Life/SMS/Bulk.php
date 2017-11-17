368ba91b4bbff7540d038ddfc559062f412276ae
Michael <michael@adl-crm.uk>
2017-11-17 14:47:59 +0000

    Changed external resource to internal jquery-ui-js

--- Life/SMS/Bulk.php
+++ Life/SMS/Bulk.php
@@ -167,7 +167,7 @@ if(isset($RETURN)) {
         <script type="text/javascript" language="javascript" src="/resources/lib/jquery/jquery-3.0.0.min.js"></script>
         <script type="text/javascript" language="javascript" src="/resources/lib/jquery-ui-1.11.4/external/jquery/jquery.js"></script>
         <script type="text/javascript" src="/resources/lib/DataTable/datatables.min.js"></script>
-        <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+        <script src="/resources/lib/jquery-ui-1.11.4/jquery-ui.min.js"></script>
         <script src="/resources/templates/bootstrap-3.3.5-dist/js/bootstrap.min.js"></script> 
         
         <script type="text/javascript">

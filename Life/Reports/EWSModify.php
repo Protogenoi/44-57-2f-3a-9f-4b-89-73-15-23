dca847f0e076b02199ccf16faf248fa53d8dd350
Michael <michael@adl-crm.uk>
2017-11-17 09:33:22 +0000

    Changes to app directory structure (sweet-alert.min.js). .

--- Life/Reports/EWSModify.php
+++ Life/Reports/EWSModify.php
@@ -429,7 +429,7 @@ if($ffews=='0') {
 
     <script type="text/javascript" language="javascript" src="/js/jquery/jquery-3.0.0.min.js"></script>
     <script type="text/javascript" language="javascript" src="/resources/lib/jquery-ui-1.11.4/jquery-ui.min.js"></script>
-            <script src="/js/sweet-alert.min.js"></script>
+            <script src="/resources/lib/sweet-alert/sweet-alert.min.js"></script>
     <script>
         document.querySelector('#EWSSUBMIT').addEventListener('submit', function(e) {
             var form = this;

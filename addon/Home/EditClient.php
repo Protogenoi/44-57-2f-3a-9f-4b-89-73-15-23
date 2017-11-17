ff9f894b49880edce6d1998cec84f6e7ff6a4bee
Michael <michael@adl-crm.uk>
2017-11-17 14:29:45 +0000

    Pointed external sources to internal

--- addon/Home/EditClient.php
+++ addon/Home/EditClient.php
@@ -109,11 +109,10 @@ if (isset($CID)) {
         <link rel="stylesheet" href="/resources/lib/sweet-alert/sweet-alert.min.css" />
         <link href="/img/favicon.ico" rel="icon" type="image/x-icon" />
         
-    <script type="text/javascript" language="javascript" src="js/jquery/jquery-3.0.0.min.js"></script>
-    <script src="//code.jquery.com/jquery-1.10.2.js"></script>
-    <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
-        <script src="/resources/lib/sweet-alert/sweet-alert.min.js"></script>
-        <script src="/resources/templates/bootstrap-3.3.5-dist/js/bootstrap.min.js"></script>
+    <script type="text/javascript" language="javascript" src="/js/jquery/jquery-3.0.0.min.js"></script>
+    <script src="/resources/lib/jquery-ui-1.11.4/jquery-ui.min.js"></script>
+    <script src="/resources/lib/sweet-alert/sweet-alert.min.js"></script>
+    <script src="/resources/templates/bootstrap-3.3.5-dist/js/bootstrap.min.js"></script>
 
         <script>
             $(function () {

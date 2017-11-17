14dbfcf16a6f2a8f8f65849bc698f8cb574d6c60
Michael <michael@adl-crm.uk>
2017-11-17 09:33:54 +0000

    Changes to app directory structure (sweet-alert.min.js). .

--- Life/Assura/FinancialUpload.php
+++ Life/Assura/FinancialUpload.php
@@ -215,6 +215,6 @@ if (!in_array($hello_name,$Level_8_Access, true)) {
     <script type="text/javascript" language="javascript" src="/js/jquery/jquery-3.0.0.min.js"></script>
     <script type="text/javascript" language="javascript" src="/resources/lib/jquery-ui-1.11.4/jquery-ui.min.js"></script>
     <script src="../../resources/templates/bootstrap-3.3.5-dist/js/bootstrap.min.js"></script>
-    <script src="../../js/sweet-alert.min.js"></script>
+    <script src="../../resources/lib/sweet-alert/sweet-alert.min.js"></script>
 </body>
 </html>

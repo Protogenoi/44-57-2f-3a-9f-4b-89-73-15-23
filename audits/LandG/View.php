aca6ddd5ce18f55623e178e183326ed306ae1377
Michael <michael@adl-crm.uk>
2017-11-17 11:18:07 +0000

    Updated JS links.

--- audits/LandG/View.php
+++ audits/LandG/View.php
@@ -82,11 +82,10 @@ $EXECUTE = filter_input(INPUT_GET, 'EXECUTE', FILTER_SANITIZE_NUMBER_INT);
         <link rel="stylesheet" href="/resources/templates/bootstrap-3.3.5-dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="/resources/templates/bootstrap-3.3.5-dist/css/bootstrap-theme.min.css">
         <link rel="stylesheet" href="/resources/templates/font-awesome/css/font-awesome.min.css">
-        <script type="text/javascript" language="javascript" src="/js/jquery.dataTables.min.js"></script>
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-        <script src="/resources/templates/bootstrap-3.3.5-dist/js/bootstrap.min.js"></script>
         <link href="/img/favicon.ico" rel="icon" type="image/x-icon" />
-        <script src="/js/jquery-1.4.min.js"></script>
+        
+        <script type="text/javascript" language="javascript" src="/js/jquery/jquery-3.0.0.min.js"></script>
+        <script src="/resources/templates/bootstrap-3.3.5-dist/js/bootstrap.min.js"></script>
         <script>
             function textAreaAdjust(o) {
                 o.style.height = "1px";

e762136ae8a89146ed2462924743d308c52157dc
Michael <michael@adl-crm.uk>
2017-11-20 13:18:18 +0000

    Updated for token checks.

--- Life/Policy.php
+++ Life/Policy.php
@@ -50,6 +50,33 @@ require_once(__DIR__ . '/../includes/adlfunctions.php');
 require_once(__DIR__ . '/../classes/database_class.php');
 require_once(__DIR__ . '/class/Policy.php');
 
+        require_once(__DIR__ . '/../classes/database_class.php');
+        require_once(__DIR__ . '/../class/login/login.php');
+        
+        $CHECK_USER_LOGIN = new UserActions($hello_name,"NoToken");
+        
+        $CHECK_USER_LOGIN->SelectToken();
+        $CHECK_USER_LOGIN->CheckAccessLevel();
+   
+        $OUT=$CHECK_USER_LOGIN->SelectToken();
+        
+        if(isset($OUT['TOKEN_SELECT']) && $OUT['TOKEN_SELECT']!='NoToken') {
+        
+        $TOKEN=$OUT['TOKEN_SELECT'];
+                
+        }
+        
+        $USER_ACCESS_LEVEL=$CHECK_USER_LOGIN->CheckAccessLevel();
+        
+        $ACCESS_LEVEL=$USER_ACCESS_LEVEL['ACCESS_LEVEL'];
+        
+        if($ACCESS_LEVEL < 3) {
+            
+        header('Location: /../../index.php?AccessDenied&USER='.$hello_name.'&COMPANY='.$COMPANY_ENTITY);
+        die;    
+            
+        }
+
 if ($ffanalytics == '1') {
     require_once(__DIR__ . '/../php/analyticstracking.php');
 }
@@ -475,7 +502,7 @@ $data=$NewPolicy->selectPolicy();
                                 </div> 
                                 </div>  
                                             <script>var options = {
-                                                            url: "../JSON/Closers.php",
+                                                            url: "../JSON/Agents.php?EXECUTE=1&USER=<?php echo $hello_name; ?>&TOKEN=<?php echo $TOKEN; ?>",
                                                             getValue: "full_name",
                                                             list: {
                                                                 match: {
@@ -496,7 +523,7 @@ $data=$NewPolicy->selectPolicy();
                                 </div> 
                                 </div>  
                                             <script>var options = {
-                                                            url: "../JSON/Agents.php",
+                                                            url: "../JSON/Agents.php?EXECUTE=1&USER=<?php echo $hello_name; ?>&TOKEN=<?php echo $TOKEN; ?>",
                                                             getValue: "full_name",
                                                             list: {
                                                                 match: {

f0c2dcece7f8183634ca17045ceb0fee01c2de85
Michael <michael@adl-crm.uk>
2017-11-21 14:29:24 +0000

    Fixed link.

--- tests/index.php
+++ tests/index.php
@@ -218,5 +218,5 @@ if($TRACKED_IP=='81.145.167.66') {
         </script>
         <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.9/cookieconsent.min.js"></script>
 
-<?php include('php/Holidays.php'); ?>
+<?php include('app/Holidays.php'); ?>
 </html>                                		

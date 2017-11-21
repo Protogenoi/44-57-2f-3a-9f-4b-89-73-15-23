dd975654c8d42f7cfece18f5f053cab371b962d8
Michael <michael@adl-crm.uk>
2017-11-21 11:21:22 +0000

    Updated links (Moved datatables JSON for SearchPolicies.php).
    Renamed getpolicy -> SearchPolicies.php

---
+++ addon/Home/JSON/SearchPolicies.php
@@ -0,0 +1,70 @@
+<?php
+/*
+ * ------------------------------------------------------------------------
+ *                               ADL CRM
+ * ------------------------------------------------------------------------
+ * 
+ * Copyright © 2017 ADL CRM All rights reserved.
+ * 
+ * Unauthorised copying of this file, via any medium is strictly prohibited.
+ * Unauthorised distribution of this file, via any medium is strictly prohibited.
+ * Unauthorised modification of this code is strictly prohibited.
+ * 
+ * Proprietary and confidential
+ * 
+ * Written by Michael Owen <michael@adl-crm.uk>, 2017
+ * 
+ * ADL CRM makes use of the following third party open sourced software/tools:
+ *  DataTables - https://github.com/DataTables/DataTables
+ *  EasyAutocomplete - https://github.com/pawelczak/EasyAutocomplete
+ *  PHPMailer - https://github.com/PHPMailer/PHPMailer
+ *  ClockPicker - https://github.com/weareoutman/clockpicker
+ *  fpdf17 - http://www.fpdf.org
+ *  summernote - https://github.com/summernote/summernote
+ *  Font Awesome - https://github.com/FortAwesome/Font-Awesome
+ *  Bootstrap - https://github.com/twbs/bootstrap
+ *  jQuery UI - https://github.com/jquery/jquery-ui
+ *  Google Dev Tools - https://developers.google.com
+ *  Twitter API - https://developer.twitter.com
+ * 
+*/  
+
+$USER= filter_input(INPUT_GET, 'USER', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$TOKEN= filter_input(INPUT_GET, 'TOKEN', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+if(isset($USER) && $TOKEN) {
+    
+    require_once(__DIR__ . '/../../../classes/database_class.php');
+    require_once(__DIR__ . '/../../../class/login/login.php');
+
+        $CHECK_USER_TOKEN = new UserActions($USER,$TOKEN);
+        $CHECK_USER_TOKEN->CheckToken();
+        $OUT=$CHECK_USER_TOKEN->CheckToken();
+        
+        if(isset($OUT['TOKEN_CHECK']) && $OUT['TOKEN_CHECK']=='Bad') {
+         header('Location: /../../../../CRMmain.php?AccessDenied');  
+        }
+
+        if(isset($OUT['TOKEN_CHECK']) && $OUT['TOKEN_CHECK']=='Good') {
+
+$EXECUTE = filter_input(INPUT_GET, 'EXECUTE', FILTER_SANITIZE_SPECIAL_CHARS);
+
+if (isset($EXECUTE)) {
+    require_once(__DIR__ . '/../../../includes/ADL_PDO_CON.php');
+
+    if ($EXECUTE == 'Home') {
+        
+        $query = $pdo->prepare("SELECT client_id, client_name, added_date, insurer, policy_number, status FROM home_policy");
+        $query->execute()or die(print_r($query->errorInfo(), true));
+        json_encode($results['aaData'] = $query->fetchAll(PDO::FETCH_ASSOC));
+        echo json_encode($results);
+        
+    }
+}
+} }
+else {
+    header('Location: /../../../../CRMmain.php?AccessDenied');
+    die;
+}
+?>
+

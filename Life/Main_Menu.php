083eec655d5bc440f25cdc06eb2b7882ed0520b7
Michael <michael@adl-crm.uk>
2017-11-22 14:30:36 +0000

    Directory structure update (AddClient.php - > /app/)

--- Life/Main_Menu.php
+++ Life/Main_Menu.php
@@ -105,7 +105,7 @@ if (isset($fferror)) {
                     <ul class="ca-menu">
                         <?php if (in_array($hello_name, $Level_3_Access, true)) { ?>
                             <li>
-                                <a href="/AddClient.php">
+                                <a href="/app/AddClient.php">
                                     <span class="ca-icon"><i class="fa fa-user-plus"></i></span>
                                     <div class="ca-content">
                                         <h2 class="ca-main">Add New<br/> Client</h2>

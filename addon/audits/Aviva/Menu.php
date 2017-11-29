f369887570f7da0ecd80e53b908b3407946bbe11
Michael <michael@adl-crm.uk>
2017-11-29 11:07:48 +0000

    Directory structure update (audits /addon/audits/).

---
+++ addon/audits/Aviva/Menu.php
@@ -0,0 +1,213 @@
+<?php 
+/*
+ * ------------------------------------------------------------------------
+ *                               ADL CRM
+ * ------------------------------------------------------------------------
+ * 
+ * Copyright © 2017 ADL CRM All rights reserved.
+ * 
+ * Unauthorised copying of this file, via any medium is strictly prohibited.
+ * Unauthorised distribution of this file, via any medium is strictly prohibited.
+ * Unauthorised modification of this code is strictly prohibited.
+ * 
+ * Proprietary and confidential
+ * 
+ * Written by Michael Owen <michael@adl-crm.uk>, 2017
+ * 
+ * ADL CRM makes use of the following third party open sourced software/tools:
+ *  DataTables - https://github.com/DataTables/DataTables
+ *  EasyAutocomplete - https://github.com/pawelczak/EasyAutocomplete
+ *  PHPMailer - https://github.com/PHPMailer/PHPMailer
+ *  ClockPicker - https://github.com/weareoutman/clockpicker
+ *  fpdf17 - http://www.fpdf.org
+ *  summernote - https://github.com/summernote/summernote
+ *  Font Awesome - https://github.com/FortAwesome/Font-Awesome
+ *  Bootstrap - https://github.com/twbs/bootstrap
+ *  jQuery UI - https://github.com/jquery/jquery-ui
+ *  Google Dev Tools - https://developers.google.com
+ *  Twitter API - https://developer.twitter.com
+ * 
+*/  
+
+require_once(__DIR__ . '/../../../classes/access_user/access_user_class.php');
+$page_protect = new Access_user;
+$page_protect->access_page(filter_input(INPUT_SERVER,'PHP_SELF', FILTER_SANITIZE_SPECIAL_CHARS), "", 2);
+$hello_name = ($page_protect->user_full_name != "") ? $page_protect->user_full_name : $page_protect->user;
+
+$USER_TRACKING=0;
+
+require_once(__DIR__ . '/../../../includes/user_tracking.php'); 
+
+require_once(__DIR__ . '/../../../includes/time.php');
+
+if(isset($FORCE_LOGOUT) && $FORCE_LOGOUT== 1) {
+    $page_protect->log_out();
+}
+
+require_once(__DIR__ . '/../../../includes/adl_features.php');
+require_once(__DIR__ . '/../../../includes/Access_Levels.php');
+require_once(__DIR__ . '/../../../includes/adlfunctions.php');
+
+if ($ffanalytics == '1') {
+    require_once(__DIR__ . '/../../../app/analyticstracking.php');
+}
+
+        require_once(__DIR__ . '/../../../classes/database_class.php');
+        require_once(__DIR__ . '/../../../class/login/login.php');
+        
+        $CHECK_USER_LOGIN = new UserActions($hello_name,"NoToken");
+        $CHECK_USER_LOGIN->CheckAccessLevel();
+        
+        $USER_ACCESS_LEVEL=$CHECK_USER_LOGIN->CheckAccessLevel();
+        
+        $ACCESS_LEVEL=$USER_ACCESS_LEVEL['ACCESS_LEVEL'];
+        
+        if($ACCESS_LEVEL < 2) {
+            
+        header('Location: /../../../../index.php?AccessDenied&USER='.$hello_name.'&COMPANY='.$COMPANY_ENTITY);
+        die;    
+            
+        }
+
+$RETURN= filter_input(INPUT_GET, 'RETURN', FILTER_SANITIZE_SPECIAL_CHARS);
+?>
+<!DOCTYPE html>
+<html>
+<title>ADL | Aviva Menu</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="/resources/templates/ADL/main.css" type="text/css" />
+<link rel="stylesheet" href="/resources/templates/bootstrap-3.3.5-dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="/resources/templates/bootstrap-3.3.5-dist/css/bootstrap-theme.min.css">
+<link rel="stylesheet" href="/resources/templates/font-awesome/css/font-awesome.min.css">
+<link rel="stylesheet" href="/resources/lib/sweet-alert/sweet-alert.min.css" />
+<link href="/img/favicon.ico" rel="icon" type="image/x-icon" />
+</head>
+<body>
+
+<?php 
+
+require_once(__DIR__ . '/../../../app/Holidays.php');
+require_once(__DIR__ . '/../../../includes/navbar.php');
+?>
+    
+    <div class="container">
+        <div class="notice notice-default" role="alert"><strong><center><span class="label label-warning"></span> Aviva Audits</center></strong></div>
+        
+        <?php
+        if(isset($RETURN)) {
+            $GRADE= filter_input(INPUT_GET, 'GRADE', FILTER_SANITIZE_SPECIAL_CHARS);
+            
+ switch ($GRADE) {
+    case "Red":
+        $NOTICE_COLOR="danger";
+
+        break;
+    case "Amber":
+        $NOTICE_COLOR="warning";
+        break;
+    case "Green":
+        $NOTICE_COLOR="success";
+        break;
+    default:
+        $NOTICE_COLOR="info";
+       
+}             
+            
+            if($RETURN=='UPDATED'){
+                echo "<div class=\"notice notice-success\" role=\"alert\"><strong><i class=\"fa fa-edit fa-lg\"></i> Success:</strong> Aviva Audit Updated!</div>";
+                echo "<div class=\"notice notice-$NOTICE_COLOR\" role=\"alert\"><strong>Aviva: Audit grade $GRADE</strong></div>";
+                
+            }
+            if($RETURN=='ADDED') {
+                echo "<div class=\"notice notice-success\" role=\"alert\"><strong><i class=\"fa fa-check-circle-o\"></i> Success:</strong> Aviva Audit Added!</div>";
+                echo "<div class=\"notice notice-$NOTICE_COLOR\" role=\"alert\"><strong>Aviva: Audit grade $GRADE</strong></div>";
+                
+            }
+        }
+        ?>
+        
+        
+        <br>
+        <center>
+            <div class="btn-group">
+                <a href="/audits/main_menu.php" class="btn btn-default"><i class="fa fa-arrow-circle-o-left"></i> Audit Menu</a>
+                <a href="Audit.php" class="btn btn-primary"><i class="fa fa-plus"></i> Aviva Audit</a>
+                <a href="Search.php" class="btn btn-info "><i class="fa fa-search"></i> Search Audits</a>
+            </div>
+        </center>
+<br>
+    
+    <?php                
+                $query2 = $pdo->prepare("SELECT aviva_audit_policy, aviva_audit_id, aviva_audit_added_date, aviva_audit_closer, aviva_audit_added_by, aviva_audit_grade, aviva_audit_updated_by, aviva_audit_updated_date from aviva_audit where aviva_audit_added_by=:hello and aviva_audit_added_date between DATE_ADD(CURDATE(), INTERVAL 1-DAYOFWEEK(CURDATE()) DAY) AND DATE_ADD(CURDATE(), INTERVAL 7-DAYOFWEEK(CURDATE()) DAY) or aviva_audit_updated_date between DATE_ADD(CURDATE(), INTERVAL 1-DAYOFWEEK(CURDATE()) DAY) AND DATE_ADD(CURDATE(), INTERVAL 7-DAYOFWEEK(CURDATE()) DAY) AND aviva_audit_updated_by =:hello ORDER BY aviva_audit_added_date DESC");
+                $query2->bindParam(':hello', $hello_name, PDO::PARAM_STR, 12);                
+                $query2->execute();
+                $i=0;
+                if ($query2->rowCount()>0) {
+                                    echo "<table align=\"center\" class=\"table\">";
+                
+                echo "<thead>
+	<tr>
+	<th colspan= 12>Your Recent Audits</th>
+	</tr>
+    	<tr>
+	<th>ID</th>
+	<th>Policy Number</th>
+	<th>Submitted</th>
+	<th>Closer</th>
+	<th>Auditor</th>
+	<th>Grade</th>
+	<th>Edited By</th>
+	<th>Date Edited</th>
+	<th colspan='5'>Options</th>
+	</tr>
+	</thead>";
+                    while ($result=$query2->fetch(PDO::FETCH_ASSOC)){
+                        $i++;
+                        $AUDIT_ID=$result['aviva_audit_id'];
+                        
+                        switch( $result['aviva_audit_grade'] ) {
+                            case("Red"):
+                                $class = 'Red';
+                                break;
+                            case("Green"):
+                                $class = 'Green';
+                                break;
+                            case("Amber"):
+                                $class = 'Amber';
+                                break;
+                            case("Save"):
+                                $class = 'Purple';
+                                break;
+                            default:
+                                }
+                                
+                                echo '<tr class='.$class.'>';
+                                echo "<td>".$result['aviva_audit_id']."</td>";
+                                echo "<td>".$result['aviva_audit_policy']."</td>";
+                                echo "<td>".$result['aviva_audit_added_date']."</td>";
+                                echo "<td>".$result['aviva_audit_closer']."</td>";
+                                echo "<td>".$result['aviva_audit_added_by']."</td>";
+                                echo "<td>".$result['aviva_audit_grade']."</td>";
+                                echo "<td>".$result['aviva_audit_updated_by']."</td>";
+                                echo "<td>".$result['aviva_audit_updated_date']."</td>";
+   echo "<td><a href='Audit.php?EXECUTE=EDIT&AUDITID=$AUDIT_ID' class='btn btn-warning btn-xs'><span class='glyphicon glyphicon-pencil'></span></a></td>";
+   echo "<td><a href='View.php?EXECUTE=VIEW&AUDITID=$AUDIT_ID' class='btn btn-info btn-xs'><span class='glyphicon glyphicon-eye-open'></span></a></td>";
+    echo "</tr>";
+
+	}echo "</table>";
+} else {
+    echo "<br><div class=\"notice notice-warning\" role=\"alert\"><strong>Info!</strong> No Aviva Audits found</div>";
+}
+
+?>
+    
+
+   
+</div>
+
+    <script type="text/javascript" language="javascript" src="/resources/lib/jquery/jquery-3.0.0.min.js"></script>
+    <script type="text/javascript" language="javascript" src="/resources/lib/jquery-ui-1.11.4/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="/resources/templates/bootstrap-3.3.5-dist/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ addon/audits/audit_search.php
@@ -0,0 +1,245 @@
+<?php
+/*
+ * ------------------------------------------------------------------------
+ *                               ADL CRM
+ * ------------------------------------------------------------------------
+ * 
+ * Copyright © 2017 ADL CRM All rights reserved.
+ * 
+ * Unauthorised copying of this file, via any medium is strictly prohibited.
+ * Unauthorised distribution of this file, via any medium is strictly prohibited.
+ * Unauthorised modification of this code is strictly prohibited.
+ * 
+ * Proprietary and confidential
+ * 
+ * Written by Michael Owen <michael@adl-crm.uk>, 2017
+ * 
+ * ADL CRM makes use of the following third party open sourced software/tools:
+ *  DataTables - https://github.com/DataTables/DataTables
+ *  EasyAutocomplete - https://github.com/pawelczak/EasyAutocomplete
+ *  PHPMailer - https://github.com/PHPMailer/PHPMailer
+ *  ClockPicker - https://github.com/weareoutman/clockpicker
+ *  fpdf17 - http://www.fpdf.org
+ *  summernote - https://github.com/summernote/summernote
+ *  Font Awesome - https://github.com/FortAwesome/Font-Awesome
+ *  Bootstrap - https://github.com/twbs/bootstrap
+ *  jQuery UI - https://github.com/jquery/jquery-ui
+ *  Google Dev Tools - https://developers.google.com
+ *  Twitter API - https://developer.twitter.com
+ * 
+*/  
+
+require_once(__DIR__ . '/../../classes/access_user/access_user_class.php');
+$page_protect = new Access_user;
+$page_protect->access_page(filter_input(INPUT_SERVER,'PHP_SELF', FILTER_SANITIZE_SPECIAL_CHARS), "", 2);
+$hello_name = ($page_protect->user_full_name != "") ? $page_protect->user_full_name : $page_protect->user;
+
+$USER_TRACKING=0;
+
+require_once(__DIR__ . '/../../includes/user_tracking.php'); 
+
+require_once(__DIR__ . '/../../includes/time.php');
+
+if(isset($FORCE_LOGOUT) && $FORCE_LOGOUT== 1) {
+    $page_protect->log_out();
+}
+
+require_once(__DIR__ . '/../../includes/adl_features.php');
+require_once(__DIR__ . '/../../includes/Access_Levels.php');
+require_once(__DIR__ . '/../../includes/adlfunctions.php');
+
+    require_once(__DIR__ . '../../classes/database_class.php');
+    require_once(__DIR__ . '../../class/login/login.php');
+
+        $CHECK_USER_LOGIN = new UserActions($hello_name,"NoToken");
+        
+        $CHECK_USER_LOGIN->SelectToken();
+        $CHECK_USER_LOGIN->CheckAccessLevel();
+   
+        $OUT=$CHECK_USER_LOGIN->SelectToken();
+        
+        if(isset($OUT['TOKEN_SELECT']) && $OUT['TOKEN_SELECT']!='NoToken') {
+        
+        $TOKEN=$OUT['TOKEN_SELECT'];
+                
+        }
+        
+        $USER_ACCESS_LEVEL=$CHECK_USER_LOGIN->CheckAccessLevel();
+        
+        $ACCESS_LEVEL=$USER_ACCESS_LEVEL['ACCESS_LEVEL'];
+        
+        if($ACCESS_LEVEL < 2) {
+            
+        header('Location: /../../../index.php?AccessDenied&USER='.$hello_name.'&COMPANY='.$COMPANY_ENTITY);
+        die;    
+            
+        }
+
+if ($ffanalytics == '1') {
+    require_once(__DIR__ . '/../../app/analyticstracking.php');
+}
+
+if (isset($fferror)) {
+    if ($fferror == '1') {
+        ini_set('display_errors', 1);
+        ini_set('display_startup_errors', 1);
+        error_reporting(E_ALL);
+    }
+}
+
+if ($ffaudits == '0') {
+
+    header('Location: /../../../CRMmain.php');
+    die;
+}
+?>
+<!DOCTYPE html>
+<!-- 
+ Copyright (C) ADL CRM - All Rights Reserved
+ Unauthorised copying of this file, via any medium is strictly prohibited
+ Proprietary and confidential
+ Written by Michael Owen <michael@adl-crm.uk>, 2017
+-->
+<html lang="en">
+    <title>ADL | Closer Audit Search</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="/resources/templates/ADL/main.css" type="text/css" />
+        <link rel="stylesheet" href="/resources/templates/bootstrap-3.3.5-dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/resources/templates/bootstrap-3.3.5-dist/css/bootstrap-theme.min.css">
+        <link rel="stylesheet" type="text/css" href="/resources/lib/DataTable/datatables.min.css"/>
+        <link rel="stylesheet" href="/resources/templates/font-awesome/css/font-awesome.min.css">
+        <link href="/img/favicon.ico" rel="icon" type="image/x-icon" />
+</head>
+<body>
+
+<?php
+require_once(__DIR__ . '/../../includes/navbar.php');
+?>
+
+    <div class="container">
+
+        <br>
+
+        <table id="clients" width="auto" cellspacing="0" class="table-condensed">
+            <thead>
+                <tr>
+                    <th></th>
+                    <th>Submitted</th>
+                    <th>ID</th>
+                    <th>Policy</th>
+                    <th>AN Number</th>
+                    <th>Closer</th>
+                    <th>Auditor</th>
+                    <th>Grade</th>
+                    <th>Edit</th>
+                    <th>View</th>
+
+                </tr>
+            </thead>
+            <tfoot>
+                <tr>
+                    <th></th>
+                    <th>Submitted</th>
+                    <th>ID</th>
+                    <th>Policy</th>
+                    <th>AN Number</th>
+                    <th>Closer</th>
+                    <th>Auditor</th>
+                    <th>Grade</th>
+                    <th>Edit</th>
+                    <th>View</th>
+                </tr>
+            </tfoot>
+        </table>
+    </div>
+
+        <script type="text/javascript" language="javascript" src="/resources/lib/jquery/jquery-3.0.0.min.js"></script>
+        <script type="text/javascript" language="javascript" src="/resources/lib/jquery-ui-1.11.4/external/jquery/jquery.js"></script>
+        <script type="text/javascript" src="/resources/lib/DataTable/datatables.min.js"></script>
+        <script src="/resources/lib/jquery-ui-1.11.4/jquery-ui.min.js"></script>
+        <script src="/resources/templates/bootstrap-3.3.5-dist/js/bootstrap.min.js"></script> 
+        <script>
+        function format(d) {
+            return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">' +
+                    '<tr>' +
+                    '<td>Changes:</td>' +
+                    '<td>' + d.date_edited + ' </td>' +
+                    '<td>' + d.edited + ' </td>' +
+                    '</tr>' +
+                    '<tr>' +
+                    '<td>Grade:</td>' +
+                    '<td>' + d.grade + ' </td>' +
+                    '</tr>' +
+                    '<tr>' +
+                    '<td>Answered Correctly:</td>' +
+                    '<td>' + d.total + '/54 </td>' +
+                    '</tr>' +
+                    '</table>';
+        }
+
+        $(document).ready(function () {
+            var table = $('#clients').DataTable({
+                "fnRowCallback": function (nRow, aData, iDisplayIndex, iDisplayIndexFull) {
+                    if (aData["grade"] === "Red") {
+                        $(nRow).addClass('Red');
+                    } else if (aData["grade"] === "Amber") {
+                        $(nRow).addClass('Amber');
+                    } else if (aData["grade"] === "Green") {
+                        $(nRow).addClass('Green');
+                    } else if (aData["grade"] === "SAVED") {
+                        $(nRow).addClass('Purple');
+                    }
+                },
+
+                "response": true,
+                "processing": true,
+                "iDisplayLength": 50,
+                "aLengthMenu": [[5, 10, 25, 50, 100, 125, 150, 200, 500], [5, 10, 25, 50, 100, 125, 150, 200, 500]],
+                "language": {
+                    "processing": "<div></div><div></div><div></div><div></div><div></div>"
+
+                },
+                "ajax": "datatables/AuditSearch.php?AuditType=Closer&USER=<?php echo $hello_name; ?>&TOKEN=<?php echo $TOKEN; ?>",
+                "columns": [
+                    {
+                        "className": 'details-control',
+                        "orderable": false,
+                        "data": null,
+                        "defaultContent": ''
+                    },
+                    {"data": "date_submitted"},
+                    {"data": "id"},
+                    {"data": "policy_number"},
+                    {"data": "an_number"},
+                    {"data": "closer"},
+                    {"data": "auditor"},
+                    {"data": "grade"},
+                    {"data": "id",
+                        "render": function (data, type, full, meta) {
+                            return '<a href="closer_form_edit.php?auditid=' + data + '"><button type=\'submit\' class=\'btn btn-warning btn-xs\'><span class=\'glyphicon glyphicon-pencil\'></span> </button></a>';
+                        }},
+                    {"data": "id",
+                        "render": function (data, type, full, meta) {
+                            return '<a href="closer_form_view.php?auditid=' + data + '"><button type=\'submit\' class=\'btn btn-info btn-xs\'><span class=\'glyphicon glyphicon-eye-open\'></span> </button></a></a>';
+                        }}
+                ],
+                "order": [[1, 'desc']]
+            });
+
+            $('#clients tbody').on('click', 'td.details-control', function () {
+                var tr = $(this).closest('tr');
+                var row = table.row(tr);
+
+                if (row.child.isShown()) {
+                    row.child.hide();
+                    tr.removeClass('shown');
+                } else {
+                    row.child(format(row.data())).show();
+                    tr.addClass('shown');
+                }
+            });
+        });
+    </script>
+</body>
+</html>

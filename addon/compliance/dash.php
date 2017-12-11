f386f6fac46baced377ad76971c201c925e99fec
Michael <michael@adl-crm.uk>
2017-12-11 10:24:45 +0000

    Removed comments and added semicolons to JS.

--- addon/compliance/dash.php
+++ addon/compliance/dash.php
@@ -398,21 +398,17 @@ GROUP BY closer");
             </div>
         </div>
 
-
         <script src="/resources/lib/jquery/jquery-3.0.0.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.2.0/js/tether.min.js" integrity="sha384-Plbmg8JY28KFelvJVai01l8WyZzrYWG825m+cZ0eDDS1f7d/js6ikvy1+X+guPIB" crossorigin="anonymous"></script>
         <script src="/resources/templates/bootstrap/js/bootstrap.min.js"></script>
 
         <script>
-            // Initialize tooltip component
             $(function () {
-                $('[data-toggle="tooltip"]').tooltip()
-            })
-
-            // Initialize popover component
+                $('[data-toggle="tooltip"]').tooltip();
+            });
             $(function () {
-                $('[data-toggle="popover"]').popover()
-            })
+                $('[data-toggle="popover"]').popover();
+            });
         </script> 
 
 

a3bef414eff7d66a5a8b6d33054292a8e58ea66d
Michael <michael@adl-crm.uk>
2017-12-12 18:14:28 +0000

    Updated access level.

--- Life/php/EditPolicy.php
+++ Life/php/EditPolicy.php
@@ -31,7 +31,7 @@
 
 include(filter_input(INPUT_SERVER,'DOCUMENT_ROOT', FILTER_SANITIZE_SPECIAL_CHARS)."/classes/access_user/access_user_class.php");  
 $page_protect = new Access_user;
-$page_protect->access_page(filter_input(INPUT_SERVER,'PHP_SELF', FILTER_SANITIZE_SPECIAL_CHARS), "", 10);
+$page_protect->access_page(filter_input(INPUT_SERVER,'PHP_SELF', FILTER_SANITIZE_SPECIAL_CHARS), "", 3);
 $hello_name = ($page_protect->user_full_name != "") ? $page_protect->user_full_name : $page_protect->user;
 
 $USER_TRACKING=0;

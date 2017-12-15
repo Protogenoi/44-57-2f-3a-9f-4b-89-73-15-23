d3133f0294744a3132aaaa93c2c07af2fcd4ea20
Michael <michael@adl-crm.uk>
2017-12-15 15:48:22 +0000

    Major changes to ViewClient.php renamed and moved to /app/Client.php

--- Life/views/LG-Policies.php
+++ Life/views/LG-Policies.php
@@ -131,8 +131,6 @@
         }
         echo "<td><a href='/Life/ViewPolicy.php?policyID=$PID&search=$search&WHICH_COMPANY=$WHICH_COMPANY' class='btn btn-info btn-xs'><i class='fa fa-eye'></i> </a></td>";
         echo "<td><a href='/Life/EditPolicy.php?id=$PID&search=$search&name=$POL_HOLDER' class='btn btn-warning btn-xs'><i class='fa fa-edit'></i> </a></td>";
-
-        if ($companynamere == 'Bluestone Protect') {
             if (in_array($hello_name, $Level_10_Access, true)) {
 
 
@@ -143,11 +141,9 @@
                                                                                             </form>
                                                                                             </td>";
             }
-        }
-
 
         if (!empty($EWSSTATUS)) {
-            echo "<td><a href='Reports/EWSPolicySearch.php?EWSView=1&search=$search&policy_number=$polref' class='btn btn-success btn-xs'><i class='fa fa-warning'></i> </a></td>";
+            echo "<td><a href='/Life/Reports/EWSPolicySearch.php?EWSView=1&search=$search&policy_number=$polref' class='btn btn-success btn-xs'><i class='fa fa-warning'></i> </a></td>";
         }
         echo "</tr>";
         ?>

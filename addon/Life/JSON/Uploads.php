d5ae6c9483267fb05b6133d477db339a453e4463
Michael <michael@adl-crm.uk>
2018-01-15 12:28:36 +0000

    Added Royal London for upload checking (Policy Application).

--- addon/Life/JSON/Uploads.php
+++ addon/Life/JSON/Uploads.php
@@ -384,7 +384,43 @@ json_encode($results['aaData']=$query->fetchAll(PDO::FETCH_ASSOC));
 
 echo json_encode($results);
 
-    }      
+    }    
+    
+    if($EXECUTE=='11') {
+
+        $query = $pdo->prepare("
+SELECT 
+    client_details.client_id,
+    client_details.submitted_date,
+    CONCAT(title, ' ', first_name, ' ', last_name) AS Name,
+    CONCAT(title2,
+            ' ',
+            first_name2,
+            ' ',
+            last_name2) AS Name2,
+    post_code,
+    client_details.company
+FROM
+    client_details
+        JOIN
+    client_note ON client_note.client_id = client_details.client_id
+WHERE
+    client_details.client_id NOT IN (SELECT 
+            client_id
+        FROM
+            client_note
+        WHERE
+            note_type = 'RLpolicy')
+        AND DATE(client_details.submitted_date) >= '2018-01-01'
+        AND client_details.company ='Royal London'
+GROUP BY client_id
+    ORDER BY client_details.submitted_date DESC");
+$query->execute()or die(print_r($query->errorInfo(), true));
+json_encode($results['aaData']=$query->fetchAll(PDO::FETCH_ASSOC));
+
+echo json_encode($results);
+
+    }    
     
 }
 

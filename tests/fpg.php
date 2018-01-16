ea45da14fa44591e2decf013ad4d7267dcb414c3
Michael <michael@adl-crm.uk>
2018-01-16 21:03:31 +0000

    Keyfacts for FPG.

---
+++ tests/fpg.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="en">
+<title>FPG | BLANK</title>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="styles/layout3.css" type="text/css" />
+<link rel="stylesheet" href="/resources/templates/bootstrap-3.3.5-dist/css/bootstrap.min.css">
+</head>
+<body>
+
+
+<div class="wrapper col4">
+  <div class="container">
+    <div class="content">
+
+
+          <br>
+<br>
+<br>  
+
+
+<br>
+
+
+<style type="text/css">
+.tg  {border-collapse:collapse;border-spacing:0;}
+.tg td{font-family:Arial, sans-serif;font-size:14px;padding:2px 9px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;}
+.tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:2px 9px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;}
+.tg .tg-s6z2{text-align:center}
+.tg .tg-0ord{text-align:right}
+.tg .tg-x0v7{font-weight:bold;font-size:24px;text-align:right}
+.tg .tg-k3mp{font-weight:bold;color:#000000;}
+.tg .tg-fo4m{font-weight:bold;color:#000000;text-align:right}
+.tg .tg-a1hm{color:#cb0000;text-align:right}
+.tg .tg-oopb{color:#cb0000}
+.tg .tg-qtja{font-weight:bold;font-size:24px;color:#cb0000;text-align:right}
+.tg .tg-698h{background-color:#000000}
+.tg .tg-e3zv{font-weight:bold}
+.tg .tg-8xqh{font-weight:bold;background-color:#c0c0c0}
+.tg .tg-0onx{background-color:#000000;color:#000000}
+.tg .tg-97j9{font-weight:bold;background-color:#c0c0c0;color:#000000}
+.tg .tg-wsi6{font-style:italic;font-size:100%}
+</style>
+<table class="tg">
+  <tr>
+    <th class="tg-s6z2" colspan="2" rowspan="2"><img src="/img/Keyfacts.jpg" style="width:200px;height:100px"></th>
+    <th class="tg-0ord"><img src="/img/fpg_logo.png" style="float:right; width:200px;height:150px"></th>
+  </tr>
+  <tr>
+    <td class="tg-x0v7">about our services and costs</td>
+  </tr>
+  <tr>
+    <td class="tg-k3mp" colspan="2"><br>First Priority Group</td>
+    <td class="tg-fo4m"> 5 Prospect Place<br>Swansea, SA1 1QP</td>
+  </tr>
+  <tr>
+    <td class="tg-a1hm"></td>
+    <td class="tg-oopb"></td>
+    <td class="tg-qtja"></td>
+  </tr>
+  <tr>
+    <td class="tg-698h" colspan="3"></td>
+  </tr>
+  <tr>
+    <td class="tg-e3zv" colspan="3">1. The Financial Conduct Authority (FCA)</td>
+  </tr>
+  <tr>
+    <td class="tg-698h" colspan="3"></td>
+  </tr>
+  <tr>
+    <td class="tg-031e" colspan="3">The FCA is the independent watchdog that regulates financial services. This document has been designed by the FCA to be given to consumers considering buying certain financial products. You need to read this important document. It explains the service you are being offered and how you will pay for it.</td>
+  </tr>
+  <tr>
+    <td class="tg-698h" colspan="3"></td>
+  </tr>
+  <tr>
+    <td class="tg-e3zv" colspan="3">2. Whose products do we offer?</td>
+  </tr>
+  <tr>
+    <td class="tg-698h" colspan="3"></td>
+  </tr>
+  <tr>
+    <td class="tg-8xqh" colspan="3">Insurance</td>
+  </tr>
+  <tr>
+    <td class="tg-031e"><img src="/img/cross.png" style="width:50px;height:50px"></th></td>
+    <td class="tg-031e" colspan="2">We offer products from a range of insurers for Term Assurance, Critical Illness cover, Income protection and Private medical Insurance.</td>
+  </tr>
+  <tr>
+    <td class="tg-031e"><img src="/img/tick.png" style="width:50px;height:50px"></th></td>
+    <td class="tg-031e" colspan="2">We only offer products from a limited number of insurers for Home Insurance</td>
+  </tr>
+  <tr>
+    <td class="tg-031e"><img src="/img/tick.png" style="width:50px;height:50px"></th></td>
+    <td class="tg-031e" colspan="2">We can offer products for life insurance; please ask us for a list of insurers we offer insurance from</td>
+  </tr>
+  <tr>
+    <td class="tg-0onx" colspan="3"></td>
+  </tr>
+  <tr>
+    <td class="tg-e3zv" colspan="3">3. Which service will we provide you with?</td>
+  </tr>
+  <tr>
+    <td class="tg-698h" colspan="3"></td>
+  </tr>
+  <tr>
+    <td class="tg-97j9" colspan="3">Insurance</td>
+  </tr>
+  <tr>
+    <td class="tg-031e"><img src="/img/cross.png" style="width:50px;height:50px"></td>
+    <td class="tg-031e" colspan="2">We will advise and make a recommendation for you after we have assessed your needs.</td>
+  </tr>
+  <tr>
+    <td class="tg-031e"><img src="/img/tick.png" style="width:50px;height:50px"></th></td>
+    <td class="tg-031e" colspan="2">You will not receive advice or a recommendation from us. We may ask some questions to narrow down the selection of products that we will provide details on. You will then need to make your own choice about how to proceed.</td>
+  </tr>
+  <tr>
+    <td class="tg-698h" colspan="3"></td>
+  </tr>
+  <tr>
+    <td class="tg-e3zv" colspan="3">4. What will you have to pay us for our services?</td>
+  </tr>
+  <tr>
+    <td class="tg-698h" colspan="3"></td>
+  </tr>
+  <tr>
+    <td class="tg-8xqh" colspan="3">Insurance</td>
+  </tr>
+  <tr>
+    <td class="tg-031e"><img src="/img/cross.png" style="width:50px;height:50px"></td>
+    <td class="tg-031e" colspan="2">A fee<br></td>
+  </tr>
+  <tr>
+    <td class="tg-031e"><img src="/img/tick.png" style="width:50px;height:50px"></th></td>
+    <td class="tg-031e" colspan="2">No fee<br></td>
+  </tr>
+  <tr>
+    <td class="tg-031e" colspan="3">You will receive a quotation which will tell you about any other fees relating to any particular insurance policy.</td>
+  </tr>
+  <tr>
+    <td class="tg-698h" colspan="3"></td>
+  </tr>
+  <tr>
+    <td class="tg-e3zv" colspan="3">5. Who regulates us?</td>
+  </tr>
+  <tr>
+    <td class="tg-698h" colspan="3"></td>
+  </tr>
+  <tr>
+    <td class="tg-e3zv" colspan="3">First Priority Group is authorised and regulated by the Financial Conduct Authority. FCA Register number is 737871. You can check this on the FCA?s Register by visiting the FCA?s Website www.fca.gov.uk/register or by contacting the FCA on 0845 606 1234.<br></td>
+  </tr>
+  <tr>
+    <td class="tg-031e" colspan="3"></td>
+  </tr>
+  <tr>
+    <td class="tg-698h" colspan="3"></td>
+  </tr>
+  <tr>
+    <td class="tg-e3zv" colspan="3">6. What to do if you have a complaint</td>
+  </tr>
+  <tr>
+    <td class="tg-698h" colspan="3"></td>
+  </tr>
+  <tr>
+    <td class="tg-wsi6" colspan="3">If you wish to register a complaint, please contact us:<br><br>...in writing,First Priority Group LTD 5 Prospect Place, Swansea SA1 1QP<br><br>...by phone 01639 262100<br><br>If you cannot settle your complaint with us, you may be entitled to refer it to the Financial Ombudsman Service.</td>
+  </tr>
+  <tr>
+    <td class="tg-698h" colspan="3"></td>
+  </tr>
+  <tr>
+    <td class="tg-e3zv" colspan="3">7. Are we covered by the Financial Services Compensation Scheme (FSCS)?</td>
+  </tr>
+  <tr>
+    <td class="tg-698h" colspan="3"></td>
+  </tr>
+  <tr>
+    <td class="tg-031e" colspan="3">We are covered by the FSCS. You may be entitled to compensation from the scheme if we cannot meet our obligations. This depends on the type of business and the circumstances of the claim.</td>
+  </tr>
+  <tr>
+    <td class="tg-8xqh" colspan="3">Insurance</td>
+  </tr>
+  <tr>
+    <td class="tg-031e"><br></td>
+    <td class="tg-031e" colspan="2">Insurance advising and arranging is covered for 100% of the first £2,000 and 90% of the remainder of the claim, without any upper limit.,For compulsory classes of insurance, advising and arranging is covered for 100% of the claim, without any upper limit.</td>
+  </tr>
+</table>
+
+
+
+    </div>
+  </div>
+</div>
+
+</body>
+</html>

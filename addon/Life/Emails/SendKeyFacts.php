5df69aafb4d03d16e14611dbbbe5c251227c028d
Michael <michael@adl-crm.uk>
2018-01-17 13:39:54 +0000

    Minor changes.

--- addon/Life/Emails/SendKeyFacts.php
+++ addon/Life/Emails/SendKeyFacts.php
@@ -178,4 +178,125 @@ header('Location: /../../../../app/Client.php?search='.$CID.'&EMAIL_SENT=1&CLIEN
 }
 
 }
+
+if($companynamere=='First Priority Group') {
+
+$target_dir = filter_input(INPUT_SERVER,'DOCUMENT_ROOT', FILTER_SANITIZE_SPECIAL_CHARS)."/uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+
+if ($_FILES["fileToUpload"]["size"] > 700000) {
+    $uploadOk = 0;
+}
+
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" && $imageFileType != "pdf" ) {
+
+    $uploadOk = 0;
+}
+
+$message ="<img src='cid:KeyFacts'>";
+$sig = "<br>-- \n
+<br>
+<br>
+<br>
+
+$signat";
+$body = $message;
+$body .= $sig;
+$mail             = new PHPMailer();
+$mail->IsSMTP();
+$mail->CharSet = 'UTF-8';
+$mail->Host       = "$emailsmtpdb";
+$mail->SMTPAuth   = true; 
+$mail->SMTPSecure = "ssl"; 
+$mail->Port       = $emailsmtpportdb;
+$mail->Username   = "$emaildb";
+$mail->Password   = "$passworddb";
+$mail->SMTPDebug  = 2; 
+
+$mail->AddEmbeddedImage(filter_input(INPUT_SERVER,'DOCUMENT_ROOT', FILTER_SANITIZE_SPECIAL_CHARS).'/img/Key Facts - First Priority Group.png', 'KeyFacts');
+$mail->AddEmbeddedImage(filter_input(INPUT_SERVER,'DOCUMENT_ROOT', FILTER_SANITIZE_SPECIAL_CHARS).'/img/fpg_logo.png', 'logo');
+
+if (isset($_FILES["fileToUpload"]) &&
+    $_FILES["fileToUpload"]["error"] == UPLOAD_ERR_OK) {
+    $mail->AddAttachment($_FILES["fileToUpload"]["tmp_name"],
+                         $_FILES["fileToUpload"]["name"]);
+}
+
+if (isset($_FILES["fileToUpload2"]) &&
+    $_FILES["fileToUpload2"]["error"] == UPLOAD_ERR_OK) {
+    $mail->AddAttachment($_FILES["fileToUpload2"]["tmp_name"],
+                         $_FILES["fileToUpload2"]["name"]);
+}
+
+if (isset($_FILES["fileToUpload3"]) &&
+    $_FILES["fileToUpload3"]["error"] == UPLOAD_ERR_OK) {
+    $mail->AddAttachment($_FILES["fileToUpload3"]["tmp_name"],
+                         $_FILES["fileToUpload3"]["name"]);
+}
+
+if (isset($_FILES["fileToUpload4"]) &&
+    $_FILES["fileToUpload4"]["error"] == UPLOAD_ERR_OK) {
+    $mail->AddAttachment($_FILES["fileToUpload4"]["tmp_name"],
+                         $_FILES["fileToUpload4"]["name"]);
+}
+
+if (isset($_FILES["fileToUpload5"]) &&
+    $_FILES["fileToUpload5"]["error"] == UPLOAD_ERR_OK) {
+    $mail->AddAttachment($_FILES["fileToUpload5"]["tmp_name"],
+                         $_FILES["fileToUpload5"]["name"]);
+}
+
+if (isset($_FILES["fileToUpload6"]) &&
+    $_FILES["fileToUpload6"]["error"] == UPLOAD_ERR_OK) {
+    $mail->AddAttachment($_FILES["fileToUpload6"]["tmp_name"],
+                         $_FILES["fileToUpload6"]["name"]);
+}
+
+$mail->SetFrom("$emailfromdb", "$emaildisplaynamedb");
+$mail->AddReplyTo("$emailreplydb","$emaildisplaynamedb");
+$mail->AddBCC("$emailbccdb", "$emaildisplaynamedb");
+$mail->Subject    = "$emailsubjectdb";
+$mail->IsHTML(true); 
+$mail->AltBody    = "To view the message, please use an HTML compatible email viewer!";
+$mail->AddAddress($email, $recipient);
+$mail->Body    = $body;
+
+if(!$mail->Send()) {
+  echo "Mailer Error: " . $mail->ErrorInfo;
+  
+   $message="Closer KeyFacts email failed ($email)!";
+  
+                $noteq = $pdo->prepare("INSERT into client_note set client_id=:CID, note_type='Email Failed', client_name=:ref, message=:message, sent_by=:sent");
+                $noteq->bindParam(':CID', $CID, PDO::PARAM_STR);
+                $noteq->bindParam(':sent', $hello_name, PDO::PARAM_STR);
+                $noteq->bindParam(':message', $message, PDO::PARAM_STR);
+                $noteq->bindParam(':ref', $recipient, PDO::PARAM_STR);
+                $noteq->execute()or die(print_r($noteq->errorInfo(), true));  
+                
+  //header('Location: /../../../../app/Client.php?search='.$CID.'&EMAIL_SENT=0&CLIENT_EMAIL=Closer Keyfacts&EMAIL_SENT_TO='.$email); die;  
+  
+} else {
+       
+   $INSERT = $pdo->prepare("INSERT INTO keyfactsemail set keyfactsemail_email=:email, keyfactsemail_added_by=:hello");
+   $INSERT->bindParam(':email', $email, PDO::PARAM_STR);
+   $INSERT->bindParam(':hello', $hello_name, PDO::PARAM_STR);
+   $INSERT->execute()or die(print_r($INSERT->errorInfo(), true));
+   
+   $message="Closer KeyFacts email sent ($email)";
+
+                $noteq = $pdo->prepare("INSERT into client_note set client_id=:CID, note_type='Email Sent', client_name=:ref, message=:message, sent_by=:sent");
+                $noteq->bindParam(':CID', $CID, PDO::PARAM_STR);
+                $noteq->bindParam(':sent', $hello_name, PDO::PARAM_STR);
+                $noteq->bindParam(':message', $message, PDO::PARAM_STR);
+                $noteq->bindParam(':ref', $recipient, PDO::PARAM_STR);
+                $noteq->execute()or die(print_r($noteq->errorInfo(), true));
+
+header('Location: /../../../../app/Client.php?search='.$CID.'&EMAIL_SENT=1&CLIENT_EMAIL=Closer Keyfacts&EMAIL_SENT_TO='.$email); die;
+  
+}
+
+}
 ?>

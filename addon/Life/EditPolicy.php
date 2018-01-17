1533878663af507ea1f5620157ac72f0839cd38c
Michael <michael@adl-crm.uk>
2018-01-17 17:07:15 +0000

    Changes for LV insurance.

--- addon/Life/EditPolicy.php
+++ addon/Life/EditPolicy.php
@@ -207,7 +207,7 @@ if(empty($POST_NAME)) {
                                 </div>                                   
 <?php
 
-$AN_NUMBER_ARRAY=array("Legal and General","Zurich","Scottish Widows")
+$AN_NUMBER_ARRAY=array("Legal and General","Zurich","Scottish Widows","LV");
 
 ?>
                                 <div class="alert alert-info"><strong>Application Number:</strong> 
@@ -313,6 +313,11 @@ $AN_NUMBER_ARRAY=array("Legal and General","Zurich","Scottish Widows")
                                                 echo "selected";
                                             }
                                             ?> value="Royal London">Royal London</option>
+                                        <option <?php
+                                            if ($data2["insurer"] == 'LV') {
+                                                echo "selected";
+                                            }
+                                            ?> value="LV">LV</option>                                        
                                                                                <option <?php
                                             if ($data2["insurer"] == 'Zurich') {
                                                 echo "selected";

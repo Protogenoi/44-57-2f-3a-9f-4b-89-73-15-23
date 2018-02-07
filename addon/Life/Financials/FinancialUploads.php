c732017f9ec73e58099a9f82424cc4a63c9bc3c1
Michael <michael@adl-crm.uk>
2018-02-07 18:16:33 +0000

    Updated for LV.

--- addon/Life/Financials/FinancialUploads.php
+++ addon/Life/Financials/FinancialUploads.php
@@ -164,6 +164,18 @@ $dateto = filter_input(INPUT_GET, 'dateto', FILTER_SANITIZE_SPECIAL_CHARS);
                                             <button type="submit" name="Submit" value="Submit" data-toggle="modal" data-target="#processing-modal" class="btn btn-success "><span class="glyphicon glyphicon-open"></span> Upload</button>
                                         </form>
                                     </div>
+                                    
+                                    <div class="col-xs-6 col-md-6">
+                                        <h3>Upload LV Financials</h3>
+
+
+                                        <form action="/addon/Life/Financials/upload/finrupload.php?EXECUTE=11" method="post" enctype="multipart/form-data" name="form1" id="form1">
+                                            <input class="form-control" name="csv" type="file" id="csv" required>
+                                            <br>
+                                            <button type="submit" name="Submit" value="Submit" data-toggle="modal" data-target="#processing-modal" class="btn btn-success "><span class="glyphicon glyphicon-open"></span> Upload</button>
+                                        </form>
+                                    </div>                                      
+                                    
                                     <div class="col-xs-6 col-md-6">
                                         <h3>Upload Vitality Financials</h3>
 

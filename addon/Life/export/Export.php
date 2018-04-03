f3750cf9b9c26d51f8e80baa64ae3bfecaa43e67
Michael <michael@adl-crm.uk>
2018-04-03 12:19:30 +0100

    Added export for Aviva policies.

--- addon/Life/export/Export.php
+++ addon/Life/export/Export.php
@@ -423,7 +423,90 @@ WHERE
             }
             echo $output;
             exit;
-            }             
+            }      
+            
+            if($INSURER == 'Aviva') {
+            
+            $output = "Application_Number,Policy_Number, Sale_THEN_OLP_Date,COMM Date,Forename,ADL Amount,COMM Amount,Tel,Alt_Tel,DOB,Email,Address_1,Address_2,Town,Postcode,Premium,Type,Commission,Non-Idem Comm,Net_Paid,Closer,Status,Insurer,Owner,Company,Date_Added\n";
+            $query = $pdo->prepare("SELECT 
+    royal_london_financial.royal_london_financial_commission_credit_amount AS CREDIT,
+    royal_london_financial.royal_london_financial_commission_debits_amount AS DEBITS,
+    client_policy.application_number,
+    client_policy.policy_number AS POLICY_NUMBER,
+    CONCAT(DATE(client_policy.submitted_date), ' - ',
+    DATE(client_policy.sale_date)) as sale_sub,
+    client_policy.commission,
+    DATE(royal_london_financial.royal_london_financial_uploaded_date) AS insert_date,
+    '' AS empty_col,
+    client_policy.client_name,
+    '' AS empty_col,
+    client_details.phone_number,
+    client_details.alt_number,
+    CONCAT(client_details.dob,
+            ' - ',
+            client_details.dob2) AS CDOB,
+    client_details.email,
+    client_details.address1,
+    client_details.address2,
+    CONCAT(client_details.address3,
+            ' ',
+            client_details.town) AS TOWN,
+    client_details.post_code,
+    client_policy.premium,
+    client_policy.type,
+    client_policy.commission,
+    '' AS empty_col,
+    '' AS empty_col2,
+    CONCAT(client_policy.lead,
+            '/',
+            client_policy.closer) AS AGENTS,
+    client_policy.policystatus,
+    client_policy.insurer,
+    client_policy.submitted_by,
+    client_details.company,
+    client_details.submitted_date,
+    client_policy.non_indem_com
+FROM
+    client_policy
+        LEFT JOIN
+    client_details ON client_policy.client_id = client_details.client_id
+        LEFT JOIN
+    royal_london_financial ON royal_london_financial.royal_london_financial_plan_number = client_policy.policy_number
+WHERE
+    DATE(client_policy.sale_date) BETWEEN :datefrom AND :dateto AND client_policy.insurer='Aviva'
+        OR DATE(client_policy.submitted_date) BETWEEN :datefrom2 AND :dateto2 AND client_policy.insurer='Aviva'");
+            $query->bindParam(':datefrom', $DATE_FROM, PDO::PARAM_STR);
+            $query->bindParam(':dateto', $DATE_TO, PDO::PARAM_STR);
+            $query->bindParam(':datefrom2', $DATE_FROM, PDO::PARAM_STR);
+            $query->bindParam(':dateto2', $DATE_TO, PDO::PARAM_STR);
+            $query->execute();
+            $list = $query->fetchAll();
+            foreach ($list as $rs) {
+                
+            $ADL_AMOUNT = ($simply_biz/100) * $rs['commission'];
+            $pipe=$rs['commission']-$ADL_AMOUNT;  
+            $ADL_SUM = number_format($pipe, 2, '.', '.' ); 
+            $NON_INDEM_COM=$rs['non_indem_com'];
+
+            if(empty($rs['CREDIT'])) {
+                $CREDITS='NO CREDITS';
+            } else {
+                $CREDITS=$rs['CREDIT'];
+            }
+            if(empty($rs['DEBITS'])) {
+                $DEBITS='NO DEBITS';
+            }   else {
+                $DEBITS=$rs['DEBITS'];
+            }         
+
+            $AMOUNT="$CREDITS | $DEBITS";
+                
+                $output .= $rs['application_number'].",".$rs['POLICY_NUMBER'].",".$rs['sale_sub'].",".$rs['insert_date'].",".$rs['client_name'].",$ADL_SUM,".$AMOUNT.",".$rs['phone_number'].",".$rs['alt_number'].",".$rs['CDOB'].",".$rs['email'].",".$rs['address1'].",".$rs['address2'].",".$rs['TOWN'].",".$rs['post_code'].",".$rs['premium'].",".$rs['type'].",".$rs['commission'].",".$NON_INDEM_COM.",".$rs['empty_col2'].",".$rs['AGENTS'].",".$rs['policystatus'].",".$rs['insurer'].",".$rs['submitted_by'].",".$rs['company'].",".$rs['submitted_date']."\n";
+                
+            }
+            echo $output;
+            exit;
+            }              
             
             if($INSURER == 'Royal London') {
             

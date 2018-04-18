cbd8f8a68b6db406926d08d4cc536eb623ecadc2
Michael <michael@adl-crm.uk>
2018-04-18 13:56:52 +0100

    Updated for new Vitality policy.

--- addon/Life/Financials/models/financials/VITALITY/TotalMissingWithDates.php
+++ addon/Life/Financials/models/financials/VITALITY/TotalMissingWithDates.php
@@ -11,21 +11,23 @@ class TotalMissingWithDatesModal {
     public function getTotalMissingWithDates($datefrom, $dateto) {
 
         $stmt = $this->pdo->prepare("SELECT 
-    SUM(client_policy.commission) AS commission
+    SUM(vitality_policy.vitality_policy_comms) AS commission
 FROM
-    client_policy
+    adl_policy
+        JOIN
+    vitality_policy ON adl_policy.adl_policy_id = vitality_policy.vitality_policy_id_fk
         LEFT JOIN
-    vitality_financial ON vitality_financial.vitality_financial_policy_number = client_policy.policy_number
+    vitality_financial ON vitality_financial.vitality_financial_policy_number = adl_policy_ref
 WHERE
-    DATE(client_policy.sale_date) BETWEEN :datefrom AND :dateto
-        AND client_policy.policy_number NOT IN (SELECT 
+    DATE(adl_policy_sub_date) BETWEEN :DATEFROM AND :DATETO
+        AND adl_policy_ref NOT IN (SELECT 
             vitality_financial.vitality_financial_policy_number
         FROM
             financials)
-        AND client_policy.insurer = 'Vitality'
-        AND client_policy.policystatus = 'Live'");
-        $stmt->bindParam(':datefrom', $datefrom, PDO::PARAM_STR);
-        $stmt->bindParam(':dateto', $dateto, PDO::PARAM_STR);
+        AND adl_policy_insurer = 'Vitality'
+        AND adl_policy_status = 'Live'");
+        $stmt->bindParam(':DATEFROM', $datefrom, PDO::PARAM_STR);
+        $stmt->bindParam(':DATETO', $dateto, PDO::PARAM_STR);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }

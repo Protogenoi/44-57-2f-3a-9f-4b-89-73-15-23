ba489da9ca11f94014e39cf96b6a12bf728b240e
Michael <michael@adl-crm.uk>
2018-04-26 15:45:49 +0100

    Fixed duplicate label ids.

--- addon/Life/Insurers/Vitality/add_policy.php
+++ addon/Life/Insurers/Vitality/add_policy.php
@@ -600,7 +600,7 @@ if (isset($EXECUTE)) {
                                     <label class="radio-inline">
                                         <input id="KID_OPT_1" name="KID_OPT_1" type="radio" value="Index">Index</label>
                                         <label class="radio-inline">
-                                        <input id="KID_OPT_6" name="KID_OPT_6" type="radio" value="Comp and Index">Comp and Index</label> 
+                                        <input id="KID_OPT_1" name="KID_OPT_1" type="radio" value="Comp and Index">Comp and Index</label> 
                         </div>
                     </div>
                 </div>
@@ -645,7 +645,7 @@ if (isset($EXECUTE)) {
                                     <label class="radio-inline">
                                         <input id="KID_OPT_2" name="KID_OPT_2" type="radio" value="Index">Index</label>
                                         <label class="radio-inline">
-                                        <input id="KID_OPT_6" name="KID_OPT_6" type="radio" value="Comp and Index">Comp and Index</label> 
+                                        <input id="KID_OPT_2" name="KID_OPT_2" type="radio" value="Comp and Index">Comp and Index</label> 
                         </div>
                     </div>
                 </div>
@@ -735,7 +735,7 @@ if (isset($EXECUTE)) {
                                     <label class="radio-inline">
                                         <input id="KID_OPT_4" name="KID_OPT_4" type="radio" value="Index">Index</label>
                                         <label class="radio-inline">
-                                        <input id="KID_OPT_6" name="KID_OPT_6" type="radio" value="Comp and Index">Comp and Index</label> 
+                                        <input id="KID_OPT_4" name="KID_OPT_4" type="radio" value="Comp and Index">Comp and Index</label> 
                         </div>
                     </div>
                 </div>
@@ -780,7 +780,7 @@ if (isset($EXECUTE)) {
                                     <label class="radio-inline">
                                         <input id="KID_OPT_5" name="KID_OPT_5" type="radio" value="Index">Index</label>
                                         <label class="radio-inline">
-                                        <input id="KID_OPT_6" name="KID_OPT_6" type="radio" value="Comp and Index">Comp and Index</label> 
+                                        <input id="KID_OPT_5" name="KID_OPT_5" type="radio" value="Comp and Index">Comp and Index</label> 
                         </div>
                     </div>
                 </div>

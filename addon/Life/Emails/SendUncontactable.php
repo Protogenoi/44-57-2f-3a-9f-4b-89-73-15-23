9edf85bcdeb2d5a5ec940cc685bbdbe70dd6e9e5
Michael <michael@adl-crm.uk>
2018-05-11 15:57:30 +0100

    Updated uncontactable emails

--- addon/Life/Emails/SendUncontactable.php
+++ addon/Life/Emails/SendUncontactable.php
@@ -4,7 +4,7 @@
  *                               ADL CRM
  * ------------------------------------------------------------------------
  * 
- * Copyright © 2017 ADL CRM All rights reserved.
+ * Copyright © 2018 ADL CRM All rights reserved.
  * 
  * Unauthorised copying of this file, via any medium is strictly prohibited.
  * Unauthorised distribution of this file, via any medium is strictly prohibited.
@@ -12,7 +12,7 @@
  * 
  * Proprietary and confidential
  * 
- * Written by Michael Owen <michael@adl-crm.uk>, 2017
+ * Written by Michael Owen <michael@adl-crm.uk>, 2018
  * 
  * ADL CRM makes use of the following third party open sourced software/tools:
  *  DataTables - https://github.com/DataTables/DataTables
@@ -26,6 +26,7 @@
  *  jQuery UI - https://github.com/jquery/jquery-ui
  *  Google Dev Tools - https://developers.google.com
  *  Twitter API - https://developer.twitter.com
+ *  Webshim - https://github.com/aFarkas/webshim/releases/latest
  * 
 */  
 
@@ -76,54 +77,47 @@ require_once(__DIR__ . '/../../../resources/lib/PHPMailer_5.2.0/class.phpmailer.
         $emaildisplaynamedb=$queryr['displayname'];
         $SMTP_PASS=$queryr['password'];
         $SMTP_USER=$queryr['email'];
-        $signat=  html_entity_decode($queryr['sig']);
-        
-                $cnquery = $pdo->prepare("select company_name from company_details limit 1");
-                            $cnquery->execute()or die(print_r($query->errorInfo(), true));
-                            $companydetailsq=$cnquery->fetch(PDO::FETCH_ASSOC);
-                            $companynamere=$companydetailsq['company_name'];       
+        $signat=  html_entity_decode($queryr['sig']);     
  
-if(isset($hello_name)) {
-    
-     switch ($hello_name) {
-         case "Michael":
-             $hello_name_full="Michael Owen";
-             break;
-         case "Jakob":
-             $hello_name_full="Jakob Lloyd";
-             break;
-         case "leighton":
-             $hello_name_full="Leighton Morris";
-             break;
-         case "Roxy":
-             $hello_name_full="Roxanne Studholme";
-             break;
-         case "Nicola":
-             $hello_name_full="Nicola Griffiths";
-             break;
-         case "carys":
-             $hello_name_full="Carys Riley";
-             break;
-         case "Matt":
-             $hello_name_full="Matthew Jones";
-             break;
-         case "Tina":
-             $hello_name_full="Tina Dennis";
-             break;
-         case "Nick":
-             $hello_name_full="Nick Dennis";
-             break;
-         case "Ryan":
-             $hello_name_full="Ryan Lloyd";
-             break;         
-         default:
-             $hello_name_full=$hello_name;
-             
-     }
-     
-     }                            
+ if (isset($hello_name)) {
+
+        switch ($hello_name) {
+            case "Michael":
+                $hello_name_full = "Michael Owen";
+                break;
+            case "Jakob":
+                $hello_name_full = "Jakob Lloyd";
+                break;
+            case "leighton":
+                $hello_name_full = "Leighton Morris";
+                break;
+            case "Nicola":
+                $hello_name_full = "Nicola Griffiths";
+                break;
+            case "Jacs":
+                $hello_name_full = "Jaclyn Haford";
+                break;
+            case "carys":
+                $hello_name_full = "Carys Riley";
+                break;
+            case "Matt":
+                $hello_name_full = "Matthew Jones";
+                break;
+            case "Tina":
+                $hello_name_full = "Tina Dennis";
+                break;
+            case "Nick":
+                $hello_name_full = "Nick Dennis";
+                break;
+            case "Ryan":
+                $hello_name_full = "Ryan Lloyd";
+                break;
+            default:
+                $hello_name_full = $hello_name;
+        }
+}                           
                             
-if(isset($companynamere))  {      
+if(isset($COMPANY_ENTITY))  {      
 
 $EXECUTE= filter_input(INPUT_GET, 'EXECUTE', FILTER_SANITIZE_NUMBER_INT);
 $CID= filter_input(INPUT_GET, 'search', FILTER_SANITIZE_NUMBER_INT);
@@ -132,107 +126,7 @@ $email= filter_input(INPUT_POST, 'email', FILTER_SANITIZE_SPECIAL_CHARS);
 $recipient= filter_input(INPUT_GET, 'recipient', FILTER_SANITIZE_SPECIAL_CHARS);
 $INSURER= filter_input(INPUT_GET, 'insurer', FILTER_SANITIZE_SPECIAL_CHARS);
 
-if($companynamere=='Bluestone Protect') {
-     
-     if(isset($EXECUTE)) {
-         if($EXECUTE=='1') {
-
-$subject = "Bluestone Protect - Direct Debit" ;
-$sig = "<br>-- \n
-<br>
-<br>
-<br>
-$signat";
-
-$body = "<p>Dear $recipient,</p>
-          <p>           
-There is an issue with your $INSURER direct debit <strong>$policy</strong>. </p>
-
-          <p>
-We have tried contacting you on numerous occasions but have been unsuccessful, It is very important we speak to you.
-          </p>
-          <p>Please contact us on 03300 100 707 or email us back with a preferred contact time and number for us to call you. Office hours are between Monday to Friday 10:00 - 18:30.</p>
-          Many thanks,<br>
-$hello_name_full<br>Bluestone Protect
-          </p>";
-
-         }
-         
-         if($EXECUTE=='2') {
-
-$subject = "Bluestone Protect Life Insurance Application" ;
-$sig = "<br>-- \n
-<br>
-<br>
-<br>
-$signat";
-
-$body = "<p>Dear $recipient,</p>
-          <p>           
-There is an issue with your $INSURER life insurance application. </p>
-
-          <p>
-We have tried contacting you on numerous occasions but have been unsuccessful, It is very important we speak to you.
-          </p>
-          <p>Please contact us on 03300 100 707 or email us back with a preferred contact time and number for us to call you. Office hours are between Monday to Friday 10:00 - 18:30.</p>
-          Many thanks,<br>
-$hello_name_full<br>Bluestone Protect
-          </p>";
-
-         }         
-         
-$body .= $sig;
-
-$mail             = new PHPMailer();
-$mail->IsSMTP(); 
-$mail->CharSet = 'UTF-8';
-$mail->Host       = "$SMTP_HOST"; 
-$mail->SMTPAuth   = true;                 
-$mail->SMTPSecure = "ssl"; 
-$mail->Port       = $SMTP_PORT;                   
-$mail->Username   = "$SMTP_USER"; 
-$mail->Password   = "$SMTP_PASS";     
-$mail->SetFrom("$emailfromdb", "$emaildisplaynamedb");
-$mail->AddReplyTo("$emailreplydb","$emaildisplaynamedb");
-$mail->Subject    = $subject;
-$mail->IsHTML(true); 
-$mail->AltBody    = "To view the message, please use an HTML compatible email viewer!";
-$address = $email;
-$mail->AddAddress($address, $recipient);
-$mail->Body    = $body;
-
-if(!$mail->Send()) {
-  echo "Mailer Error: " . $mail->ErrorInfo;
-  
-$message="Uncontactable email failed ($email)";  
-  
-                $noteq = $pdo->prepare("INSERT into client_note set client_id=:CID, note_type='Email Failed', client_name=:ref, message=:message, sent_by=:sent");
-                $noteq->bindParam(':CID', $CID, PDO::PARAM_STR);
-                $noteq->bindParam(':sent', $hello_name, PDO::PARAM_STR);
-                $noteq->bindParam(':message', $message, PDO::PARAM_STR);
-                $noteq->bindParam(':ref', $recipient, PDO::PARAM_STR);
-                $noteq->execute()or die(print_r($noteq->errorInfo(), true));  
-                
-  header('Location: /../../../../app/Client.php?search='.$CID.'&EMAIL_SENT=0&CLIENT_EMAIL=Send policy number&EMAIL_SENT_TO='.$email); die;    
-  
-} else {
-    
-$message="Uncontactable email sent ($email)";
-
-                $noteq = $pdo->prepare("INSERT into client_note set client_id=:CID, note_type='Email Sent', client_name=:ref, message=:message, sent_by=:sent");
-                $noteq->bindParam(':CID', $CID, PDO::PARAM_STR);
-                $noteq->bindParam(':sent', $hello_name, PDO::PARAM_STR);
-                $noteq->bindParam(':message', $message, PDO::PARAM_STR);
-                $noteq->bindParam(':ref', $recipient, PDO::PARAM_STR);
-                $noteq->execute()or die(print_r($noteq->errorInfo(), true));
-                
-header('Location: /../../../../app/Client.php?search='.$CID.'&EMAIL_SENT=1&CLIENT_EMAIL=Send policy number&EMAIL_SENT_TO='.$email); die;
-
-}
-}
-}
-
-if($companynamere=='First Priority Group') {
+if($COMPANY_ENTITY=='First Priority Group') {
      
      if(isset($EXECUTE)) {
          if($EXECUTE=='1') {

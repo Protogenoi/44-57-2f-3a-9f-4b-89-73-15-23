cfa023ff9d56f6373958512e6d365446ed300fd1
Michael <michael@adl-crm.uk>
2018-05-15 17:42:23 +0100

    Removed old links.

--- app/admin/Upload.php
+++ app/admin/Upload.php
@@ -133,95 +133,10 @@ if (isset($fferror)) {
                                 </form>
                             </div>
 
-                            <div class="col-xs-6 col-md-6">
-                                <h3>Upload Client data</h3>
-                                <form action="/upload/clientpolup.php" method="post" enctype="multipart/form-data" name="form1" id="form1">
-                                    <input name="csv" type="file" id="csv" />
-                                    <input type="hidden" name="Processor" value="<?php echo $hello_name ?>"><br>
-                                    <button type="submit" class="btn btn-success "><span class="glyphicon glyphicon-open"></span> Upload</button>
-                                </form>
-
-                                <form action="/export/clientdetailstemp.php" method="post"><br>
-                                    <button type="submit" class="btn btn-info "><span class="glyphicon glyphicon-save"></span> Template</button>
-                                </form>
-                            </div>
-
-                            <div class="col-xs-6 col-md-6">
-                                <h3>Upload PBA Client data</h3>
-                                <form action="/upload/PBAclientup.php" method="post" enctype="multipart/form-data" name="form1" id="form1">
-                                    <input name="csv" type="file" id="csv" />
-
-                                    <button type="submit" class="btn btn-success "><span class="glyphicon glyphicon-open"></span> Upload</button>
-                                </form>
-
-                            </div>                            
-
-                            <div class="col-xs-6 col-md-6">
-                                <h3>Upload PBA Details</h3>
-                                <form action="/upload/PBAdetails.php" method="post" enctype="multipart/form-data" name="form1" id="form1">
-                                    <input name="csv" type="file" id="csv" />
-
-                                    <button type="submit" class="btn btn-success "><span class="glyphicon glyphicon-open"></span> Upload</button>
-                                </form>
-
-                            </div>
-
-                            <div class="col-xs-6 col-md-6">
-                                <h3>Upload Client Notes data</h3>
-                                <form action="/upload/clientnotesup.php" method="post" enctype="multipart/form-data" name="form1" id="form1">
-                                    <input name="csv" type="file" id="csv" />
-                                    <button type="submit" class="btn btn-success "><span class="glyphicon glyphicon-open"></span> Upload</button>
-                                </form>
-                            </div>
-
-                            <div class="col-xs-6 col-md-6">
-                                <h3>Upload Legacy Client data</h3>
-                                <form action="/upload/legclientpolup.php" method="post" enctype="multipart/form-data" name="form1" id="form1">
-                                    <input name="csv" type="file" id="csv" />
-                                    <input type="hidden" name="Processor" value="<?php echo $hello_name ?>"><br>
-                                    <button type="submit" class="btn btn-success "><span class="glyphicon glyphicon-open"></span> Upload</button>
-                                </form>
-
-                            </div>
-                            <div class="col-xs-6 col-md-6">
-                                <h3>Upload Legacy Policy data</h3>
-                                <form action="/upload/legpolup.php" method="post" enctype="multipart/form-data" name="form1" id="form1">
-                                    <input name="csv" type="file" id="csv" />
-                                    <input type="hidden" name="Processor" value="<?php echo $hello_name ?>"><br>
-                                    <button type="submit" class="btn btn-success "><span class="glyphicon glyphicon-open"></span> Upload</button>
-                                </form>
-                            </div>
-                            <div class="col-xs-6 col-md-6">
-                                <h3>Upload Legacy EWS</h3>
-                                <form action="/upload/ewslegupload.php" method="post" enctype="multipart/form-data" name="form1" id="form1">
-                                    <input name="csv" type="file" id="csv" />
-                                    <input type="hidden" name="Processor" value="<?php echo $hello_name ?>"><br>
-                                    <button type="submit" class="btn btn-success "><span class="glyphicon glyphicon-open"></span> Upload</button>
-                                </form>
-                            </div>
-
                         </div>
                     </div>
                 </div>
 
-                <?php if (isset($_GET['success'])) { ?>
-                    <div class="notice notice-success fade in">
-                        <a href=\"#\" class=\"close\" data-dismiss=\"alert\">&times;</a>
-                        <strong>Success!</strong> File Uploaded to Database.
-                    </div>
-
-                <?php } else if (isset($_GET['fail'])) {
-                    ?>
-                    <div class="notice notice-danger fade in">
-                        <a href="#" class="close" data-dismiss="alert">&times;</a>
-                        <strong>Error!</strong> File Upload Failed.
-                    </div>
-
-                    <?php
-                } else {
-                    
-                }
-                ?>
             </div>
         </div>
     </div>

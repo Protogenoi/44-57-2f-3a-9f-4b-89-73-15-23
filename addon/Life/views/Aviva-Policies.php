d6016d8276a2cf43d035750fefbf781da21dde26
Michael <michael@adl-crm.uk>
2018-06-01 11:02:49 +0100

    Updated for new Aviva financials.

--- addon/Life/views/Aviva-Policies.php
+++ addon/Life/views/Aviva-Policies.php
@@ -13,7 +13,6 @@
             <th>Premium</th>
             <th>Cover</th>
             <th>Status</th>
-            <th>EWS</th>
             <th>Financial</th>
             <th colspan="4">Options</th>
         </tr>
@@ -28,9 +27,6 @@
         $POL_HOLDER = $Aviva_Policies['client_name'];
         $APP_NUMBER = $Aviva_Policies['application_number'];
 
-        $ADLSTATUS = $Aviva_Policies['ADLSTATUS'];
-        $EWSSTATUS = $Aviva_Policies['warning'];
-        
         $COVER_AMOUNT = number_format($Aviva_Policies['covera'],2);
 
         echo '<tr>';
@@ -38,7 +34,7 @@
         if (empty($polref)) {
             echo "<td>TBC</td>";
         } else {
-            echo "<td><form target='_blank' action='#' method='post'><input type='hidden' value='$polref'><input type='hidden' name='searchCriteria.referenceType' id='searchCriteria.referenceType' value='B'><input type='hidden' name='searchCriteria.includeLife' value='true' ><button type='submit' value='Search' name='command' class='btn btn-default btn-sm'><i class='fa fa-search'></i> $polref</button></form></td>";
+            echo "<td><button type='submit' value='Search' name='command' class='btn btn-default btn-sm'><i class='fa fa-search'></i> $polref</button></td>";
         }
         echo "<td>$APP_NUMBER</td>";
         echo "<td>" . $Aviva_Policies['type'] . "</td>";
@@ -49,73 +45,32 @@
 
         if ($Aviva_Policies['PolicyStatus'] == 'CLAWBACK' || ['PolicyStatus'] == 'CLAWBACK-LAPSE' || $Aviva_Policies['PolicyStatus'] == 'Declined') {
             echo "<td><span class=\"label label-danger\">" . $Aviva_Policies['PolicyStatus'] . "</span></td>";
-        } elseif ($Aviva_Policies['PolicyStatus'] == 'PENDING' || $Aviva_Policies['PolicyStatus'] == 'Live Awaiting Policy Number' || $Aviva_Policies['PolicyStatus'] == 'Awaiting Policy Number') {
+        } 
+        
+        elseif ($Aviva_Policies['PolicyStatus'] == 'PENDING' || $Aviva_Policies['PolicyStatus'] == 'Live Awaiting Policy Number' || $Aviva_Policies['PolicyStatus'] == 'Awaiting Policy Number') {
             echo "<td><span class=\"label label-warning\">" . $Aviva_Policies['PolicyStatus'] . "</span></td>";
-        } elseif ($Aviva_Policies['PolicyStatus'] == 'SUBMITTED-LIVE' || $Aviva_Policies['PolicyStatus'] == 'Live') {
+        }
+        
+        elseif ($Aviva_Policies['PolicyStatus'] == 'SUBMITTED-LIVE' || $Aviva_Policies['PolicyStatus'] == 'Live') {
             echo "<td><span class=\"label label-success\">" . $Aviva_Policies['PolicyStatus'] . "</span></td>";
-        } else {
+        } 
+        
+        else {
             echo "<td><span class=\"label label-default\">" . $Aviva_Policies['PolicyStatus'] . "</span></td>";
         }
+      
 
-        if ($ADLSTATUS != $EWSSTATUS) {
-            switch ($EWSSTATUS) {
-                case "RE-INSTATED":
-                    echo "<td><span class='label label-success'>$EWSSTATUS</span></td>";
-                    break;
-                case "WILL CANCEL":
-                    echo "<td><span class='label label-warning'>$EWSSTATUS</span></td>";
-                    break;
-                case "REDRAWN":
-                case "WILL REDRAW":
-                    echo "<td><span class='label label-purple'>$EWSSTATUS</td>";
-                    break;
-                case "CANCELLED":
-                case "CFO":
-                case "LAPSED":
-                case "CANCELLED DD":
-                case "BOUNCED DD":
-                    echo "<td><span class='label label-danger'>$EWSSTATUS</td>";
-                    break;
-                default:
-                    echo "<td><span class='label label-info'>$EWSSTATUS</td>";
-            }
+        if (($Aviva_Policies['aviva_financial_amount'])) {
+            echo "<td><span class='label label-success'>On Financials</span> </td>";
         } else {
 
-            switch ($ADLSTATUS) {
-                case "RE-INSTATED":
-                    echo "<td><span class='label label-success'>$ADLSTATUS</span></td>";
-                    break;
-                case "WILL CANCEL":
-                    echo "<td><span class='label label-warning'>$ADLSTATUS</span></td>";
-                    break;
-                case "REDRAWN":
-                case "WILL REDRAW":
-                    echo "<td><span class='label label-purple'>$ADLSTATUS</td>";
-                    break;
-                case "CANCELLED":
-                case "CFO":
-                case "LAPSED":
-                case "CANCELLED DD":
-                case "BOUNCED DD":
-                    echo "<td><span class='label label-danger'>$ADLSTATUS</td>";
-                    break;
-                default:
-                    echo "<td><span class='label label-info'>$ADLSTATUS</td>";
-            }
-        }
-
-        if (($Aviva_Policies['financials_payment'])) {
-            echo "<td><span class='label label-warning'>On Financial</span> </td>";
-        } else {
-
-            echo "<td> </td>";
+            echo "<td><span class='label label-warning'>Pending</span></td>";
         }
 
         echo "<td><a href='/addon/Life/ViewPolicy.php?policyID=$PID&search=$search&WHICH_COMPANY=$WHICH_COMPANY' class='btn btn-info btn-xs'><i class='fa fa-eye'></i> </a></td>";
         echo "<td><a href='/addon/Life/EditPolicy.php?id=$PID&search=$search&name=$POL_HOLDER' class='btn btn-warning btn-xs'><i class='fa fa-edit'></i> </a></td>";
 
             if (in_array($hello_name, $Level_10_Access, true)) {
-
 
                 echo "<td>
                                                                                         <form method='POST' action='/app/admin/deletepolicy.php?DeleteLifePolicy=1'>
@@ -125,10 +80,6 @@
                                                                                             </td>";
             }
 
-
-        if (!empty($EWSSTATUS)) {
-            echo "<td><a href='Reports/EWSPolicySearch.php?EWSView=1&search=$search&policy_number=$polref' class='btn btn-success btn-xs'><i class='fa fa-warning'></i> </a></td>";
-        }
         echo "</tr>";
         ?>
 

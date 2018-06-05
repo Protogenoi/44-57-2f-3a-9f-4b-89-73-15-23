a46df45e29a6f86ff1b35660e15b3bf289d0bc84
Michael <michael@adl-crm.uk>
2018-06-05 16:19:51 +0100

    Changed variable names for Zurich figures.

--- addon/Life/Financials/views/financials/ZURICH/Total-Expected-With-Dates.php
+++ addon/Life/Financials/views/financials/ZURICH/Total-Expected-With-Dates.php
@@ -3,14 +3,14 @@
 
 
         <?php
-                $EXPECTED_WITH_DATES_COMMISSION = $ZURICH_TotalExpectedWithDatesList_Resuts['commission'];
+                $ZURICH_EXPECTED_WITH_DATES_COMMISSION = $ZURICH_TotalExpectedWithDatesList_Resuts['commission'];
                 
-                            $simply_EXPECTED_SUM = ($simply_biz / 100) * $EXPECTED_WITH_DATES_COMMISSION;
-                            $ADL_EXPECTED_SUM = $EXPECTED_WITH_DATES_COMMISSION - $simply_EXPECTED_SUM;
+                            $ZURICH_simply_EXPECTED_SUM = ($simply_biz / 100) * $ZURICH_EXPECTED_WITH_DATES_COMMISSION;
+                            $ZURICH_ADL_EXPECTED_SUM = $ZURICH_EXPECTED_WITH_DATES_COMMISSION - $ZURICH_simply_EXPECTED_SUM;
                             
-                            $ADL_EXPECTED_SUM_DATES_FORMAT = number_format($EXPECTED_WITH_DATES_COMMISSION, 2);
-                            $simply_EXPECTED_SUM_FORMAT = number_format($simply_EXPECTED_SUM, 2);
-                            $ADL_EXPECTED_SUM_FORMAT = number_format($ADL_EXPECTED_SUM, 2);
+                            $ZURICH_ADL_EXPECTED_SUM_DATES_FORMAT = number_format($ZURICH_EXPECTED_WITH_DATES_COMMISSION, 2);
+                            $ZURICH_simply_EXPECTED_SUM_FORMAT = number_format($ZURICH_simply_EXPECTED_SUM, 2);
+                            $ZURICH_ADL_EXPECTED_SUM_FORMAT = number_format($ZURICH_ADL_EXPECTED_SUM, 2);
                             
                         ?>
 

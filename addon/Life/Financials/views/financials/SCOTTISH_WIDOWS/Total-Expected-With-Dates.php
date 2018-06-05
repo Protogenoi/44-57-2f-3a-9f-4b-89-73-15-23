b2541af2a69a0c204a636b1c5fd96d3d2baaad65
Michael <michael@adl-crm.uk>
2018-06-05 16:56:12 +0100

    Changed variable names for Scottish Widows figures.

--- addon/Life/Financials/views/financials/SCOTTISH_WIDOWS/Total-Expected-With-Dates.php
+++ addon/Life/Financials/views/financials/SCOTTISH_WIDOWS/Total-Expected-With-Dates.php
@@ -3,14 +3,14 @@
 
 
         <?php
-                $EXPECTED_WITH_DATES_COMMISSION = $SCOTTISH_WIDOWS_TotalExpectedWithDatesList_Resuts['commission'];
+                $SW_EXPECTED_WITH_DATES_COMMISSION = $SCOTTISH_WIDOWS_TotalExpectedWithDatesList_Resuts['commission'];
                 
-                            $simply_EXPECTED_SUM = ($simply_biz / 100) * $EXPECTED_WITH_DATES_COMMISSION;
-                            $ADL_EXPECTED_SUM = $EXPECTED_WITH_DATES_COMMISSION - $simply_EXPECTED_SUM;
+                            $SW_simply_EXPECTED_SUM = ($simply_biz / 100) * $SW_EXPECTED_WITH_DATES_COMMISSION;
+                            $SW_ADL_EXPECTED_SUM = $SW_EXPECTED_WITH_DATES_COMMISSION - $SW_simply_EXPECTED_SUM;
                             
-                            $ADL_EXPECTED_SUM_DATES_FORMAT = number_format($EXPECTED_WITH_DATES_COMMISSION, 2);
-                            $simply_EXPECTED_SUM_FORMAT = number_format($simply_EXPECTED_SUM, 2);
-                            $ADL_EXPECTED_SUM_FORMAT = number_format($ADL_EXPECTED_SUM, 2);
+                            $SW_ADL_EXPECTED_SUM_DATES_FORMAT = number_format($SW_EXPECTED_WITH_DATES_COMMISSION, 2);
+                            $SW_simply_EXPECTED_SUM_FORMAT = number_format($SW_simply_EXPECTED_SUM, 2);
+                            $SW_ADL_EXPECTED_SUM_FORMAT = number_format($SW_ADL_EXPECTED_SUM, 2);
                             
                         ?>
 

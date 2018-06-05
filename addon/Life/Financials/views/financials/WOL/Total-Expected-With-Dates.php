e8c597f9f158aa00f1c0571110265778b43f969c
Michael <michael@adl-crm.uk>
2018-06-05 15:50:22 +0100

    Changed variable names for One Family figures.

--- addon/Life/Financials/views/financials/WOL/Total-Expected-With-Dates.php
+++ addon/Life/Financials/views/financials/WOL/Total-Expected-With-Dates.php
@@ -3,14 +3,14 @@
 
 
         <?php
-                $EXPECTED_WITH_DATES_COMMISSION = $WOL_TotalExpectedWithDatesList_Resuts['commission'];
+                $WOL_EXPECTED_WITH_DATES_COMMISSION = $WOL_TotalExpectedWithDatesList_Resuts['commission'];
                 
-                            $simply_EXPECTED_SUM = ($simply_biz / 100) * $EXPECTED_WITH_DATES_COMMISSION;
-                            $ADL_EXPECTED_SUM = $EXPECTED_WITH_DATES_COMMISSION - $simply_EXPECTED_SUM;
+                            $WOL_simply_EXPECTED_SUM = ($simply_biz / 100) * $WOL_EXPECTED_WITH_DATES_COMMISSION;
+                            $WOL_ADL_EXPECTED_SUM = $WOL_EXPECTED_WITH_DATES_COMMISSION - $WOL_simply_EXPECTED_SUM;
                             
-                            $ADL_EXPECTED_SUM_DATES_FORMAT = number_format($EXPECTED_WITH_DATES_COMMISSION, 2);
-                            $simply_EXPECTED_SUM_FORMAT = number_format($simply_EXPECTED_SUM, 2);
-                            $ADL_EXPECTED_SUM_FORMAT = number_format($ADL_EXPECTED_SUM, 2);
+                            $WOL_ADL_EXPECTED_SUM_DATES_FORMAT = number_format($WOL_EXPECTED_WITH_DATES_COMMISSION, 2);
+                            $WOL_simply_EXPECTED_SUM_FORMAT = number_format($WOL_simply_EXPECTED_SUM, 2);
+                            $WOL_ADL_EXPECTED_SUM_FORMAT = number_format($WOL_ADL_EXPECTED_SUM, 2);
                             
                         ?>
 

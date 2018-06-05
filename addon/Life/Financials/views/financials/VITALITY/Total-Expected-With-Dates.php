d0b5e90d342fcfc5ff5cc70174b578265513cdf8
Michael <michael@adl-crm.uk>
2018-06-05 15:44:15 +0100

    Changed variable names for Vitality and Royal London figures.

--- addon/Life/Financials/views/financials/VITALITY/Total-Expected-With-Dates.php
+++ addon/Life/Financials/views/financials/VITALITY/Total-Expected-With-Dates.php
@@ -3,14 +3,14 @@
 
 
         <?php
-                $EXPECTED_WITH_DATES_COMMISSION = $TotalExpectedWithDatesList_Resuts['commission'];
+                $VIT_EXPECTED_WITH_DATES_COMMISSION = $TotalExpectedWithDatesList_Resuts['commission'];
                 
-                            $simply_EXPECTED_SUM = ($simply_biz / 100) * $EXPECTED_WITH_DATES_COMMISSION;
-                            $ADL_EXPECTED_SUM = $EXPECTED_WITH_DATES_COMMISSION - $simply_EXPECTED_SUM;
+                            $VIT_simply_EXPECTED_SUM = ($simply_biz / 100) * $VIT_EXPECTED_WITH_DATES_COMMISSION;
+                            $VIT_ADL_EXPECTED_SUM = $VIT_EXPECTED_WITH_DATES_COMMISSION - $VIT_simply_EXPECTED_SUM;
                             
-                            $ADL_EXPECTED_SUM_DATES_FORMAT = number_format($EXPECTED_WITH_DATES_COMMISSION, 2);
-                            $simply_EXPECTED_SUM_FORMAT = number_format($simply_EXPECTED_SUM, 2);
-                            $ADL_EXPECTED_SUM_FORMAT = number_format($ADL_EXPECTED_SUM, 2);
+                            $VIT_ADL_EXPECTED_SUM_DATES_FORMAT = number_format($VIT_EXPECTED_WITH_DATES_COMMISSION, 2);
+                            $VIT_simply_EXPECTED_SUM_FORMAT = number_format($VIT_simply_EXPECTED_SUM, 2);
+                            $VIT_ADL_EXPECTED_SUM_FORMAT = number_format($VIT_ADL_EXPECTED_SUM, 2);
                             
                         ?>
 

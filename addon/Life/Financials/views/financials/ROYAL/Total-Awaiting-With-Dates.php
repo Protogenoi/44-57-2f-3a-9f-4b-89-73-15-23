7f324d498b8ab66ffefb07510964b062e7a34a4a
Michael <michael@adl-crm.uk>
2018-06-06 11:40:51 +0100

    Updated variable names.

--- addon/Life/Financials/views/financials/ROYAL/Total-Awaiting-With-Dates.php
+++ addon/Life/Financials/views/financials/ROYAL/Total-Awaiting-With-Dates.php
@@ -6,11 +6,11 @@
                 $AWAITING_WITH_DATES_COMMISSION = $RL_TotalAwaitingWithDatesList_Resuts['commission'];
                 
                             $simply_AWAITING_SUM = ($simply_biz / 100) * $AWAITING_WITH_DATES_COMMISSION;
-                            $ADL_AWAITING_SUM = $AWAITING_WITH_DATES_COMMISSION - $simply_AWAITING_SUM;
+                            $RL_ADL_AWAITING_SUM = $AWAITING_WITH_DATES_COMMISSION - $simply_AWAITING_SUM;
                             
-                            $ADL_AWAITING_SUM_DATES_FORMAT = number_format($AWAITING_WITH_DATES_COMMISSION, 2);
+                            $RL_ADL_AWAITING_SUM_DATES_FORMAT = number_format($AWAITING_WITH_DATES_COMMISSION, 2);
                             $simply_AWAITING_SUM_FORMAT = number_format($simply_AWAITING_SUM, 2);
-                            $ADL_AWAITING_SUM_FORMAT = number_format($ADL_AWAITING_SUM, 2);
+                            $RL_ADL_AWAITING_SUM_FORMAT = number_format($RL_ADL_AWAITING_SUM, 2);
                             
                         ?>
 

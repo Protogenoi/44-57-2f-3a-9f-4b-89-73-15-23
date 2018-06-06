4fee06f2e4d5cc165a11cced382712a502255444
Michael <michael@adl-crm.uk>
2018-06-06 18:02:58 +0100

    Corrected spelling for marital.

--- addon/audits/Zurich/call_audit.php
+++ addon/audits/Zurich/call_audit.php
@@ -868,7 +868,7 @@ $QUESTION_NUMBER=1;
                         </script>
 
                         <p>
-                            <label for="CD_Q5">Q<?php echo $QUESTION_NUMBER++; ?>. Did the closer ask the customers martial status?</label>
+                            <label for="CD_Q5">Q<?php echo $QUESTION_NUMBER++; ?>. Did the closer ask the customers marital status?</label>
                             <input type="radio" name="CD_Q5" 
 <?php if (isset($CD_Q5) && $CD_Q5 == "1") {
     echo "checked";

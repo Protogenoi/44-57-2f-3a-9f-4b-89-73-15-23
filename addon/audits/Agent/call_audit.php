c3744bc79c5688414225eae423a5817d3af069fe
Michael <michael@adl-crm.uk>
2018-06-12 13:42:26 +0100

    Updated input ids.

--- addon/audits/Agent/call_audit.php
+++ addon/audits/Agent/call_audit.php
@@ -419,11 +419,11 @@ function SHOW_MORE_QUESTIONS() {
   <label class="col-md-4 control-label" for="S2AQ2">Q2. What was the main reason you took the policy out?</label>
   <div class="col-md-4"> 
     <label class="radio-inline" for="S2AQ2">
-      <input name="S2AQ2" id="Q6RADIO" value="Yes" checked="checked" type="radio" onclick="javascript:q6JAVA();" >
+      <input name="S2AQ2" id="S2AQ2" value="Yes" checked="checked" type="radio" onclick="javascript:S2AC2_JAVA();" >
       Yes
     </label> 
     <label class="radio-inline" for="S2AQ2">
-      <input name="S2AQ2" id="radios-Yes" value="No"  type="radio" onclick="javascript:q6JAVA();" >
+      <input name="S2AQ2" id="S2AQ2" value="No"  type="radio" onclick="javascript:S2AC2_JAVA();" >
       No
     </label> 
   </div>
@@ -433,7 +433,7 @@ function SHOW_MORE_QUESTIONS() {
 <div class="form-group">
   <label class="col-md-4 control-label" for="S2AC2"></label>
   <div class="col-md-4">                     
-    <textarea class="form-control" id="S2AC2" name="S2AC2" rows="1" cols="74" maxlength="1000" onkeyup="textAreaAdjust(this)"></textarea>
+    <textarea class="form-control" id="S2AC2" name="S2AC2" rows="1" cols="74" maxlength="1000" onkeyup="textAreaAdjust(this)">NOT READY</textarea>
     <span class="help-block"><p id="S2AC2_LEFT" class="help-block ">You have reached the limit</p></span>
     <script>
 $(document).ready(function(){ 
@@ -457,8 +457,8 @@ $(document).ready(function(){
 </script>
 <script type="text/javascript">
 
-function q4JAVA() {
-    if (document.getElementById('S2AC2').checked) {
+function S2AC2_JAVA() {
+    if (document.getElementById('S2AQ2').checked) {
         document.getElementById('S2AC2_DIV').style.display = 'none';
     }
     else document.getElementById('S2AC2_DIV').style.display = 'block';
@@ -467,27 +467,67 @@ function q4JAVA() {
 </script>
   </div>
 </div>
-</div>    
+</div>   
 
 <div class="form-group">
   <label class="col-md-4 control-label" for="S2AQ3">Q3. Repayment or interest only?</label>
   <div class="col-md-4"> 
     <label class="radio-inline" for="S2AQ3">
-      <input name="S2AQ3" id="Q7RADIO" value="Yes" checked="checked" type="radio" onclick="javascript:q7JAVA();" >
+      <input name="S2AQ3" id="S2AQ3" value="Yes" checked="checked" type="radio" onclick="javascript:S2AC3_JAVA();" >
       Yes
     </label> 
     <label class="radio-inline" for="S2AQ3">
-      <input name="S2AQ3" id="S2AQ3" value="No"  type="radio" onclick="javascript:q7JAVA();" >
+      <input name="S2AQ3" id="S2AQ3" value="No"  type="radio" onclick="javascript:S2AC3_JAVA();" >
       No
     </label> 
   </div>
 </div>
+    
+<div id="S2AC3_DIV" style="display:none">
+<div class="form-group">
+  <label class="col-md-4 control-label" for="S2AC3"></label>
+  <div class="col-md-4">                     
+    <textarea class="form-control" id="S2AC3" name="S2AC3" rows="1" cols="74" maxlength="1000" onkeyup="textAreaAdjust(this)">NOT READY</textarea>
+    <span class="help-block"><p id="S2AC3_LEFT" class="help-block ">You have reached the limit</p></span>
+    <script>
+$(document).ready(function(){ 
+    $('#S2AC3_LEFT').text('1000 characters left');
+    $('#S2AC3').keydown(function () {
+        var max = 1000;
+        var len = $(this).val().length;
+        if (len >= max) {
+            $('#S2AC3_LEFT').text('You have reached the limit');
+            $('#S2AC3_LEFT').addClass('red');
+            $('#btnSubmit').addClass('disabled');            
+        } 
+        else {
+            var ch = max - len;
+            $('#S2AC3_LEFT').text(ch + ' characters left');
+            $('#btnSubmit').removeClass('disabled');
+            $('#S2AC3_LEFT').removeClass('red');            
+        }
+    });    
+});
+</script>
+<script type="text/javascript">
+
+function S2AC3_JAVA() {
+    if (document.getElementById('S2AQ3').checked) {
+        document.getElementById('S2AC3_DIV').style.display = 'none';
+    }
+    else document.getElementById('S2AC3_DIV').style.display = 'block';
+
+}
+</script>
+  </div>
+</div>
+</div>      
 
 <div class="form-group">
   <label class="col-md-4 control-label" for="S2AQ4">Q4. When was your last review on the policy?</label>
   <div class="col-md-4"> 
     <label class="radio-inline" for="S2AQ4">
-      <input name="S2AQ4" id="Q8RADIO" value="Yes" checked="checked" type="radio" onclick="javascript:q8JAVA();" >
+      <input name="S2AQ4" id="S2AQ4" value="Yes" checked="checked" type="radio" onclick="javascript:q8JAVA();" >
       Yes
     </label> 
     <label class="radio-inline" for="S2AQ4">
@@ -501,7 +541,7 @@ function q4JAVA() {
   <label class="col-md-4 control-label" for="S2AQ5">Q5. How did you take out the policy?</label>
   <div class="col-md-4"> 
     <label class="radio-inline" for="S2AQ5">
-      <input name="S2AQ5" id="Q9RADIO" value="Yes" checked="checked" type="radio" onclick="javascript:q9JAVA();" >
+      <input name="S2AQ5" id="S2AQ5" value="Yes" checked="checked" type="radio" onclick="javascript:q9JAVA();" >
       Yes
     </label> 
     <label class="radio-inline" for="S2AQ5">
@@ -515,7 +555,7 @@ function q4JAVA() {
   <label class="col-md-4 control-label" for="S2AQ6">Q6. How much are you paying on a monthly basis?</label>
   <div class="col-md-4"> 
     <label class="radio-inline" for="S2AQ6">
-      <input name="S2AQ6" id="Q10RADIO" value="Yes" checked="checked" type="radio" onclick="javascript:q10JAVA();" >
+      <input name="S2AQ6" id="S2AQ6" value="Yes" checked="checked" type="radio" onclick="javascript:q10JAVA();" >
       Yes
     </label> 
     <label class="radio-inline" for="radios-Yes">
@@ -529,7 +569,7 @@ function q4JAVA() {
   <label class="col-md-4 control-label" for="S2AQ7">Q7. How much are you covered for?</label>
   <div class="col-md-4"> 
     <label class="radio-inline" for="S2AQ7">
-      <input name="S2AQ7" id="Q11RADIO" value="Yes" checked="checked" type="radio" onclick="javascript:q11JAVA();" >
+      <input name="S2AQ7" id="S2AQ7" value="Yes" checked="checked" type="radio" onclick="javascript:q11JAVA();" >
       Yes
     </label> 
     <label class="radio-inline" for="radios-Yes">
@@ -543,7 +583,7 @@ function q4JAVA() {
   <label class="col-md-4 control-label" for="S2AQ8">Q8. How long do you have left on the policy?</label>
   <div class="col-md-4"> 
     <label class="radio-inline" for="radios-0">
-      <input name="S2AQ8" id="Q12RADIO" value="Yes" checked="checked" type="radio" onclick="javascript:q12JAVA();">
+      <input name="S2AQ8" id="S2AQ8" value="Yes" checked="checked" type="radio" onclick="javascript:q12JAVA();">
       Yes
     </label> 
     <label class="radio-inline" for="S2AQ8">
@@ -557,7 +597,7 @@ function q4JAVA() {
   <label class="col-md-4 control-label" for="S2AQ9">Q9. Is your policy single, joint or separate?</label>
   <div class="col-md-4"> 
     <label class="radio-inline" for="S2AQ9">
-      <input name="S2AQ9" id="Q13RADIO" value="Yes" checked="checked" type="radio" onclick="javascript:q13JAVA();">
+      <input name="S2AQ9" id="S2AQ9" value="Yes" checked="checked" type="radio" onclick="javascript:q13JAVA();">
       Yes
     </label> 
     <label class="radio-inline" for="S2AQ9">
@@ -571,7 +611,7 @@ function q4JAVA() {
   <label class="col-md-4 control-label" for="S2AQ10">Q10. Have you or your partner smoked in the last 12 months?</label>
   <div class="col-md-4"> 
     <label class="radio-inline" for="S2AQ10">
-      <input name="S2AQ10" id="Q14RADIO" value="Yes" checked="checked" type="radio" onclick="javascript:q14JAVA();" >
+      <input name="S2AQ10" id="S2AQ10" value="Yes" checked="checked" type="radio" onclick="javascript:q14JAVA();" >
       Yes
     </label> 
     <label class="radio-inline" for="S2AQ10">
@@ -585,7 +625,7 @@ function q4JAVA() {
   <label class="col-md-4 control-label" for="S2AQ11">Q11. Have you or your partner got or has had any health issues?</label>
   <div class="col-md-4"> 
     <label class="radio-inline" for="S2AQ11">
-      <input name="S2AQ11" id="Q15RADIO" value="Yes" checked="checked" type="radio" onclick="javascript:q15JAVA();">
+      <input name="S2AQ11" id="S2AQ11" value="Yes" checked="checked" type="radio" onclick="javascript:q15JAVA();">
       Yes
     </label> 
     <label class="radio-inline" for="S2AQ11">

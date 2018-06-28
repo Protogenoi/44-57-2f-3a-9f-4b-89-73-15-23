c5762003891ea1457e55ce1cd52bf65e0879654c
Michael <michael@adl-crm.uk>
2018-06-28 17:36:00 +0100

    Updated to use /app/core/head.php

--- app/SearchComplaints.php
+++ app/SearchComplaints.php
@@ -90,19 +90,12 @@ $EXECUTE = filter_input(INPUT_GET, 'EXECUTE', FILTER_SANITIZE_SPECIAL_CHARS);
         die;    
             
         }        
-?>
-<!DOCTYPE html>
-<html>
-    <title>ADL | Search Compliants</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <head>
-        <link rel="stylesheet" href="/resources/templates/ADL/main.css" type="text/css" />
-        <link rel="stylesheet" href="/resources/templates/bootstrap-3.3.5-dist/css/bootstrap.min.css">
-        <link rel="stylesheet" href="/resources/templates/bootstrap-3.3.5-dist/css/bootstrap-theme.min.css">
+        
+        $ADL_PAGE_TITLE = "Search Compliants";
+        require_once(__DIR__ . '/../app/core/head.php'); 
+        
+        ?>
         <link rel="stylesheet" type="text/css" href="/resources/lib/DataTable/datatables.min.css"/>
-        <link href="/img/favicon.ico" rel="icon" type="image/x-icon" />
-        <script type="text/javascript" language="javascript" src="/resources/templates/fontawesome/svg-with-js/js/fontawesome-all.js"></script>
     </head>
     <body>
 

8767e215291ddfe515ccc676ec1a04e9c15892b8
Michael <michael@adl-crm.uk>
2018-06-28 17:28:55 +0100

    Combined core <head> items i.e. js, css (moved to a php file for more options)

--- app/core/head.php
+++ app/core/head.php
@@ -1,3 +1,6 @@
+<!DOCTYPE html>
+<html lang="en">
+    <title><?php if(isset($ADL_PAGE_TITLE)) { echo "ADL | $ADL_PAGE_TITLE"; }else { echo "ADL"; } ?></title>
     
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -7,3 +10,4 @@
 <link rel="stylesheet" href="/resources/templates/bootstrap-3.3.5-dist/css/bootstrap.css">
 <script type="text/javascript" language="javascript" src="/resources/templates/fontawesome/svg-with-js/js/fontawesome-all.js"></script>
 <link href="/img/favicon.ico" rel="icon" type="image/x-icon" />
+

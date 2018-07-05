f6b00f6eaa709f99530ef0a93fafab6444486f64
Michael <michael@adl-crm.uk>
2018-07-05 15:16:50 +0100

    Menu page for new EWS.

---
+++ addon/Life/EWS/menu.php
@@ -0,0 +1,105 @@
+<?php
+/*
+ * ------------------------------------------------------------------------
+ *                               ADL CRM
+ * ------------------------------------------------------------------------
+ * 
+ * Copyright © 2018 ADL CRM All rights reserved.
+ * 
+ * Unauthorised copying of this file, via any medium is strictly prohibited.
+ * Unauthorised distribution of this file, via any medium is strictly prohibited.
+ * Unauthorised modification of this code is strictly prohibited.
+ * 
+ * Proprietary and confidential
+ * 
+ * Written by Michael Owen <michael@adl-crm.uk>, 2018
+ * 
+ * ADL CRM makes use of the following third party open sourced software/tools:
+ *  DataTables - https://github.com/DataTables/DataTables
+ *  EasyAutocomplete - https://github.com/pawelczak/EasyAutocomplete
+ *  PHPMailer - https://github.com/PHPMailer/PHPMailer
+ *  ClockPicker - https://github.com/weareoutman/clockpicker
+ *  fpdf17 - http://www.fpdf.org
+ *  summernote - https://github.com/summernote/summernote
+ *  Font Awesome - https://github.com/FortAwesome/Font-Awesome
+ *  Bootstrap - https://github.com/twbs/bootstrap
+ *  jQuery UI - https://github.com/jquery/jquery-ui
+ *  Google Dev Tools - https://developers.google.com
+ *  Twitter API - https://developer.twitter.com
+ *  Webshim - https://github.com/aFarkas/webshim/releases/latest
+ * 
+*/
+
+require_once(__DIR__ . '/../../../classes/access_user/access_user_class.php');
+$page_protect = new Access_user;
+$page_protect->access_page(filter_input(INPUT_SERVER,'PHP_SELF', FILTER_SANITIZE_SPECIAL_CHARS), "", 10);
+$hello_name = ($page_protect->user_full_name != "") ? $page_protect->user_full_name : $page_protect->user;
+
+$USER_TRACKING=0;
+
+require_once(__DIR__ . '/../../../includes/user_tracking.php'); 
+
+require_once(__DIR__ . '/../../../includes/adl_features.php');
+require_once(__DIR__ . '/../../../includes/Access_Levels.php');
+require_once(__DIR__ . '/../../../includes/adlfunctions.php');
+require_once(__DIR__ . '/../../../includes/ADL_PDO_CON.php');
+
+if ($ffanalytics == '1') {
+    require_once(__DIR__ . '/../../../app/analyticstracking.php');
+}
+
+if (isset($fferror)) {
+    if ($fferror == '1') {
+        ini_set('display_errors', 1);
+        ini_set('display_startup_errors', 1);
+        error_reporting(E_ALL);
+    }
+}
+
+if($fffinancials=='0') {
+    header('Location: /../../../../CRMmain.php?FEATURE=EWS');
+}
+
+        $ADL_PAGE_TITLE = "EWS Menu";
+        require_once(__DIR__ . '/../../../app/core/head.php'); 
+        
+        ?>  
+</head>
+<body>
+
+    <?php require_once(__DIR__ . '/../../../includes/navbar.php'); ?>
+    
+    <div class="container">        
+        <div class="row">
+            <div class="twelve columns">
+                <ul class="ca-menu">
+                    
+                    <li>
+                        <a href="ews_overview.php">
+                            <span class="ca-icon"><i class="fas fa-exclamation-triangle"></i></span>
+                            <div class="ca-content">
+                                <h2 class="ca-main">EWS<br/>Overview</h2>
+                                <h3 class="ca-sub"></h3>
+                            </div>
+                        </a>
+                    </li>                       
+                    
+                    <li>
+                        <a href="/addon/Life/EWS/upload_ews.php">
+                            <span class="ca-icon"><i class="fas fa-upload"></i></span>
+                            <div class="ca-content">
+                                <h2 class="ca-main">Upload RAW<br/>EWS</h2>
+                                <h3 class="ca-sub"></h3>
+                            </div>
+                        </a>
+                    </li>                     
+                    
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <script type="text/javascript" language="javascript" src="/resources/lib/jquery/jquery-3.0.0.min.js"></script>
+    <script src="/resources/templates/bootstrap-3.3.5-dist/js/bootstrap.min.js"></script> 
+</body>
+</html>
